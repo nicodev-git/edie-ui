e65b8a840c4ba73d931129e540a1727d7bc0ed10
nicodev-git <schaefernicker@gmail.com>
2016-12-20 18:02:14 +0200

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,40 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/main/resources/static/scripts');
-var APP_DIR = path.resolve(__dirname, 'web/src');
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
 
-var config = {
-  entry: APP_DIR + '/index.js',
-
-  resolve: {
-    root: [
-      APP_DIR,
-    ],
-    extensions: ['', '.js', '.jsx']
-  },
-
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-
-  module: {
-    loaders: [
-      { test: /\.jsx?/, loaders: ['babel'], include: APP_DIR },
-      { test: /\.json$/, loader: 'json' },
-      //{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-      { test: /\.css$/, loaders: ['style', 'css'] }
+    output: {
+        path: path.join(__dirname),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx?/, loaders: ['babel'] },
+            { test: /\.json$/, loader: 'json' },
+            //{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+            { test: /\.css$/, loaders: ['style', 'css'] }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false },
+            mangle: true,
+            sourcemap: false,
+            beautify: false,
+            dead_code: true
+        })
     ]
-  },
-  plugins: [
-    // new webpack.DefinePlugin({
-    //     'process.env.NODE_ENV': JSON.stringify('production')
-    // }),
-    // new webpack.optimize.UglifyJsPlugin({
-    //     compress: {
-    //         warnings: false
-    //     }
-    // }),
-    new webpack.ExtendedAPIPlugin(),
-  ]
 };
-
-module.exports = [config];
