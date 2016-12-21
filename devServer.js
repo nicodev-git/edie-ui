294f06edf469b152fb37f45e4eb539882b663c6b
nicodev-git <schaefernicker@gmail.com>
2016-12-21 17:17:58 +0300

    cnahge webpack config and npm scripts

---
+++ devServer.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const webpack = require('webpack')
+const path = require('path')
+const config = require('./webpack.config.dev')
+
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+
+const app = express()
+const port = 8123
+
+const compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  }
+}))
+
+app.use(webpackHotMiddleware(compiler))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+app.listen(port, 'localhost', function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.info('==> Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port)
+})
---
+++ webpack.config.prod.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: [
+    'babel-polyfill',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'static'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': "'production'",
+        'BACKEND_URL': JSON.stringify(process.env.BACKEND_URL || 'http://localhost:8080/')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      // js
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      // CSS
+      {
+        test: /\.styl$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'style-loader!css-loader!stylus-loader'
+      },
+      // images under 32k will be bundled into the js
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=32768'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  }
+}
