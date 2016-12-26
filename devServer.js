e06d45fd283bfaa65e6c8fca5c2d9d0dd4b69ec7
nicodev-git <schaefernicker@gmail.com>
2016-12-27 00:32:35 +0700

    redirect pictures requests to /images/{name}

--- devServer.js
+++ devServer.js
@@ -8,8 +8,15 @@ const webpackHotMiddleware = require('webpack-hot-middleware')
 
 const app = express()
 const port = 8123
+
+app.get('/externalpictures', (req, res, next) => {
+  return res.redirect(302, `/images/${req.query.name}`)
+  return next()
+})
+
 app.use(express.static('src'))
 app.use(express.static('static'))
+
 const compiler = webpack(config)
 app.use(webpackDevMiddleware(compiler, {
   noInfo: true,
