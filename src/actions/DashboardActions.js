76f1a6125d5ccd357c5c98ab17c63b70e36fde6d
nicodev-git <schaefernicker@gmail.com>
2016-12-29 02:39:15 +0700

    refactoring is in progress /3

--- src/actions/DashboardActions.js
+++ src/actions/DashboardActions.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { browserHistory } from 'react-router'
 import { assign, concat } from 'lodash'
 import {
   UPDATE_DASHBOARD,
@@ -9,3 +8,56 @@ import {
 
   API_ERROR
 } from './types'
+
+export const updateDashboard = (data) => {
+  return (dispatch) => {
+    dispatch({
+      type: UPDATE_DASHBOARD,
+      data
+    })
+  }
+}
+
+export const fetchIncidents = () => {
+  return (dispatch) => {
+    axios.get(`${ROOT_URL}/incident`, { params: { } })
+      .then(response => fetchIncidentsSuccess(dispatch, response))
+      .catch(error => fetchIncidentsFail(dispatch, error))
+  }
+
+  const fetchIncidentsFail = (dispatch, error) => {
+    dispatch({
+      type: API_ERROR, 
+      msg: error
+    })
+  }
+
+  const fetchIncidentsSuccess = (dispatch, response) => {
+    dispatch({ 
+      type: FETCH_DASHBOARD_INCIDENTS, 
+      data: c.data._embedded.incidents 
+    })
+  }
+}
+
+export const fetchBigIncidents = (params) => {
+  return (dispatch) => {
+    axios.get(`${ROOT_URL}/incident/search/findBy?${encodeUrlParams(params)}`)
+      .then(response => fetchBigIncidentsSuccess(dispatch, response))
+      .catch(error => fetchBigIncidentsFail(dispatch, error))
+  }
+
+  const fetchBigIncidentsFail = (dispatch, error) => {
+    dispatch({
+      type: API_ERROR, 
+      msg: error
+    })
+  }
+
+  const fetchBigIncidentsSuccess = (dispatch, response) => {
+    dispatch({ 
+      type: FETCH_DASHBOARD_BIGINCIDENTS, 
+      data: response.data._embedded.incidents 
+    })
+  }
+}
