b6f520f359883fb972471ac0e7bf63d31b059b8f
nicodev-git <schaefernicker@gmail.com>
2016-12-31 02:22:46 +0700

    decorator removed

--- src/containers/auth/SigninContainer.js
+++ src/containers/auth/SigninContainer.js
@@ -1,36 +1,23 @@
 import React, { Component } from 'react'
 import Signin from '../../components/auth/Signin'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 import { signUser } from '../../actions'
 
 class SigninContainer extends Component {
-  getChildContext () {
-    console.log(this.props)
-    return {
-      errorMessage: this.props.errorMessage,
-      signUser: this.props.signUser,
-      dispatch: this.props.dispatch
-    }
-  }
-
   render () {
-    console.log(this.props)
     return (
       <Signin {...this.props} />
     )
   }
 }
 
-SigninContainer.childContextTypes = {
-  errorMessage: React.PropTypes.string,
-  signUser: React.PropTypes.func,
-  dispatch: React.PropTypes.func
-}
-
-export default connect((state) => ({
-  errorMessage: state.auth.error
-}),
-(dispatch) => ({
-  ...bindActionCreators({ signUser }, dispatch)
-}))(SigninContainer)
+export default connect(
+  state => ({ errorMessage: state.auth.error }),
+  dispatch => {
+    return {
+      signUser: ({email, password}) => {
+        dispatch(signUser({email, password}))
+      }
+    }
+  }
+)(SigninContainer)
