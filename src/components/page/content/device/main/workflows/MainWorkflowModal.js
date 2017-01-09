4fcbb7a692d4c1166e706e2a801959b1fe30b38a
nicodev-git <schaefernicker@gmail.com>
2017-01-09 14:05:36 +0800

    Fixed workflows

--- src/components/page/content/device/main/workflows/MainWorkflowModal.js
+++ src/components/page/content/device/main/workflows/MainWorkflowModal.js
@@ -11,6 +11,10 @@ class MainWorkflowModal extends React.Component {
 
   }
 
+  onClickClose () {
+
+  }
+
   render () {
     const { handleSubmit } = this.props
     return (
@@ -21,50 +25,50 @@ class MainWorkflowModal extends React.Component {
         className="bootstrap-dialog type-primary">
         <div className="modal-header">
           <h4 className="modal-title bootstrap-dialog-title">
-            Copy/Move Rules
+            Workflow
           </h4>
           <div className="bootstrap-dialog-close-button">
             <button className="close" onClick={this.onClickClose.bind(this)}>×</button>
           </div>
         </div>
-        <div className="modal-body bootstrap-dialog-message p-none">
+        <div className="modal-body bootstrap-dialog-message">
           <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-            <div className="row">
+            <div className="row margin-md-bottom">
               <label className="col-md-3">Name</label>
               <div className="col-md-9">
                 <Field name="name" component="input" className="form-control" />
               </div>
             </div>
 
-            <div className="row">
+            <div className="row margin-md-bottom">
               <label className="col-md-3">Description</label>
               <div className="col-md-9">
-                <Field name="description" component="input" className="form-control" />
+                <Field name="desc" component="input" className="form-control" />
               </div>
             </div>
 
-            <div className="row">
+            <div className="row margin-md-bottom">
               <label className="col-md-3">Name</label>
               <div className="col-md-9">
                 <Field name="name" component="input" className="form-control" />
               </div>
             </div>
 
-            <div className="row">
+            <div className="row margin-md-bottom">
               <label className="col-md-3">Present</label>
               <div className="col-md-9">
                 <Field name="present" component="input" className="form-control" />
               </div>
             </div>
 
-            <div className="row">
+            <div className="row margin-md-bottom">
               <label className="col-md-3">Category</label>
               <div className="col-md-9">
                 <Field name="category" component="input" className="form-control" />
               </div>
             </div>
 
-            <div className="row">
+            <div className="row margin-md-bottom">
               <label className="col-md-3">Severity</label>
               <div className="col-md-9">
                 <Field name="severity" component="select" className="form-control">
@@ -76,6 +80,20 @@ class MainWorkflowModal extends React.Component {
                   <option>IGNOREDELETE</option>
                   <option>DEVICE</option>
                 </Field>
+              </div>
+            </div>
+
+            <div className="row margin-md-bottom">
+              <label className="col-md-3">Enabled</label>
+              <div className="col-md-9">
+                <Field name="enable" component="input" type="checkbox"/>
+              </div>
+            </div>
+
+            <div>
+              <div className="text-right">
+                <button className="btn btn-primary margin-sm-right" type="submit">OK</button>
+                <a href="javascript:;" className="btn btn-default" onClick={this.onClickClose.bind(this)}>Cancel</a>
               </div>
             </div>
           </form>
