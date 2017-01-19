62e40557dde330e723c94f9e996f4e790210e976
nicodev-git <schaefernicker@gmail.com>
2017-01-19 19:12:50 +0800

    Fixed monitors container

--- src/containers/page/content/device/monitors/MonitorsContainer.js
+++ src/containers/page/content/device/monitors/MonitorsContainer.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import Monitors from '../../../../../components/page/content/device/monitors/Monitors'
 import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
+import {
+  openDeviceMonitorPicker,
+  openDeviceMonitorWizard,
+  closeDeviceMonitorWizard,
+  updateMapDevice
+} from 'actions'
 
 @connect(
   state => ({ device: state.dashboard.selectedDevice })
