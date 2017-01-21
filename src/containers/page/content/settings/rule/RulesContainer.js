e8fd568fadbcfe2fb094586f8593ebba966fab2d
nicodev-git <schaefernicker@gmail.com>
2017-01-21 12:28:17 +0800

    Fixed routes

--- src/containers/page/content/settings/rule/RulesContainer.js
+++ src/containers/page/content/settings/rule/RulesContainer.js
@@ -2,18 +2,31 @@ import React from 'react'
 import Rules from '../../../../../components/page/content/settings/rule/Rules'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { fetchWorkflows, openWorkflowModal, removeWorkflow } from '../../../../../actions'
+import {
+  fetchWorkflows,
+  openWorkflowModal,
+  removeWorkflow,
+
+  addWorkflow,
+  updateWorkflow,
+  closeWorkflowModal
+} from '../../../../../actions'
 
 @connect(
   state => ({
     workflows: state.settings.workflows,
+    editWorkflow: state.settings.editWorkflow,
     workflowModalVisible: state.settings.workflowModalVisible
   }),
   dispatch => ({
     ...bindActionCreators({
       fetchWorkflows,
       openWorkflowModal,
-      removeWorkflow
+      removeWorkflow,
+
+      addWorkflow,
+      updateWorkflow,
+      closeWorkflowModal
     }, dispatch)
   })
 )
