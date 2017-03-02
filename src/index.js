91918970ade83fa86ce8f6256d82d9f86bb35ca8
nicodev-git <schaefernicker@gmail.com>
2017-03-02 13:29:42 +0200

    import material ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { createStore, applyMiddleware } from 'redux'
 import reducers from './reducers'
 import Routes from './routes'
@@ -114,8 +115,10 @@ if (token) {
 console.log('Version: 0.8.4')
 
 ReactDOM.render(
+  <MuiThemeProvider>
     <Provider store={store}>
         {Routes}
     </Provider>
+  </MuiThemeProvider>
     , document.getElementById('app')
 )
