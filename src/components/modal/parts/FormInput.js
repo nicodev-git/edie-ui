8038140c2b9d54d250f87a2ac7896d7445937229
nicodev-git <schaefernicker@gmail.com>
2017-03-07 12:29:45 +0200

    material styles

--- src/components/modal/parts/FormInput.js
+++ src/components/modal/parts/FormInput.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import TextField from 'material-ui/TextField'
 
+const labelStyle = {
+  color: '#5683bb'
+}
+
+const errorStyle = {
+  color: '#d32f2f'
+}
+
+const inputStyle = {
+  color: '#777777'
+}
+
 const FormInput = ({input, label, meta: { touched, error }, ...custom}) => (
   <TextField hintText={label}
     floatingLabelText={label}
+    floatingLabelStyle={labelStyle}
     errorText={touched && error}
+    errorStyle={errorStyle}
+    inputStyle={inputStyle}
+    underlineFocusStyle={labelStyle}
     {...input}
     {...custom}
   />
