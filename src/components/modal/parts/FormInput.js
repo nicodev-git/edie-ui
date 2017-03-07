1bd1a7f6a17138a76cc7381a98fd17dcce1478a8
nicodev-git <schaefernicker@gmail.com>
2017-03-07 13:29:01 +0200

    styles

--- src/components/modal/parts/FormInput.js
+++ src/components/modal/parts/FormInput.js
@@ -1,32 +1,13 @@
 import React from 'react'
 import TextField from 'material-ui/TextField'
-
-const labelStyle = {
-  color: '#5683bb'
-}
-
-const errorStyle = {
-  color: '#d32f2f'
-}
-
-const inputStyle = {
-  color: '#777777',
-  fontSize: '18pt'
-}
-
-const textareaStyle = {
-  color: '#777777',
-  fontSize: '18pt'
-}
-
-const underlineStyle = {
-  borderColor: '#5683bb'
-}
+import { labelStyle, labelFocusStyle, errorStyle, inputStyle, textareaStyle,
+  underlineStyle } from './materialStyles'
 
 const FormInput = ({input, label, meta: { touched, error }, ...custom}) => (
   <TextField hintText={label}
     floatingLabelText={label}
     floatingLabelStyle={labelStyle}
+    floatingLabelFocusStyle={labelFocusStyle}
     errorText={touched && error}
     errorStyle={errorStyle}
     inputStyle={inputStyle}
---
+++ src/components/modal/parts/materialStyles.js
@@ -0,0 +1,25 @@
+export const labelStyle = {
+  color: '#5683bb'
+}
+
+export const labelFocusStyle = {
+  display: 'none'
+}
+
+export const errorStyle = {
+  color: '#d32f2f'
+}
+
+export const inputStyle = {
+  color: '#777777',
+  fontSize: '18pt'
+}
+
+export const textareaStyle = {
+  color: '#777777',
+  fontSize: '18pt'
+}
+
+export const underlineStyle = {
+  borderColor: '#5683bb'
+}
