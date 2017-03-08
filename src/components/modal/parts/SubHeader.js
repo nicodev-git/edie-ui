b9f85401adc178c5d5376de6b3125ca32a006734
nicodev-git <schaefernicker@gmail.com>
2017-03-08 15:53:31 +0200

    modals

---
+++ src/components/modal/parts/SubHeader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { subHeaderStyle } from './materialStyles'
+
+const SubHeader = ({name}) => (
+  <div className="modal-header" style={headerStyle}>
+    <h4 className="modal-title bootstrap-dialog-title">{name}</h4>
+    <div className="bootstrap-dialog-close-button">
+      <button className="close" onClick={onClick}>×</button>
+    </div>
+  </div>
+)
+
+export default SubHeader
