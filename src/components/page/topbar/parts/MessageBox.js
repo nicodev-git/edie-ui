21d77088ce6471a9767841da47b2cf5efdd04b18
nicodev-git <schaefernicker@gmail.com>
2017-03-09 17:27:51 +0200

    topbar

--- src/components/page/topbar/parts/MessageBox.js
+++ src/components/page/topbar/parts/MessageBox.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import Messages from './Messages'
+import Badge from 'material-ui/Badge'
+import IconButton from 'material-ui/IconButton'
+import MailOutlineIcon from 'material-ui/svg-icons/communication/mail-outline'
 
 const MessageBox = () => (
   <li className="dropdown">
-    <a data-toggle="dropdown" className="dropdown-toggle" href="javascript:;">
-      <i className="glyphicon glyphicon-envelope" style={{padding: '2px'}} />
-      <span className="badge badge-up badge-dark badge-small">3</span>
-    </a>
-    <Messages />
+    <Badge
+      badgeContent={4}
+      primary
+      >
+        <IconButton tooltip={<Messages/>}>
+          <MailOutlineIcon />
+        </IconButton>
+    </Badge>
   </li>
 )
 
 export default MessageBox
+
+/* <li className="dropdown">
+  <a data-toggle="dropdown" className="dropdown-toggle" href="javascript:;">
+    <i className="glyphicon glyphicon-envelope" style={{padding: '2px'}} />
+    <span className="badge badge-up badge-dark badge-small">3</span>
+  </a>
+  <Messages />
+</li> */
