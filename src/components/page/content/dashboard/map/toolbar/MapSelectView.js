bc933f84228041d4a0c4ba68bb4ebe1d52135b84
nicodev-git <schaefernicker@gmail.com>
2017-03-13 15:41:26 +0200

    material ui

--- src/components/page/content/dashboard/map/toolbar/MapSelectView.js
+++ src/components/page/content/dashboard/map/toolbar/MapSelectView.js
@@ -1,6 +1,26 @@
 import React from 'react'
+import IconButton from 'material-ui/IconButton'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
 
 const MapSelectView = ({ selectedMap, onChange, maps }) => (
+  <IconMenu
+    iconButtonElement={
+      <IconButton style={buttonStyle} iconStyle={iconStyle}>
+          <AccoutCircleIcon color="#777777"/>
+      </IconButton>
+    }
+    anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+    targetOrigin={{horizontal: 'left', vertical: 'top'}}
+  >
+    <MenuItem primaryText="Profile" onTouchTap={onClickProfile}/>
+    <MenuItem primaryText="Messages" onTouchTap={onClickMessages}/>
+    <Divider />
+    <MenuItem primaryText="Log out" onTouchTap={onSignOut}/>
+  </IconMenu>
+)
+
+/* const MapSelectView = ({ selectedMap, onChange, maps }) => (
   <select
     className="input-sm map-select margin-sm-left"
     style={{marginTop: '-9px'}}
@@ -14,6 +34,6 @@ const MapSelectView = ({ selectedMap, onChange, maps }) => (
       )
     }
   </select>
-)
+) */
 
 export default MapSelectView
