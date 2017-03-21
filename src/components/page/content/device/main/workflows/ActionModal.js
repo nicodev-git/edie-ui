f6eee70bfb18e278efc90c508a1db82f38ef7b34
nicodev-git <schaefernicker@gmail.com>
2017-03-21 18:35:05 +0200

    modals

--- src/components/page/content/device/main/workflows/ActionModal.js
+++ src/components/page/content/device/main/workflows/ActionModal.js
@@ -1,95 +1,43 @@
 import React from 'react'
-import Modal from 'react-bootstrap-modal'
 import { assign } from 'lodash'
-import {reduxForm, Field} from 'redux-form'
-import { connect } from 'react-redux'
-import { closeWfActionModal } from 'actions'
+import SimpleModalContainer from '../../../../../../containers/modal/SimpleModalContainer'
+import { validate } from '../../../../../modal/validation/NameValidation'
+import { closeWfActionModal } from '../../../../../../actions'
 
-class ActionModal extends React.Component {
-  onHide () {
-
-  }
-
-  handleFormSubmit (values) {
+export default class ActionModal extends React.Component {
+  doAction (values) {
     const {editWfAction} = this.props
     let props = assign({}, editWfAction, values)
-
-    if (!props.name) return window.alert('Please type name.')
-
     this.props.onClose(props, editWfAction)
-    this.onClickClose()
   }
 
   onClickClose () {
-    this.props.closeWfActionModal()
+    console.log('closing modal...')
+    // TODO
+    closeWfActionModal()
   }
 
   render () {
-    const {handleSubmit} = this.props
+    let header = 'Action'
+    let options = [
+      { value: 'OPEN_INCIDENT', label: 'OPEN_INCIDENT' }
+    ]
+    let content = [
+      {name: 'Name'},
+      {type: 'select', name: 'Type', options: options},
+      {name: 'Command'},
+      {name: 'Params'}
+    ]
+    let initialValues = assign({actionType: 'OPEN_INCIDENT'}, this.props.editWfAction)
     return (
-      <Modal
-        show
-        onHide={this.onHide.bind(this)}
-        aria-labelledby="ModalHeader"
-        className="bootstrap-dialog type-primary">
-        <div className="modal-header">
-          <h4 className="modal-title bootstrap-dialog-title">
-            Action
-          </h4>
-          <div className="bootstrap-dialog-close-button">
-            <button className="close" onClick={this.onClickClose.bind(this)}>×</button>
-          </div>
-        </div>
-        <div className="modal-body bootstrap-dialog-message">
-          <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-            <div className="row margin-md-bottom">
-              <label className="col-md-3">Name</label>
-              <div className="col-md-9">
-                <Field name="name" component="input" className="form-control"/>
-              </div>
-            </div>
-
-            <div className="row margin-md-bottom">
-              <label className="col-md-3">Type</label>
-              <div className="col-md-9">
-                <Field name="actionType" component="select" className="form-control">
-                  <option>OPEN_INCIDENT</option>
-                </Field>
-              </div>
-            </div>
-
-            <div className="row margin-md-bottom">
-              <label className="col-md-3">Command</label>
-              <div className="col-md-9">
-                <Field name="command" component="input" className="form-control"/>
-              </div>
-            </div>
-
-            <div className="row margin-md-bottom">
-              <label className="col-md-3">Params</label>
-              <div className="col-md-9">
-                <Field name="params" component="input" className="form-control"/>
-              </div>
-            </div>
-
-            <div className="text-right">
-              <button className="btn btn-primary btn-sm margin-sm-right" type="submit">OK</button>
-              <a href="javascript:;" className="btn btn-defa  ult btn-sm" onClick={this.onClickClose.bind(this)}>Cancel</a>
-            </div>
-          </form>
-        </div>
-      </Modal>
+      <SimpleModalContainer
+        header={header}
+        content={content}
+        doAction={this.doAction.bind(this)}
+        onClose={this.onClickClose.bind(this)}
+        validate={validate}
+        initialValues={initialValues}
+      />
     )
   }
 }
-
-export default connect(
-  state => ({
-    editWfAction: state.devices.editWfAction,
-    initialValues: assign({
-      actionType: 'OPEN_INCIDENT'
-    }, state.devices.editWfAction)
-  }), {
-    closeWfActionModal
-  }
-)(reduxForm({form: 'workflowActionForm'})(ActionModal))
