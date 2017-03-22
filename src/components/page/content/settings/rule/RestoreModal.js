7aadbd89a64e693b0e38682a35a85203422fa00c
nicodev-git <schaefernicker@gmail.com>
2017-03-22 17:30:55 +0200

    modals

--- src/components/page/content/settings/rule/RestoreModal.js
+++ src/components/page/content/settings/rule/RestoreModal.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
-import Modal from 'react-bootstrap-modal'
-import {
-    Button
-} from 'react-bootstrap'
-import { showAlert } from '../../../../shared/Alert'
 import { ROOT_URL } from '../../../../../actions/config'
+import SimpleModalContainer from '../../../../../containers/modal/SimpleModalContainer'
+import { validate } from '../../../../modal/validation/NameValidation'
 
 export default class RestoreModal extends Component {
   constructor (props) {
@@ -13,6 +10,9 @@ export default class RestoreModal extends Component {
       open: true,
       files: []
     }
+    this.closeModal = this.closeModal.bind(this)
+    this.onClickSave = this.onClickSave.bind(this)
+    this.renderOptions = this.renderOptions.bind(this)
   }
 
   componentWillMount () {
@@ -21,24 +21,13 @@ export default class RestoreModal extends Component {
     })
   }
 
-  onHide () {
-    this.onClickClose()
-  }
-
   closeModal (data) {
     this.setState({ open: false }, () => {
       this.props.onClose && this.props.onClose(this, data)
     })
   }
 
-  onClickClose () {
-    this.closeModal()
-  }
-
-  onClickSave () {
-    const name = this.refs.name.value
-    if (!name) return showAlert('Please choose backup.')
-
+  onClickSave ({name}) {
     $.get(`${ROOT_URL}${Api.rule.restoreRules}`, { // eslint-disable-line no-undef
       exportName: name
     }).done((res) => {
@@ -49,43 +38,32 @@ export default class RestoreModal extends Component {
     })
   }
 
+  renderOptions () {
+    let files = this.state.files
+    let options = files.map(item =>
+      {
+        value: item,
+        label: item
+      }
+    )
+  }
+
   render () {
+    let header = 'Backup'
+    let options = this.renderOptions()
+    let content = [
+      {type: 'select', name: 'Name', options: options}
+    ]
+    let buttonText = 'Restore'
     return (
-      <Modal
-        show={this.state.open}
-        onHide={this.onHide.bind(this)}
-        aria-labelledby="ModalHeader"
-        className="bootstrap-dialog type-primary"
-      >
-
-        <div className="modal-header">
-          <h4 className="modal-title bootstrap-dialog-title">
-            Backup
-          </h4>
-          <div className="bootstrap-dialog-close-button">
-            <button className="close" onClick={this.onClickClose.bind(this)}>×</button>
-          </div>
-        </div>
-
-        <div className="modal-body bootstrap-dialog-message">
-
-          <div className="row margin-md-bottom">
-            <label className="control-label col-md-3">Name</label>
-            <div className="col-md-9">
-              <select className="form-control" ref="name">{
-                this.state.files.map(item =>
-                  <option key={item}>{item}</option>
-                )
-              }</select>
-            </div>
-          </div>
-
-          <div className="text-right p-none">
-            <Button className="btn-primary" onClick={this.onClickSave.bind(this)}>Restore</Button>
-            <Button className="margin-sm-left" onClick={this.onClickClose.bind(this)}>Cancel</Button>
-          </div>
-        </div>
-      </Modal>
+      <SimpleModalContainer
+        header={header}
+        content={content}
+        doAction={this.onClickSave}
+        onClose={this.closeModal}
+        validate={validate}
+        buttonText={buttonText}
+      />
     )
   }
 }
