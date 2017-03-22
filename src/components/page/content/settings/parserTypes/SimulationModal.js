e75fa1a3668578ab31926ff4e8d725aa8d64a443
nicodev-git <schaefernicker@gmail.com>
2017-03-22 16:28:49 +0200

    modals

--- src/components/page/content/settings/parserTypes/SimulationModal.js
+++ src/components/page/content/settings/parserTypes/SimulationModal.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import Modal from 'react-bootstrap-modal'
-import {
-  Button
-} from 'react-bootstrap'
+import SimpleModalContainer from '../../../../../containers/modal/SimpleModalContainer'
+import { validate } from '../../../../modal/validation/NameValidation'
 
 class SimulationModal extends React.Component {
-
-  onHide () {
-
+  constructor (props) {
+    super(props)
+    this.onClickClose = this.onClickClose.bind(this)
+    this.onClickSave = this.onClickSave.bind(this)
   }
 
   onClickClose () {
@@ -15,56 +14,26 @@ class SimulationModal extends React.Component {
   }
 
   onClickSave () {
-
+    // TODO
+    this.onClickClose()
   }
 
   render () {
+    let header = 'Simulation'
+    let content = [
+      {name: 'Text'},
+      {name: 'Filter 1'},
+      {name: 'Filter 2'},
+      {name: 'Parser'}
+    ]
     return (
-      <Modal show onHide={this.onHide.bind(this)} aria-labelledby="ModalHeader" className="bootstrap-dialog type-primary">
-        <div className="modal-header">
-          <h4 className="modal-title bootstrap-dialog-title">
-            Simulation
-          </h4>
-          <div className="bootstrap-dialog-close-button">
-            <button className="close" onClick={this.onClickClose.bind(this)}>×</button>
-          </div>
-        </div>
-
-        <div className="modal-body bootstrap-dialog-message">
-          <div className="row margin-md-bottom">
-            <label className="col-md-2 control-label">Text</label>
-            <div className="col-md-10">
-                  <textarea className="form-control" ref="text" style={{minHeight: '90px'}}/>
-            </div>
-          </div>
-
-          <div className="row margin-md-bottom">
-            <label className="col-md-2 control-label">Filter 1</label>
-            <div className="col-md-10">
-              <input type="text" className="form-control"/>
-            </div>
-          </div>
-
-          <div className="row margin-md-bottom">
-            <label className="col-md-2 control-label">Filter 2</label>
-            <div className="col-md-10">
-              <input type="text" className="form-control"/>
-            </div>
-          </div>
-
-          <div className="row margin-md-bottom">
-            <label className="col-md-2 control-label">Parser</label>
-            <div className="col-md-10">
-              <input className="form-control"/>
-            </div>
-          </div>
-
-          <div className="text-right">
-            <Button className="btn-primary btn-sm" onClick={this.onClickSave.bind(this)}>OK</Button>
-            <Button className="btn-sm margin-sm-left" onClick={this.onClickClose.bind(this)}>Cancel</Button>
-          </div>
-        </div>
-      </Modal>
+      <SimpleModalContainer
+        header={header}
+        content={content}
+        doAction={this.onClickSave}
+        onClose={this.onClickClose}
+        validate={validate}
+      />
     )
   }
 }
