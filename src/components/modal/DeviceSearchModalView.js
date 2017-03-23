f05383c38157ab518c85c799ac37b8ecfc90ae19
nicodev-git <schaefernicker@gmail.com>
2017-03-23 18:02:34 +0200

    modals

--- src/components/modal/DeviceSearchModalView.js
+++ src/components/modal/DeviceSearchModalView.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Modal from 'react-bootstrap-modal'
-import { Header, TwoButtonsBlock } from './parts'
+import Autocomplete from 'react-autocomplete'
+import { Header, SubHeader, CloseButton } from './parts'
 
-export default class DiagramModalView extends Component {
+export default class DeviceSearchModalView extends Component {
   render () {
-    const {show, header, onHide, onSave, dragLayer, toolbar, sidebar,
-      panel, objectModal} = this.props
+    const {show, onHide, value, selected, items, styles,
+      onSelect, onChange, onRemove} = this.props
     return (
       <Modal
         show={show}
@@ -13,18 +14,64 @@ export default class DiagramModalView extends Component {
         aria-labelledby="ModalHeader"
         className="bootstrap-dialog type-primary"
       >
-        <Header name={header} />
+        <Header name="Device Search" />
         <div className="modal-body bootstrap-dialog-message">
-          <div className="diagram">
-            {dragLayer}
-            {toolbar}
-            <div className="flex-horizontal">
-              {sidebar}
-              {panel}
+          <div className="search-modal-container">
+            <SubHeader name="Search:"/>
+            <div className="inline" style={{position: 'relative'}}>
+              <Autocomplete
+                inputProps={{
+                  name: 'Device',
+                  id: 'device-autocomplete'
+                }}
+                className="form-control input-sm"
+                menuStyle={{
+                  borderRadius: '3px',
+                  boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
+                  background: 'rgba(255, 255, 255, 0.9)',
+                  padding: '2px 0',
+                  fontSize: '90%',
+                  position: 'absolute',
+                  overflow: 'auto',
+                  maxHeight: '300px',
+                  top: '100%',
+                  left: 0
+                }}
+                ref="autocomplete"
+                value={value}
+                items={items}
+                getItemValue={(item) => item.name}
+                onSelect={onSelect}
+                onChange={onChange}
+                renderItem={(item, isHighlighted) => (
+                    <div
+                      style={isHighlighted ? styles.highlightedItem : styles.item}
+                      key={item.id}
+                      id={item.id}
+                    >{item.name}</div>
+                )}
+              />
             </div>
           </div>
-          {objectModal}
-          <TwoButtonsBlock onSave={onSave} onClose={onHide}/>
+          <div className="margin-md-top" style={{height: '300px', overflow: 'auto'}}>
+            <table className="table table-hover">
+              <tbody>
+              {
+                selected.map(device =>
+                  <tr key={device.id}>
+                    <td>{device.name}</td>
+                    <td style={{width: '40px'}}>
+                      <a href="javascript:;" onClick={onRemove.bind(this, device)}>
+                        <i className="fa fa-times" />
+                      </a>
+                    </td>
+                  </tr>
+                )
+              }
+              </tbody>
+            </table>
+          </div>
+          <CloseButton onClose={onHide}/>
         </div>
       </Modal>
     )
--- src/components/page/content/search/DeviceSearchModal.js
+++ src/components/page/content/search/DeviceSearchModal.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react'
-import Modal from 'react-bootstrap-modal'
-import Autocomplete from 'react-autocomplete'
 import { findIndex, concat } from 'lodash'
+import { DeviceSearchModalView } from '../../../modal'
 
 const styles = {
   item: {
     padding: '2px 6px',
     cursor: 'default'
   },
-
   highlightedItem: {
     color: 'white',
     background: 'hsl(200, 50%, 50%)',
     padding: '2px 6px',
     cursor: 'default'
   },
-
   menu: {
     border: 'solid 1px #ccc'
   }
@@ -26,12 +23,14 @@ export default class DeviceSearchModal extends Component {
     super(props)
     this.state = {
       open: true,
-
       value: '',
       loading: false,
-
       selected: props.devices || []
     }
+    this.closeModal = this.closeModal.bind(this)
+    this.onSelect = this.onSelect.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.onClickRemove = this.onClickRemove.bind(this)
   }
 
   componentWillMount () {
@@ -47,17 +46,10 @@ export default class DeviceSearchModal extends Component {
     }
   }
 
-  onHide () {
-    this.closeModal()
-  }
-
-  closeModal (data) {
+  closeModal () {
+    let data = this.state.selected
     this.props.onClose &&
     this.props.onClose(this, data)
-  }
-
-  onClickClose () {
-    this.closeModal(this.state.selected)
   }
 
   searchDevice (keyword) {
@@ -71,95 +63,35 @@ export default class DeviceSearchModal extends Component {
     const index = selected.indexOf(device)
     if (index < 0) return
     selected.splice(index, 1)
-
     this.setState({selected})
+  }
+
+  onSelect (value, item) {
+    let {selected} = this.state
+    const index = findIndex(selected, {id: value})
+    if (index >= 0) return
+    selected.push(item)
+    this.setState({ value: '', selected })
+  }
+
+  onChange (event, value) {
+    this.setState({ value, loading: true })
+    this.searchDevice(value)
   }
 
   render () {
     return (
-      <Modal
+      <DeviceSearchModalView
         show
-        onHide={this.onHide.bind(this)}
-        aria-labelledby="ModalHeader"
-        className="bootstrap-dialog type-primary">
-
-        <div className="modal-header">
-          <h4 className="modal-title bootstrap-dialog-title">
-            Device Search
-          </h4>
-          <div className="bootstrap-dialog-close-button">
-            <button className="close"
-              onClick={this.onClickClose.bind(this)}>×
-            </button>
-          </div>
-        </div>
-
-        <div className="modal-body bootstrap-dialog-message form-inline">
-          <label className="inline margin-xs-right">Search: </label>
-          <div className="inline" style={{position: 'relative'}}>
-            <Autocomplete
-              inputProps={{
-                name: 'Device',
-                id: 'device-autocomplete'
-              }}
-              className="form-control input-sm"
-              menuStyle={{
-                borderRadius: '3px',
-                boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
-                background: 'rgba(255, 255, 255, 0.9)',
-                padding: '2px 0',
-                fontSize: '90%',
-                position: 'absolute',
-                overflow: 'auto',
-                maxHeight: '300px',
-                top: '100%',
-                left: 0
-              }}
-              ref="autocomplete"
-              value={this.state.value}
-              items={this.props.incidentDevices}
-              getItemValue={(item) => item.name}
-              onSelect={(value, item) => {
-                let {selected} = this.state
-                const index = findIndex(selected, {id: value})
-                if (index >= 0) return
-                selected.push(item)
-                this.setState({ value: '', selected })
-              }}
-              onChange={(event, value) => {
-                this.setState({ value, loading: true })
-                this.searchDevice(value)
-              }}
-              renderItem={(item, isHighlighted) => (
-                  <div
-                    style={isHighlighted ? styles.highlightedItem : styles.item}
-                    key={item.id}
-                    id={item.id}
-                  >{item.name}</div>
-              )}
-            />
-          </div>
-
-          <div className="margin-md-top" style={{height: '300px', overflow: 'auto'}}>
-            <table className="table table-hover">
-              <tbody>
-              {
-                this.state.selected.map(device =>
-                  <tr key={device.id}>
-                    <td>{device.name}</td>
-                    <td style={{width: '40px'}}>
-                      <a href="javascript:;" onClick={this.onClickRemove.bind(this, device)}>
-                        <i className="fa fa-times" />
-                      </a>
-                    </td>
-                  </tr>
-                )
-              }
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </Modal>
+        onHide={this.closeModal}
+        value={this.state.value}
+        selected={this.state.selected}
+        items={this.props.incidentDevices}
+        onSelect={this.onSelect}
+        onChange={this.onChange}
+        onRemove={this.onClickRemove}
+        styles={styles}
+      />
     )
   }
 }
