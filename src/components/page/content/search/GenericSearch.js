efd3f33d5101b7b622f91630532dddb449a7bec7
nicodev-git <schaefernicker@gmail.com>
2017-03-25 18:51:03 +0800

    Fixed search tabs

--- src/components/page/content/search/GenericSearch.js
+++ src/components/page/content/search/GenericSearch.js
@@ -1,13 +1,64 @@
 import React from 'react'
 
+import {ResponsiveInfiniteTable} from '../../../shared/InfiniteTable'
+
 import SearchTabs from './SearchTabs'
 import TabPage from '../../../shared/TabPage'
 import TabPageBody from '../../../shared/TabPageBody'
 import TabPageHeader from '../../../shared/TabPageHeader'
 
 class GenericSearch extends React.Component {
-  onSearchKeyUp (e) {
-    this.props.updateSearchKeyword(e.target.value)
+  constructor (props) {
+    super(props)
+
+    this.state = {
+
+      severities: [
+        { label: 'High', value: 'HIGH' },
+        { label: 'Medium', value: 'MEDIUM' },
+        { label: 'Low', value: 'LOW' },
+        { label: 'Audit', value: 'AUDIT' },
+        { label: 'Ignore', value: 'IGNORE' }
+      ],
+
+      selectedSeverity: ['HIGH', 'MEDIUM'],
+      selectedDevices: [],
+      afterStartTimestamp: '',
+      beforeStartTimestamp: '',
+      fixed: false,
+      search: '',
+
+      selectedIndex: -1,
+      commentModalVisible: false,
+
+      deviceModalVisible: false
+    }
+
+    this.cells = [{
+      'displayName': 'Type',
+      'columnName': 'type',
+      'cssClassName': 'width-80'
+    }, {
+      'displayName': 'Content',
+      'columnName': 'entity.id',
+      'customComponent': (props) => {
+        const {rowData} = props
+        if (!rowData.entity) return <span/>
+        let data = JSON.stringify(rowData.entity)
+        if (data.length > 500) data = `${data.substring(0, 500)}...`
+        return <span>{data}</span>
+      }
+    }]
+  }
+
+  onSearchKeyDown (e) {
+    if (e.keyCode === 13) {
+      this.props.updateSearchKeyword(e.target.value)
+    }
+  }
+
+  onRowDblClick () {
+
   }
 
   render () {
@@ -19,8 +70,8 @@ class GenericSearch extends React.Component {
               <input
                 type="text" placeholder="Search" className="form-control"
                 style={{width: '220px', paddingLeft: '35px'}}
-                value={this.props.keyword || ''}
-                onChange={this.onSearchKeyUp.bind(this)}
+                defaultValue={this.props.keyword}
+                onKeyDown={this.onSearchKeyDown.bind(this)}
                 ref="search"/>
               <a className="btn" href="javascript:;" style={{position: 'absolute', left: 0, top: 0}}>
                 <i className="fa fa-search" />
@@ -31,7 +82,17 @@ class GenericSearch extends React.Component {
         </TabPageHeader>
 
         <TabPageBody tabs={SearchTabs} tab={0}>
-          Generic
+          <ResponsiveInfiniteTable
+            url="/search/all"
+            cells={this.cells}
+            ref="table"
+            rowMetadata={{'key': 'id'}}
+            selectable
+            onRowDblClick={this.onRowDblClick.bind(this)}
+            params={{
+              query: this.props.keyword || ''
+            }}
+          />
         </TabPageBody>
       </TabPage>
     )
--- src/components/page/content/search/SearchTabs.js
+++ src/components/page/content/search/SearchTabs.js
@@ -1,13 +1,13 @@
-const tabs = [{
-  title: 'All', path: '/search'
-}, {
-  title: 'Incidents', path: '/search/incidents'
-}, {
-  title: 'IP Info', path: '/search/devices'
-}, {
-  title: 'Process', path: '/search/process'
-}, {
-  title: 'Events', path: '/search/events'
-}]
-
+// const tabs = [{
+//   title: 'All', path: '/search'
+// }, {
+//   title: 'Incidents', path: '/search/incidents'
+// }, {
+//   title: 'IP Info', path: '/search/devices'
+// }, {
+//   title: 'Process', path: '/search/process'
+// }, {
+//   title: 'Events', path: '/search/events'
+// }]
+const tabs = []
 export default tabs
