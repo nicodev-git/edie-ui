66846ed54374c0b7a332b18b1f314ea19e843282
nicodev-git <schaefernicker@gmail.com>
2017-03-28 19:03:28 +0300

    image upload

--- src/components/modal/parts/FormImg.js
+++ src/components/modal/parts/FormImg.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Dropzone from 'react-dropzone'
 
 const style = {
@@ -8,41 +8,27 @@ const style = {
   verticalAlign: 'middle'
 }
 
-class FormImg extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      count: -1
-    }
-  }
-  onDrop (filesToUpload, e) {
-    let field = this.props.field
-    field.input.onChange(filesToUpload)
-    this.setState({
-      count: this.state.count++
-    })
-  }
-  render () {
-    let field = this.props.field
-    let files = field.input.value
-    let count = this.state.count
-    return (
-      <div className="image-container">
-        <Dropzone
-          name={field.name}
-          style={style}
-          onDrop={this.onDrop.bind(this)}
-        >
-          {files ? null : <div>Here should be a nice picture designed by Cvetan</div>}
-        </Dropzone>
-        {field.meta.touched &&
-          field.meta.error &&
-          <span className="error">{field.meta.error}</span>}
-        {files && (<div className="image-uploader"><img src={files[count].preview}/></div>)}
-        {console.log(files)}
-      </div>
-    )
-  }
+const FormImg = (field) => {
+  let files = field.input.value
+  return (
+    <div className="image-container">
+      <Dropzone
+        name={field.name}
+        style={style}
+        onDrop={(filesToUpload, e) => {
+          let value = field.input.onChange(filesToUpload)
+          console.log(filesToUpload)
+          return value
+        }}
+      >
+        {files ? null : (<div>Here should be a nice picture designed by Cvetan</div>)}
+      </Dropzone>
+      {field.meta.touched &&
+        field.meta.error &&
+        <span className="error">{field.meta.error}</span>}
+      {files && (<div className="image-uploader"><img src={files[0].preview}/></div>)}
+    </div>
+  )
 }
 
 export default FormImg
