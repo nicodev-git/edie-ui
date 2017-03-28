087edd77db5a13c9b3f19cdfb47b3969764aebb4
nicodev-git <schaefernicker@gmail.com>
2017-03-28 18:41:14 +0300

    image upload

--- src/components/modal/parts/FormImg.js
+++ src/components/modal/parts/FormImg.js
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Dropzone from 'react-dropzone'
 
-const FormImg = (field) => {
-  const file = field.input.value
-  return (
-    <div className="image-container">
-      <Dropzone
-        name={field.name}
-        multiple={false}
-        onDrop={(filesToUpload, e) => field.input.onChange(filesToUpload)}
-      >
-        <div>Try dropping some files here, or click to select files to upload.</div>
-      </Dropzone>
-      {field.meta.touched &&
-        field.meta.error &&
-        <span className="error">{field.meta.error}</span>}
-      {file && (<div className="image-uploader"><img src={file[0].preview}/></div>)}
-    </div>
-  )
+const style = {
+  width: '100%',
+  height: '100%',
+  textAlign: 'center',
+  verticalAlign: 'middle'
+}
+
+class FormImg extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      count: -1
+    }
+  }
+  onDrop (filesToUpload, e) {
+    let field = this.props.field
+    field.input.onChange(filesToUpload)
+    this.setState({
+      count: this.state.count++
+    })
+  }
+  render () {
+    let field = this.props.field
+    let files = field.input.value
+    let count = this.state.count
+    return (
+      <div className="image-container">
+        <Dropzone
+          name={field.name}
+          style={style}
+          onDrop={this.onDrop.bind(this)}
+        >
+          {files ? null : <div>Here should be a nice picture designed by Cvetan</div>}
+        </Dropzone>
+        {field.meta.touched &&
+          field.meta.error &&
+          <span className="error">{field.meta.error}</span>}
+        {files && (<div className="image-uploader"><img src={files[count].preview}/></div>)}
+        {console.log(files)}
+      </div>
+    )
+  }
 }
 
 export default FormImg
