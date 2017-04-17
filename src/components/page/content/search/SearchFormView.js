c96746c858ab9d8c054fe98585c4047b526902c8
nicodev-git <schaefernicker@gmail.com>
2017-04-17 11:48:46 +0800

    Fixed search form

--- src/components/page/content/search/SearchFormView.js
+++ src/components/page/content/search/SearchFormView.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Field } from 'redux-form'
 import { concat } from 'lodash'
-import {FlatButton} from 'material-ui'
+import {FlatButton, Chip} from 'material-ui'
 import ActionSearch from 'material-ui/svg-icons/action/search'
 import ToggleStar from 'material-ui/svg-icons/toggle/star-border'
 import FilledStar from 'material-ui/svg-icons/toggle/star'
 
 import { FormInput, FormSelect } from 'components/modal/parts'
+import { chipStyles } from 'style/materialStyles'
 
 const emptySearch = {label: 'None', value: ''}
-export default class SearchFormView extends React.Component {
+export default class
+SearchFormView extends React.Component {
   render () {
     const { onSearchKeyDown,
       dateOptions,
@@ -19,7 +21,8 @@ export default class SearchFormView extends React.Component {
       searchOptions,
       onChangeSearchOption,
       workflow,
-      onClickWorkflow
+      onClickWorkflow,
+      onClearWorkflow
     } = this.props
 
     const options = dateOptions.map((m, index) => {
@@ -35,8 +38,11 @@ export default class SearchFormView extends React.Component {
         <div className="text-center margin-md-top" >
           <Field name="query" component={FormInput} label="Search" onKeyDown={onSearchKeyDown} style={{verticalAlign: 'top'}}/>
           <Field name="dateIndex" component={FormSelect} label="" options={options} style={{verticalAlign: 'top'}}/>
-          <FlatButton label={`Workflow: ${workflow || 'All'}`} style={{marginTop: '4px', verticalAlign: 'top'}} onClick={onClickWorkflow}/>
-
+          <div className="inline-block">
+            <Chip style={chipStyles.chip} onTouchTap={onClickWorkflow} onRequestDelete={workflow ? onClearWorkflow : null}>
+              <b>Workflow:</b> {workflow || 'All'}
+            </Chip>
+          </div>
           <FlatButton type="submit" icon={<ActionSearch />} style={{marginTop: '4px', verticalAlign: 'top'}}/>
 
           <FlatButton icon={starFilled ? <FilledStar/> : <ToggleStar/>} style={{marginTop: '4px', verticalAlign: 'top'}} onClick={onClickStar}/>
