321b8617750e771dec723ef5cc368934b77bb630
nicodev-git <schaefernicker@gmail.com>
2017-04-27 22:52:43 +0800

    Added cpus

--- src/components/page/content/device/monitors/CpuTable.js
+++ src/components/page/content/device/monitors/CpuTable.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 class CpuTable extends React.Component {
+  componentWillMount () {
+    this.props.fetchMonitorCpu()
+  }
+  renderContent () {
+    const {monitorCpu} = this.props
+    if (!monitorCpu) return null
+    const cpus = monitorCpu.dataobj
+    return (cpus.length ? cpus : [cpus]).map((d, i) =>
+      <div key={i} className="inline-block">
+        <img src="/images/cpu.gif" width="80"/>
+        <div className="centered text-white" style={{marginTop: '-4px'}}>
+          {d.Usage}%
+        </div>
+      </div>
+    )
+  }
   render () {
     return (
-      <div></div>
+      <div className="inline-block">
+        {this.renderContent()}
+      </div>
     )
   }
 }
