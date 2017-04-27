1c5d7c07b46bcfde09ad064dfbdff89c91ee6f29
nicodev-git <schaefernicker@gmail.com>
2017-04-27 20:31:48 +0800

    Fixed disk table

--- src/components/page/content/device/monitors/DiskTable.js
+++ src/components/page/content/device/monitors/DiskTable.js
@@ -4,6 +4,20 @@ class DiskTable extends React.Component {
   componentWillMount () {
     this.props.fetchMonitorDisk()
   }
+  buildProgress (val, title, key) {
+    let color = 'red'
+    const textcolor = 'black'
+    if (val < 70) color = 'green'
+    else if (val < 90) color = '#fec835'
+
+    return (
+      <div key={key || title} className="progress" style={{height: '12px', position: 'relative'}} title={title || ''}>
+        <div className="progress-label" style={{fontSize: '9px', top: '1px', textAlign: 'center', position: 'absolute', width: '100%', color: textcolor}}>{val}%</div>
+        <div className="progress-bar progress-bar-info" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"
+            style={{width: `${val}%`, backgroundColor: color}}/>
+      </div>
+    )
+  }
   renderContent () {
     const {monitorDisk} = this.props
     if (!monitorDisk) return 'Loading...'
