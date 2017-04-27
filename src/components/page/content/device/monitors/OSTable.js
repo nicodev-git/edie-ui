684e5cbf41f67b787ed024fe2a94ced045aa4825
nicodev-git <schaefernicker@gmail.com>
2017-04-27 18:44:52 +0800

    Fixed os table

--- src/components/page/content/device/monitors/OSTable.js
+++ src/components/page/content/device/monitors/OSTable.js
@@ -4,10 +4,20 @@ class OSTable extends React.Component {
   componentWillMount () {
     this.props.fetchMonitorOS(this.props.device.id)
   }
-  render () {
+  renderContent () {
+    const {monitorOS} = this.props
+    if (!monitorOS) return 'Loading...'
     return (
       <div>
-        OS: {this.props.monitorOS}
+        <h4>OS: {monitorOS.dataobj.Name} {monitorOS.dataobj.ServicePack}</h4>
+        <h4>Version: {monitorOS.dataobj.Version}</h4>
+      </div>
+    )
+  }
+  render () {
+    return (
+      <div className="padding-md">
+        {this.renderContent()}
       </div>
     )
   }
