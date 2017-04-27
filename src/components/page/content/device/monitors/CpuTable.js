dfd03e455a3c96e8b60e200a79bdc8d768160914
nicodev-git <schaefernicker@gmail.com>
2017-04-27 23:33:58 +0800

    Fixed cpu tables

--- src/components/page/content/device/monitors/CpuTable.js
+++ src/components/page/content/device/monitors/CpuTable.js
@@ -4,15 +4,28 @@ class CpuTable extends React.Component {
   componentWillMount () {
     this.props.fetchMonitorCpu()
   }
+  buildProgress (val) {
+    let color = 'red'
+    const textcolor = 'black'
+    if (val < 70) color = 'green'
+    else if (val < 90) color = '#fec835'
+
+    return (
+      <div className="progress" style={{height: '12px', position: 'relative'}}>
+        <div className="progress-label" style={{fontSize: '9px', top: '1px', textAlign: 'center', position: 'absolute', width: '100%', color: textcolor}}>{val}%</div>
+        <div className="progress-bar progress-bar-info" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style={{width: `${val}%`, backgroundColor: color}}/>
+      </div>
+    )
+  }
   renderContent () {
     const {monitorCpu} = this.props
     if (!monitorCpu) return null
     const cpus = monitorCpu.dataobj
     return (cpus.length ? cpus : [cpus]).map((d, i) =>
-      <div key={i} className="inline-block">
+      <div key={i} className="inline-block valign-top">
         <img src="/images/cpu.gif" width="80"/>
-        <div className="centered text-white" style={{marginTop: '-4px'}}>
-          {d.Usage}%
+        <div className="inline">
+          {this.buildProgress(d.Usage)}
         </div>
       </div>
     )
