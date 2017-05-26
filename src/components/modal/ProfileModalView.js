c0473caf933f1e5f6c8af680994e837ce72095c6
nicodev-git <schaefernicker@gmail.com>
2017-05-26 12:46:40 +0800

    Fixed multi select

--- src/components/modal/ProfileModalView.js
+++ src/components/modal/ProfileModalView.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react'
 import Modal from 'react-bootstrap-modal'
 import { Field } from 'redux-form'
-import { Header, SubmitBlock, ProfileImageUpload, FormInput, FormSelect, CheckboxItem } from './parts'
+import { Header, SubmitBlock, ProfileImageUpload, FormInput, FormMultiSelect, CheckboxItem } from './parts'
 
 export default class ProfileModalView extends Component {
   render () {
     const {show, imgSrc, onHide, onSubmit, onChangeImage, mapOptions, roleOptions,
       defaultChecked, checkboxLabel} = this.props
     return (
-      <Modal
-        show={show}
-        onHide={onHide}
-        aria-labelledby="ModalHeader"
-        className="bootstrap-dialog type-primary"
-      >
+      <Modal show={show} onHide={onHide} aria-labelledby="ModalHeader" className="bootstrap-dialog type-primary">
         <Header name="Profile" />
         <div className="modal-body bootstrap-dialog-message">
           <form onSubmit={onSubmit}>
@@ -21,11 +16,10 @@ export default class ProfileModalView extends Component {
             <div className="form-column">
               <Field name="username" component={FormInput} label="User Name"/>
               <Field name="fullname" component={FormInput} label="Full Name"/>
-              <Field name="password" component={FormInput} type="password" label="Password"/>
               <Field name="email" component={FormInput} label="Email"/>
               <Field name="phone" component={FormInput} label="Phone"/>
-              <Field name="map" component={FormSelect} label="Default Map" options={mapOptions}/>
-              <Field name="role" component={FormSelect} label="Role" options={roleOptions}/>
+              <Field name="map" component={FormMultiSelect} label="Default Map" options={mapOptions} multiple/>
+              <Field name="role" component={FormMultiSelect} label="Role" options={roleOptions} multiple/>
               <CheckboxItem label={checkboxLabel} disabled defaultChecked={defaultChecked}/>
             </div>
             <SubmitBlock name="Save" onClick={onHide}/>
