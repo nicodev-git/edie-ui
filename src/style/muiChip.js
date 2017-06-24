b9088f5730060348497ca9bf3567cd37bc7ea6a0
nicodev-git <schaefernicker@gmail.com>
2017-06-24 16:06:33 +0800

    Fixed styles

---
+++ src/style/muiChip.js
@@ -0,0 +1,16 @@
+
+
+export const chipStyles = {
+  chip: {
+    margin: 4
+  },
+  label: {
+    fontSize: '12px'
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    maxHeight: '300px',
+    overflow: 'auto'
+  }
+}
