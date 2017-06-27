44d42c1e35f19e31c1a0428f24f967946234667e
nicodev-git <schaefernicker@gmail.com>
2017-06-27 23:15:03 +0800

    Removed uploader

--- ui/src/components/dashboard/map/toolbar/OptionUploader.js
+++ ui/src/components/dashboard/map/toolbar/OptionUploader.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 const OptionUploader = () => (
   <li>
-    <img className="option uploader" src="/images/uploading.gif"
-      style={{float: 'left', width: '18px', display: 'none', opacity: 0.5, marginLeft: '10px'}}/>
+    &nbsp;
   </li>
 )
 
