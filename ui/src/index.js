cc9441df2fc1f0dcb141f98fd28379060443e7a0
nicodev-git <schaefernicker@gmail.com>
2017-07-03 17:45:50 +0800

    Updated tags

--- ui/src/index.js
+++ ui/src/index.js
@@ -13,7 +13,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 0.15.4')
+console.log('Version: 0.15.5')
 
 injectTapEventPlugin()
 ReactDOM.render(
