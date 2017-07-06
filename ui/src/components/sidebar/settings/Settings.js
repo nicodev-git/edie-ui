8cb4ea412190716bc8860d9b4bff7c47619a60d3
nicodev-git <schaefernicker@gmail.com>
2017-07-06 11:51:45 +0800

    Fixed settings

--- ui/src/components/sidebar/settings/Settings.js
+++ ui/src/components/sidebar/settings/Settings.js
@@ -9,7 +9,6 @@ import SettingIdentitiesContainer from 'containers/settings/identity/IdentitiesC
 import SettingAdvancedContainer from 'containers/settings/advanced/AdvancedContainer'
 import SettingTags from 'components/sidebar/settings/tag/TagRoutes'
 import SettingUsers from 'components/sidebar/settings/users/UserRoutes'
-import SettingCreds from 'containers/settings/settings/credentials/CredentialsContainer'
 
 class Settings extends React.Component {
   render () {
@@ -21,7 +20,6 @@ class Settings extends React.Component {
         <Route path="/settings/maps" component={SettingMapsContainer} />
         <Route path="/settings/users" component={SettingUsers}/>
         <Route path="/settings/identities" component={SettingIdentitiesContainer} />
-        <Route path="/settings/creds" component={SettingCreds} />
         <Route path="/settings/advanced" component={SettingAdvancedContainer} />
       </Switch>
     )
