21b61720d5a479e48364c22d5b97b295f7fb27f3
nicodev-git <schaefernicker@gmail.com>
2017-07-07 00:10:43 +0800

    Fixed map zoom

--- ui/src/index.js
+++ ui/src/index.js
@@ -13,7 +13,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 0.15.13')
+console.log('Version: 0.15.14')
 
 injectTapEventPlugin()
 ReactDOM.render(
