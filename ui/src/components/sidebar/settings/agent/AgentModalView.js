45ca19012610ba6cfa4c16759cf9af20261cb1b6
nicodev-git <schaefernicker@gmail.com>
2017-07-07 22:13:17 +0800

    Fixed agents

---
+++ ui/src/components/sidebar/settings/agent/AgentModalView.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Dialog} from 'material-ui'
+import { Field } from 'redux-form'
+import { SubmitBlock, FormInput } from 'components/modal/parts'
+
+export default class AgentModalView extends React.Component {
+  render () {
+    const {onHide, onSubmit} = this.props
+    return (
+      <Dialog open title="Collector" onRequestClose={onHide} contentStyle={{width: 350}}>
+        <form onSubmit={onSubmit}>
+          <div className="form-column">
+            <Field name="name" component={FormInput} label="Name"/>
+            <Field name="version" component={FormInput} label="Version"/>
+          </div>
+          <SubmitBlock name="Save" onClick={onHide}/>
+        </form>
+      </Dialog>
+    )
+  }
+}
