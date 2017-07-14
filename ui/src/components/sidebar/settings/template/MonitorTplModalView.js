d57aabe14c8d8934320229abb06d7f2b76083b74
nicodev-git <schaefernicker@gmail.com>
2017-07-14 19:08:43 +0800

    Fixed monitor tpl

--- ui/src/components/sidebar/settings/template/MonitorTplModalView.js
+++ ui/src/components/sidebar/settings/template/MonitorTplModalView.js
@@ -20,6 +20,7 @@ export default class MonitorTplModalView extends Component {
             <Field name="description" component={FormInput} label="Description"/>
             <Field name="monitortype" component={FormInput} label="Monitor type"/>
             <Field name="enabled" component={FormCheckbox} label="Enabled" labelPosition="right"/>
+            <Field name="needWindowsAgentCollector" component={FormCheckbox} label="Need Windows" labelPosition="right" disabled/>
 
             <div className="padding-sm-top padding-sm-bottom">
               <ImageUploader imgUrl={imgUrl} onChange={onChange}/>
