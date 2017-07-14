234aa95dde72508456b677d38fcc2fc684570dba
nicodev-git <schaefernicker@gmail.com>
2017-07-14 20:04:01 +0800

    Fixed style

--- ui/src/components/sidebar/settings/template/MonitorTplModalView.js
+++ ui/src/components/sidebar/settings/template/MonitorTplModalView.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import {Dialog, RaisedButton, Chip} from 'material-ui'
+import {Dialog, Chip, Card, CardText} from 'material-ui'
 import { Field } from 'redux-form'
 import { SubmitBlock, FormInput, ImageUploader, FormCheckbox } from 'components/modal/parts'
 
 import { chipStyles } from 'style/common/materialStyles'
 
 const contentStyle = {
-  width: 570
+  width: 620
 }
 export default class MonitorTplModalView extends Component {
   render () {
@@ -18,28 +18,45 @@ export default class MonitorTplModalView extends Component {
     return (
       <Dialog open title={header} onRequestClose={onHide} contentStyle={contentStyle}>
         <form onSubmit={onSubmit}>
-          <Field name="name" component={FormInput} label="Name"/>&nbsp;
-          <Field name="description" component={FormInput} label="Description"/>&nbsp;
-          <Field name="monitortype" component={FormInput} label="Monitor type"/>&nbsp;
+          <div>Detail</div>
+          <Card>
+            <CardText>
+              <Field name="name" component={FormInput} label="Name"/>&nbsp;
+              <Field name="description" component={FormInput} label="Description"/>&nbsp;
+              <Field name="monitortype" component={FormInput} label="Monitor type"/>&nbsp;
 
-          <div className="pull-right">
-            <ImageUploader imgUrl={imgUrl} onChange={onChange}/>
-          </div>
+              <div className="pull-right">
+                <ImageUploader imgUrl={imgUrl} onChange={onChange}/>
+              </div>
 
-          <Field name="enabled" component={FormCheckbox} label="Enabled" labelPosition="right"/>
-          <div style={chipStyles.wrapper}>
-            {tags.map((t, i) =>
-              <Chip key={i} style={chipStyles.chip} onRequestDelete={() => onClickDeleteTag(i)}>{t}</Chip>
-            )}
-            <Chip style={chipStyles.chip} onTouchTap={onClickAddTag}>Add Tag</Chip>
-          </div>
-          <div style={chipStyles.wrapper}>
-            {monitorTplCredTypes.map((t, i) =>
-              <Chip key={i} style={chipStyles.chip} onRequestDelete={() => onClickDeleteCredType(i)}>{t}</Chip>
-            )}
-            <Chip style={chipStyles.chip} onTouchTap={onClickAddCredType}>Add Credential Type</Chip>
-          </div>
+              <Field name="enabled" component={FormCheckbox} label="Enabled" labelPosition="right"/>
+            </CardText>
+          </Card>
 
+          <div className="margin-md-top">Tag</div>
+          <Card>
+            <CardText>
+              <div style={chipStyles.wrapper}>
+                {tags.map((t, i) =>
+                  <Chip key={i} style={chipStyles.chip} onRequestDelete={() => onClickDeleteTag(i)}>{t}</Chip>
+                )}
+                <Chip style={chipStyles.chip} onTouchTap={onClickAddTag}>Add Tag</Chip>
+              </div>
+            </CardText>
+          </Card>
+
+          <div className="margin-md-top">Credential Type</div>
+          <Card>
+            <CardText>
+              <div style={chipStyles.wrapper}>
+                {monitorTplCredTypes.map((t, i) =>
+                  <Chip key={i} style={chipStyles.chip} onRequestDelete={() => onClickDeleteCredType(i)}>{t}</Chip>
+                )}
+                <Chip style={chipStyles.chip} onTouchTap={onClickAddCredType}>Add Credential Type</Chip>
+              </div>
+            </CardText>
+          </Card>
+          <br/>
           {tagModal}
           {credTypeModal}
           <SubmitBlock name="Save" onClick={onHide}/>
