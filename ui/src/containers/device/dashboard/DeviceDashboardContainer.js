e08c9e12aa4147cc56c22bbb434e495121e3d176
nicodev-git <schaefernicker@gmail.com>
2017-07-21 16:57:14 +0800

    Fixed dashboard

--- ui/src/containers/device/dashboard/DeviceDashboardContainer.js
+++ ui/src/containers/device/dashboard/DeviceDashboardContainer.js
@@ -15,12 +15,9 @@ export default connect(
     device: state.dashboard.selectedDevice,
 
     incidents: [],
-    serviceUsage: [],
-    topService: [],
-    servicePerformance: {
-      labels: [],
-      values: []
-    }
+    serviceUsage: [{"label":"08:47 - 09:47","value":0},{"label":"09:47 - 10:47","value":0},{"label":"10:47 - 11:47","value":0}],
+    topService: [{label: 'GetCount', value: 33}, {label: 'SelectList', value: 29}],
+    servicePerformance: {"labels":[''],"values":[{"label":"08:47 - 09:47","value":[10]},{"label":"09:47 - 10:47","value":[22]},{"label":"10:47 - 11:47","value":[9]}]}
   }), {
   }
 )(withRouter(DeviceDashboardContainer))
