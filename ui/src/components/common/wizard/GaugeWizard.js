aad375b34cbaa58f9b3200ff1d7fe592c8956096
nicodev-git <schaefernicker@gmail.com>
2017-07-22 22:45:04 +0800

    Added graph select

--- ui/src/components/common/wizard/GaugeWizard.js
+++ ui/src/components/common/wizard/GaugeWizard.js
@@ -21,28 +21,24 @@ class GaugeWizard extends React.Component {
     return []
   }
   handleFormSubmit (formProps) {
-    console.log(formProps)
-    // const { extraParams, onFinish, editParams, canAddTags, monitorTags } = this.props
-    // const { monitors, currentDevice } = this.state
-    // let params = {}
-    // if (editParams) {
-    //   editParams.forEach(p => {
-    //     params[p.key] = p.value
-    //   })
-    // }
-    //
-    // let props = assign(
-    //   {},
-    //   formProps,
-    //   currentDevice.server.params || {},
-    //   extraParams, {
-    //     monitors: monitors.map(m => assign({}, m, {id: null})),
-    //     params
-    //   }
-    // )
-    // if (canAddTags) props.tags = monitorTags || []
-    // console.log(props)
-    // this.closeModal(true)
+    const { extraParams, onFinish, editParams } = this.props
+    const { monitors } = this.state
+    let params = {}
+    if (editParams) {
+      editParams.forEach(p => {
+        params[p.key] = p.value
+      })
+    }
+
+    const props = assign(
+      {},
+      formProps,
+      extraParams, {
+        params
+      }
+    )
+    console.log(props)
+    this.closeModal(true)
     // onFinish && onFinish(null, props, currentDevice.server.url)
   }
 
--- ui/src/components/common/wizard/GaugeWizardView.js
+++ ui/src/components/common/wizard/GaugeWizardView.js
@@ -3,6 +3,14 @@ import {Dialog} from 'material-ui'
 import { Field } from 'redux-form'
 import { SubmitBlock, FormInput, FormSelect } from 'components/modal/parts'
 
+const graphs = [{
+  label: 'Line', value: 'line'
+}, {
+  label: 'Bar', value: 'bar'
+}, {
+  label: 'Pie', value: 'pie'
+}]
+
 export default class GaugeWizardView extends React.Component {
   render () {
     const {onSubmit, onHide, searchList} = this.props
@@ -11,6 +19,7 @@ export default class GaugeWizardView extends React.Component {
         <form onSubmit={onSubmit}>
           <Field name="name" component={FormInput} floatingLabel="Name" className="valign-top"/>
           <Field name="savedSearch" component={FormSelect} floatingLabel="Saved Search" options={searchList} className="valign-top"/>
+          <Field name="graph" component={FormSelect} floatingLabel="Saved Search" options={graphs} className="valign-top"/>
           <SubmitBlock name="Add" onClick={onHide} />
         </form>
       </Dialog>
