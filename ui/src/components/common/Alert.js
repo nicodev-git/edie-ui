19526c21c599fee433dcbf046cf510b342baa733
nicodev-git <schaefernicker@gmail.com>
2017-07-25 22:56:28 +0800

    Fixed alert

--- ui/src/components/common/Alert.js
+++ ui/src/components/common/Alert.js
@@ -53,7 +53,7 @@ export default class Alert extends Component {
   render () {
     return (
       <MuiThemeProvider>
-        <Dialog open title={this.props.title}>
+        <Dialog open title={this.props.title} contentStyle={{width: 350}}>
           <SubHeader name={this.props.message}/>
           <div className={`form-column ${this.props.type === TYPE_PROMPT ? '' : 'hidden'}`}>
             <TextField
