e7fd8525c4e4d655204c60c3a7dc7871f03c298f
nicodev-git <schaefernicker@gmail.com>
2017-07-27 12:13:35 +0800

    Fixed accel view

--- ui/src/components/dashboard/map/device/dashboard/display/AccelMeterView.js
+++ ui/src/components/dashboard/map/device/dashboard/display/AccelMeterView.js
@@ -2,20 +2,24 @@ import React from 'react'
 import AccelView from 'react-svg-gauge'
 import Dimen from 'react-dimensions'
 
-export default class AccelMeterView extends React.Component {
+class AccelMeterView extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      value: Math.random() * 100
+      value: parseInt(Math.random() * 100, 10)
     }
   }
   render () {
     return (
-      <AccelView
-        value={this.state.value}
-        width={this.props.containerWidth * 0.8}
-        height={this.props.containerHeight * 0.8}
-        label={this.props.title} />
+      <div className="padding-md">
+        <AccelView
+          value={this.state.value}
+          width={this.props.containerWidth - 32}
+          height={this.props.containerHeight - 50}
+          label=""
+        />
+        <div className="text-center">{this.props.title}</div>
+      </div>
     )
   }
 }
