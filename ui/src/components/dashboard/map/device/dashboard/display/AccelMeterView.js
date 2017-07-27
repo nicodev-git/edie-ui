1da911b86f3c79d6ba1aa519ff99dd8766dbcdf2
nicodev-git <schaefernicker@gmail.com>
2017-07-27 11:31:05 +0800

    Added accel view

--- ui/src/components/dashboard/map/device/dashboard/display/AccelMeterView.js
+++ ui/src/components/dashboard/map/device/dashboard/display/AccelMeterView.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import AccelView from 'react-svg-gauge'
+import Dimen from 'react-dimensions'
 
 export default class AccelMeterView extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: Math.random() * 100
+    }
+  }
   render () {
     return (
-      <div></div>
+      <AccelView
+        value={this.state.value}
+        width={this.props.containerWidth * 0.8}
+        height={this.props.containerHeight * 0.8}
+        label={this.props.title} />
     )
   }
 }
+
+export default Dimen()(AccelMeterView)
