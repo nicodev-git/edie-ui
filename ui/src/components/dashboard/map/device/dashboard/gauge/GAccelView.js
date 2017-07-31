40692e75a7a2af3e359fedbc20fa1ed26bf7dd79
nicodev-git <schaefernicker@gmail.com>
2017-07-31 12:39:12 +0800

    Added accel view

---
+++ ui/src/components/dashboard/map/device/dashboard/gauge/GAccelView.js
@@ -0,0 +1,84 @@
+import React from 'react'
+// import moment from 'moment'
+// import {findIndex} from 'lodash'
+// import axios from 'axios'
+
+import { ROOT_URL } from 'actions/config'
+import { dateFormat } from 'shared/Global'
+
+import FlipView from './FlipView'
+// import AccelMeterView from './display/AccelMeterView'
+import GEditView from './GEditView'
+
+import {showAlert} from 'components/common/Alert'
+
+export default class GAccelView extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+    }
+    this.renderBackView = this.renderBackView.bind(this)
+    this.renderFrontView = this.renderFrontView.bind(this)
+  }
+
+  onSubmit (options, values) {
+    console.log(values)
+
+    if (!values.name) {
+      showAlert('Please type name.')
+      return
+    }
+    const gauge = {
+      ...this.props.gauge,
+      ...values
+    }
+
+    this.props.updateDeviceGauge(gauge, this.props.device)
+    options.onClickFlip()
+  }
+
+  onClickDelete () {
+    this.props.removeDeviceGauge(this.props.gauge, this.props.device)
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  renderFrontView () {
+    const {gauge} = this.props
+
+    return (
+      <div className="flex-vertical flex-1">
+        <div className="flex-1">
+          Accel View: {gauge.name}
+        </div>
+      </div>
+    )
+  }
+  renderBackView (options) {
+    return (
+      <div>
+        <GEditView
+          searchList={this.props.searchList}
+          gauge={this.props.gauge}
+          monitors={this.props.monitors}
+          onSubmit={this.onSubmit.bind(this, options)}
+        />
+      </div>
+    )
+  }
+  render () {
+    return (
+      <FlipView
+        style={this.props.style}
+        className={this.props.className}
+        gauge={this.props.gauge}
+
+        loading={this.state.loading}
+        renderFrontView={this.renderFrontView}
+        renderBackView={this.renderBackView}
+
+        onClickDelete={this.onClickDelete.bind(this)}
+      />
+    )
+  }
+}
