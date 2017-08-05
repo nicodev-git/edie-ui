3f4e27fb0a97e9870b452fe33ba3ea67cd969e66
nicodev-git <schaefernicker@gmail.com>
2017-08-05 15:46:16 +0800

    Fixed disk

--- ui/src/components/common/gauge/GDisk.js
+++ ui/src/components/common/gauge/GDisk.js
@@ -1,33 +1,66 @@
 import React from 'react'
+import moment from 'moment'
+import axios from 'axios'
+import {findIndex} from 'lodash'
 
 import FlipView from './FlipView'
 import LiquidView from './display/LiquidView'
+import AccelView from './display/AccelMeterView'
 import GEditView from './GEditView'
+import LineChart from './display/LineChart'
+import BarChart from './display/BarChart'
 
 import MonitorSocket from 'util/socket/MonitorSocket'
 
 import {showAlert} from 'components/common/Alert'
+import { ROOT_URL } from 'actions/config'
+
+const sampleData = []
+
+const chartOptions = {
+  legend: {
+    display: false
+  },
+  elements: {
+    line: {
+      tension: 0
+    }
+  },
+  scales: {
+    yAxes: [{
+      display: true,
+      ticks: {
+        min: 0
+      }
+    }]
+  }
+}
 
 export default class GDisk extends React.Component {
   constructor (props) {
     super (props)
     this.state = {
       loading: false,
-      disk: null
+      disk: null,
+      searchRecordCounts: []
     }
     this.renderBackView = this.renderBackView.bind(this)
     this.renderFrontView = this.renderFrontView.bind(this)
   }
 
   componentDidMount () {
-    this.monitorSocket = new MonitorSocket({
-      listener: this.onMonitorMessage.bind(this)
-    })
-    this.monitorSocket.connect(this.onSocketOpen.bind(this))
+    if (this.props.gauge.timing === 'realtime') {
+      this.monitorSocket = new MonitorSocket({
+        listener: this.onMonitorMessage.bind(this)
+      })
+      this.monitorSocket.connect(this.onSocketOpen.bind(this))
+    } else {
+      this.fetchRecordCount(this.props)
+    }
   }
 
   componentWillUnmount () {
-    this.monitorSocket.close()
+    this.monitorSocket && this.monitorSocket.close()
   }
 
   onSocketOpen () {
@@ -44,7 +77,64 @@ export default class GDisk extends React.Component {
         this.setState({ disk: disk[0].Drives[0] })
     }
   }
+  fetchRecordCount (props) {
+    const {gauge, device, devices} = props
+    const {duration, durationUnit, splitUnit} = gauge
 
+    if (gauge.timing !== 'historic') return
+    let monitors = device.monitors || []
+    if (devices) {
+      const devIndex = findIndex(devices, {id: device.id})
+      if (devIndex < 0) {
+        console.log('Disk Device not found.')
+        return
+      }
+      monitors = devices[devIndex].monitors || []
+    }
+
+    const monitorIndex = findIndex(monitors || [], {monitortype: 'disk'})
+    if (monitorIndex < 0) {
+      console.log('Disk monitor not found.')
+      return
+    }
+    const monitorId = monitors[monitorIndex].uid
+
+    this.setState({
+      loading: true
+    })
+
+    let inc = 1
+    if (durationUnit === 'month' && splitUnit === 'day') inc = 0
+    const dateFrom = moment().add(-duration + inc, `${durationUnit}s`)
+      .startOf(durationUnit === 'hour' || duration === 1 ? durationUnit : 'day')
+    const dateTo = moment().endOf(durationUnit === 'hour' ? durationUnit : 'day')
+
+    axios.get(`${ROOT_URL}/event/search/findByDate`, {
+      params: {
+        dateFrom: dateFrom.valueOf(),
+        dateTo: dateTo.valueOf(),
+        monitorId,
+        sort: 'timestamp'
+      }
+    }).then(res => {
+      this.setState({
+        searchRecordCounts: res.data._embedded.events.map(p => {
+          const disk = p.dataobj
+          let value = 0
+          if (disk && disk.length && disk[0]) {
+            const drive = disk[0]
+            value = Math.ceil(drive.FreeSpace * 100 / drive.TotalSpace)
+          }
+
+          return {
+            date: moment(p.timestamp).format('YYYY-MM-DD HH:mm:ss'),
+            count: value
+          }
+        }),
+        loading: false
+      })
+    })
+  }
   onClickDelete () {
     this.props.removeDeviceGauge(this.props.gauge, this.props.device)
   }
@@ -65,18 +155,50 @@ export default class GDisk extends React.Component {
   }
 
   renderFrontView () {
-    const {disk} = this.state
-    const value = disk ? Math.ceil(disk.FreeSpace * 100 / disk.TotalSpace) : 0
-    return (
-      <div className="flex-1 flex-vertical">
-        <div className="flex-1">
-          <LiquidView value={value}/>
+    const {gauge} = this.props
+    if (gauge.timing === 'historic') {
+      const {searchRecordCounts} = this.state
+
+      const chartData = {
+        labels: (searchRecordCounts || sampleData).map(p => p.date),
+        datasets: [{
+          data: (searchRecordCounts || sampleData).map(p => p.count),
+          borderWidth: 1,
+          borderColor: '#269C8B',
+          fill: false
+        }]
+      }
+
+      return (
+        <div className="flex-vertical flex-1" style={{overflow: 'hidden'}}>
+          <div className="flex-1">
+            {gauge.gaugeType === 'bar' ? (
+              <BarChart chartData={chartData} chartOptions={chartOptions} />
+            ) : (
+              <LineChart chartData={chartData} chartOptions={chartOptions} />
+            )}
+
+          </div>
         </div>
-        <div className="text-center">
-          {`${disk ? `${disk.Name} ${disk.FreeSpace}G/${disk.TotalSpace}G` : ''}`}
+      )
+    } else {
+      const {disk} = this.state
+      const value = disk ? Math.ceil(disk.FreeSpace * 100 / disk.TotalSpace) : 0
+      return (
+        <div className="flex-vertical flex-1">
+          <div className="flex-1">
+            {gauge.gaugeType === 'accel' ? (
+              <AccelView value={value}/>
+            ) : (
+              <LiquidView value={value}/>
+            )}
+          </div>
+          <div className="text-center">
+            {`${disk ? `${disk.Name} ${disk.FreeSpace}G/${disk.TotalSpace}G` : ''}`}
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
   renderBackView (options) {
     return (
