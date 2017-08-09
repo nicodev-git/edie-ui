5603a006b26e994dbe3a181b81be78829858f7f4
nicodev-git <schaefernicker@gmail.com>
2017-08-09 14:56:32 +0800

    Fixed gauge view

--- ui/src/components/common/gauge/display/AccelMeterView.js
+++ ui/src/components/common/gauge/display/AccelMeterView.js
@@ -5,11 +5,11 @@ import Dimen from 'react-dimensions'
 class AccelMeterView extends React.Component {
   render () {
     return (
-      <div className="text-center">
+      <div className="text-center" style={{position: 'relative'}}>
         <AccelView
           value={this.props.value}
-          width={Math.min(this.props.containerWidth, this.props.containerHeight * 2) * 0.7}
-          height={this.props.containerHeight * 0.8}
+          width={this.props.containerWidth * 0.8}
+          height={this.props.containerHeight}
           label=""
         />
       </div>
