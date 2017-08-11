94414ec597dd440bb6d25f0417766d1e2d1b16a5
nicodev-git <schaefernicker@gmail.com>
2017-08-11 23:35:50 +0800

    Fix resizing

--- ui/src/components/dashboard/map/device/dashboard/DeviceDashboard.js
+++ ui/src/components/dashboard/map/device/dashboard/DeviceDashboard.js
@@ -354,7 +354,7 @@ export default class DeviceDashboard extends React.Component {
         <ResponsiveReactGridLayout
           className="layout" cols={cols} rowHeight={85}
           layouts={layouts}
-          isResizable={false} margin={[10, 10]}
+          margin={[10, 10]}
           style={{marginTop: -10}}
           onDragStop={this.onLayoutChange.bind(this)}>
           {gauges.map(p => this.renderGauge(p))}
