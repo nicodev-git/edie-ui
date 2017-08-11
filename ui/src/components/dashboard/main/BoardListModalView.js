6a4911b103f6c795c0f76c51c0b1b5810bd170fe
nicodev-git <schaefernicker@gmail.com>
2017-08-11 16:21:27 +0800

    Fixed board list

--- ui/src/components/dashboard/main/BoardListModalView.js
+++ ui/src/components/dashboard/main/BoardListModalView.js
@@ -1,28 +1,41 @@
 import React from 'react'
-import {Dialog} from 'material-ui'
-
-import { TwoButtonsBlockCustom } from 'components/modal/parts'
+import {Dialog, IconButton, FlatButton} from 'material-ui'
+import DeleteIcon from 'material-ui/svg-icons/action/delete'
+import EditIcon from 'material-ui/svg-icons/editor/mode-edit'
 
 export default class BoardListModalView extends React.Component {
   render () {
-    const {onHide, gaugeBoards} = this.props
+    const {onHide, gaugeBoards, onClickEdit, onClickDelete} = this.props
     return (
-      <Dialog open title="Dashboards" onRequestClose={onHide} contentStyle={{width: 600}}>
+      <Dialog open title="Dashboards" onRequestClose={onHide} contentStyle={{width: 400}}>
         <div style={{maxHeight: 300, overflow: 'auto'}}>
           <table className="table table-hover">
             <thead>
               <tr>
                 <th>Name</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
             {gaugeBoards.map(p =>
-              <tr><td>{p.name}</td></tr>
+              <tr key={p.id}>
+                <td>{p.name}</td>
+                <td className="text-right">
+                  <IconButton style={{padding: 0, width: 24, height: 24}} onTouchTap={() => onClickEdit(p)}>
+                    <EditIcon color="#545454" hoverColor="#f44336"/>
+                  </IconButton>
+                  <IconButton style={{padding: 0, width: 24, height: 24}} onTouchTap={() => onClickDelete(p)}>
+                    <DeleteIcon color="#545454" hoverColor="#f44336"/>
+                  </IconButton>
+                </td>
+              </tr>
             )}
             </tbody>
           </table>
         </div>
-        <TwoButtonsBlockCustom name1="OK" action1={onHide}/>
+        <div className="form-buttons close-block">
+          <FlatButton label="OK" onClick={onHide} primary/>
+        </div>
       </Dialog>
     )
   }
