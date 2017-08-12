ea95fbabf5a2703fd367aeefcc1c3f4fc1b6007c
nicodev-git <schaefernicker@gmail.com>
2017-08-12 13:14:24 +0800

    Fixed installed apps

--- ui/src/components/common/gauge/GInstallApp.js
+++ ui/src/components/common/gauge/GInstallApp.js
@@ -3,20 +3,51 @@ import moment from 'moment'
 import {findIndex} from 'lodash'
 
 import FlipView from './FlipView'
-import NormalTable from './display/NormalTable'
 import GEditView from './GEditView'
 
 import {showAlert} from 'components/common/Alert'
 import { dateFormat, severities } from 'shared/Global'
+import MonitorSocket from 'util/socket/MonitorSocket'
+import InfiniteTable from 'components/common/InfiniteTable'
 
 export default class GInstallApp extends React.Component {
   constructor (props) {
     super (props)
     this.state = {
-      value: parseInt(Math.random() * 100, 10)
+      apps: []
     }
     this.renderBackView = this.renderBackView.bind(this)
     this.renderFrontView = this.renderFrontView.bind(this)
+
+  }
+
+  componentDidMount () {
+    if (this.props.gauge.timing === 'realtime') {
+      this.monitorSocket = new MonitorSocket({
+        listener: this.onMonitorMessage.bind(this)
+      })
+      this.monitorSocket.connect(this.onSocketOpen.bind(this))
+    } else {
+      this.fetchRecordCount(this.props)
+    }
+  }
+
+  componentWillUnmount () {
+    this.monitorSocket && this.monitorSocket.close()
+  }
+
+  onSocketOpen () {
+    this.monitorSocket.send({
+      action: 'enable-realtime',
+      monitors: 'app',
+      deviceId: this.props.device.id
+    })
+  }
+  onMonitorMessage (msg) {
+    if (msg.action === 'update' && msg.deviceId === this.props.device.id) {
+      const {cpu} = msg.data
+      if (cpu) this.setState({ cpu })
+    }
   }
 
   onSubmit (options, values) {
@@ -38,12 +69,19 @@ export default class GInstallApp extends React.Component {
   onClickDelete () {
     this.props.removeDeviceGauge(this.props.gauge, this.props.device)
   }
-
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   renderFrontView () {
     return (
       <div className="flex-vertical flex-1">
+        <InfiniteTable
+          cells={this.columns}
+          ref="table"
+          rowMetadata={{'key': 'id'}}
+          selectable
+          data={this.props.apps}
+          useExternal={false}
+        />
       </div>
     )
   }
