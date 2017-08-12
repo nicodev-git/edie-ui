941aeaf643c71d8cdd09f17e3a0b7c244a26bb1e
nicodev-git <schaefernicker@gmail.com>
2017-08-12 17:46:26 +0800

    Fixed monitor

--- ui/src/components/common/gauge/display/MonitorStatusView.js
+++ ui/src/components/common/gauge/display/MonitorStatusView.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import moment from 'moment'
-
-const iconStyle = {
-  fontSize: '70px'
-}
-
-const smallIconStyle = {
-  fontSize: '20px'
-}
+import Dimen from 'react-dimensions'
 
 const containerStyle = {
   position: 'absolute',
@@ -17,27 +10,41 @@ const containerStyle = {
   width: '100%'
 }
 
-const labelStyle = {
-  fontSize: '20px'
-}
-
-const smallLabelStyle = {
-  fontSize: '12px'
-}
-
-export default class MonitorStatusView extends React.Component {
+class MonitorStatusView extends React.Component {
+  getSizeInfo () {
+    const w = this.props.containerWidth
+    const h = this.props.containerHeight
+    if (w >= 280 && h >= 170) {
+      return {
+        mt: 20,
+        icon: 60,
+        iconFont: '70px',
+        text: '20px'
+      }
+    }
+    return {
+      mt: 5,
+      icon: 20,
+      iconFont: '20px',
+      text: '12px'
+    }
+  }
   render () {
-    const {isUp, lastUpdate, hideLabel, title, size} = this.props
+    const {isUp, lastUpdate, hideLabel, title} = this.props
     const imgName = `/resources/images/dashboard/map/triangle-${isUp ? 'up' : 'down'}.png`
-    const isSmall = size === 'small'
+    const size = this.getSizeInfo()
     return (
       <div className="text-center" style={containerStyle}>
-        <div className={`text-ellipsis ${isUp ? 'text-success' : 'text-danger'}`} style={isSmall ? smallIconStyle : iconStyle}>
-          <img src={imgName} width={isSmall ? 20 : 60} alt="" className="valign-top" style={{marginTop: isSmall ? 5 : 20}}/>
+        <div className={`text-ellipsis ${isUp ? 'text-success' : 'text-danger'}`} style={{fontSize: size.iconFont}}>
+          <img src={imgName} width={size.icon} alt="" className="valign-top" style={{marginTop: size.mt}}/>
           <span className="margin-md-left">{title || (isUp ? 'Up' : 'Down')}</span>
         </div>
-        {!hideLabel && <div style={isSmall ? smallLabelStyle : labelStyle}>Last {isUp ? 'down' : 'up'} {lastUpdate ? moment(lastUpdate).fromNow() : 'never'}</div>}
+        {!hideLabel && <div style={{fontSize: size.text}}>Last {isUp ? 'down' : 'up'} {lastUpdate ? moment(lastUpdate).fromNow() : 'never'}</div>}
       </div>
     )
   }
 }
+
+export default Dimen({
+  elementResize: true
+})(MonitorStatusView)
