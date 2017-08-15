a0b9b7b8dd375441b1ea596188d62bfb13043865
nicodev-git <schaefernicker@gmail.com>
2017-08-15 18:09:07 +0800

    Fixed search

--- ui/src/components/sidebar/search/SearchSavePopover.js
+++ ui/src/components/sidebar/search/SearchSavePopover.js
@@ -6,6 +6,25 @@ import { Popover, RadioButtonGroup, RadioButton } from 'material-ui'
 import { FormInput, FormSelect, SubmitBlock } from 'components/modal/parts'
 
 class SearchSavePopover extends React.Component {
+  renderContent () {
+    const {searchSaveType} = this.props
+    switch(searchSaveType) {
+      case 'new':
+        return (
+          <div>
+            <Field name="name" component={FormInput} label="Name"/>
+            <SubmitBlock name="Done" onClick={onRequestClose}/>
+          </div>
+        )
+      case 'repalce':
+        return (
+          <div>
+            <Field name="searchId" component={FormSelect} label="Existing" options={userOptions.map(p => ({label: p.name, value: p.id}))}/>
+            <SubmitBlock name="Done" onClick={onRequestClose}/>
+          </div>
+        )
+    }
+  }
   render () {
     const { anchorEl, onRequestClose, onSubmit, handleSubmit, userOptions, searchSaveType, changeSearchSaveType } = this.props
     return (
@@ -22,6 +41,7 @@ class SearchSavePopover extends React.Component {
             <RadioButtonGroup name="saveType" defaultSelected="new" onChange={(e, value) => changeSearchSaveType(value)}>
               <RadioButton value="new" label="New" style={{display: 'inline-block', width: 'auto'}}/>
               <RadioButton value="replace" label="Replace" style={{display: 'inline-block', width: 'auto', marginLeft: 20}}/>
+              <RadioButton value="existing" label="Existing" style={{display: 'inline-block', width: 'auto', marginLeft: 20}}/>
             </RadioButtonGroup>
           </div>
           <div>
@@ -30,9 +50,9 @@ class SearchSavePopover extends React.Component {
             ) : (
               <Field name="searchId" component={FormSelect} label="Existing" options={userOptions.map(p => ({label: p.name, value: p.id}))}/>
             )}
-          </div>
 
-          <SubmitBlock name="Done" onClick={onRequestClose}/>
+            <SubmitBlock name="Done" onClick={onRequestClose}/>
+          </div>
         </form>
       </Popover>
     )
