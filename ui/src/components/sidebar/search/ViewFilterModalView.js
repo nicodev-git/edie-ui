4674601b1e072e747b820d0fc4cfa6f76238d688
nicodev-git <schaefernicker@gmail.com>
2017-08-17 14:41:59 +0800

    Fixed modals

--- ui/src/components/sidebar/search/ViewFilterModalView.js
+++ ui/src/components/sidebar/search/ViewFilterModalView.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Checkbox} from 'material-ui'
 import {keys} from 'lodash'
 
-import {TwoButtonsBlockCustom, Modal} from 'components/modal/parts'
+import {TwoButtonsBlockCustom, Modal, CardPanel} from 'components/modal/parts'
 
 import {Tabs, Tab} from 'material-ui/Tabs'
 import {viewFilters} from 'shared/Global'
@@ -12,42 +12,44 @@ export default class ViewFilterModalView extends React.Component {
     const {onClickOK, onClickClose, selectViewFilter, selectedViewFilter, cols, viewCols} = this.props
     return (
       <Modal title="View Filter" onRequestClose={onClickClose}>
-        <Tabs>
-          <Tab label="Predefined">
-            <table className="table table-hover">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Description</th>
-                </tr>
-              </thead>
-              <tbody>
-                {keys(viewFilters).map(k =>
-                  <tr
-                    key={k}
-                    onClick={() => selectViewFilter(viewFilters[k].name)}
-                    className={selectedViewFilter === viewFilters[k].name ? 'selected' : ''}>
-                    <td>{viewFilters[k].name}</td>
-                    <td>{viewFilters[k].desc}</td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          </Tab>
-          <Tab label="Specific">
-            <div style={{maxHeight: 300, overflow: 'auto'}}>
+        <CardPanel className="margin-md-bottom">
+          <Tabs>
+            <Tab label="Predefined">
               <table className="table table-hover">
-                <tbody>
-                {cols.map(k =>
-                  <tr key={k}>
-                    <td><Checkbox label={k} checked={viewCols.indexOf(k) >= 0} onCheck={() => this.props.toggleViewCol(k)}/></td>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Description</th>
                   </tr>
-                )}
+                </thead>
+                <tbody>
+                  {keys(viewFilters).map(k =>
+                    <tr
+                      key={k}
+                      onClick={() => selectViewFilter(viewFilters[k].name)}
+                      className={selectedViewFilter === viewFilters[k].name ? 'selected' : ''}>
+                      <td>{viewFilters[k].name}</td>
+                      <td>{viewFilters[k].desc}</td>
+                    </tr>
+                  )}
                 </tbody>
               </table>
-            </div>
-          </Tab>
-        </Tabs>
+            </Tab>
+            <Tab label="Specific">
+              <div style={{maxHeight: 300, overflow: 'auto'}}>
+                <table className="table table-hover">
+                  <tbody>
+                  {cols.map(k =>
+                    <tr key={k}>
+                      <td><Checkbox label={k} checked={viewCols.indexOf(k) >= 0} onCheck={() => this.props.toggleViewCol(k)}/></td>
+                    </tr>
+                  )}
+                  </tbody>
+                </table>
+              </div>
+            </Tab>
+          </Tabs>
+        </CardPanel>
         <TwoButtonsBlockCustom name1="OK" name2="Cancel" action1={onClickOK} action2={onClickClose}/>
       </Modal>
     )
--- ui/src/components/sidebar/search/WorkflowSelectModalView.js
+++ ui/src/components/sidebar/search/WorkflowSelectModalView.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {MenuItem, SelectField, TextField} from 'material-ui'
 
-import { TwoButtonsBlockCustom, Modal } from 'components/modal/parts'
+import { TwoButtonsBlockCustom, Modal, CardPanel } from 'components/modal/parts'
 import { errorStyle, underlineFocusStyle, inputStyle,
   selectedItemStyle, underlineStyle } from 'style/common/materialStyles'
 
@@ -17,59 +17,61 @@ export default class WorkflowSelectModalView extends React.Component {
     } = this.props
     return (
       <Modal title="Workflow" onRequestClose={onClickClose}>
-        <div className="padding-md-left">
-          <SelectField
-            errorStyle={errorStyle}
-            underlineStyle={underlineFocusStyle}
-            selectedMenuItemStyle={selectedItemStyle}
-            menuItemStyle={inputStyle}
-            labelStyle={inputStyle}
-            value={selectedCategory || ''}
-            onChange={onChangeCategory}
-          >
-            <MenuItem key="0" value="" primaryText="[All]" />
-            {workflowCategories.map(c =>
-              <MenuItem key={c.id} value={c.name} primaryText={c.name} />
-            )}
-          </SelectField>
-          <TextField
-            hintText="Search"
-            errorStyle={errorStyle}
-            inputStyle={inputStyle}
-            underlineFocusStyle={underlineStyle}
-            style={{marginLeft: '20px', verticalAlign: 'top'}}
-            value={workflowFilter}
-            onChange={onChangeWorkflowFilter}
-          />
-        </div>
-        <div style={{maxHeight: '250px', overflow: 'auto'}}>
-          <table className="table table-hover">
-            <thead>
-            <tr>
-              <th>Severity</th>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Version</th>
-            </tr>
-            </thead>
-            <tbody>
-            {
-              workflows.map(w =>
-                <tr
-                  key={w.id}
-                  className={selectedRowWf.id === w.id ? 'selected' : ''}
-                  onClick={() => onClickRow(w)}
-                >
-                  <td>{w.severity}</td>
-                  <td>{w.name}</td>
-                  <td>{w.desc}</td>
-                  <td>{w.version}</td>
-                </tr>
-              )
-            }
-            </tbody>
-          </table>
-        </div>
+        <CardPanel className="margin-md-bottom">
+          <div className="padding-md-left">
+            <SelectField
+              errorStyle={errorStyle}
+              underlineStyle={underlineFocusStyle}
+              selectedMenuItemStyle={selectedItemStyle}
+              menuItemStyle={inputStyle}
+              labelStyle={inputStyle}
+              value={selectedCategory || ''}
+              onChange={onChangeCategory}
+            >
+              <MenuItem key="0" value="" primaryText="[All]" />
+              {workflowCategories.map(c =>
+                <MenuItem key={c.id} value={c.name} primaryText={c.name} />
+              )}
+            </SelectField>
+            <TextField
+              hintText="Search"
+              errorStyle={errorStyle}
+              inputStyle={inputStyle}
+              underlineFocusStyle={underlineStyle}
+              style={{marginLeft: '20px', verticalAlign: 'top'}}
+              value={workflowFilter}
+              onChange={onChangeWorkflowFilter}
+            />
+          </div>
+          <div style={{maxHeight: '250px', overflow: 'auto'}}>
+            <table className="table table-hover">
+              <thead>
+              <tr>
+                <th>Severity</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Version</th>
+              </tr>
+              </thead>
+              <tbody>
+              {
+                workflows.map(w =>
+                  <tr
+                    key={w.id}
+                    className={selectedRowWf.id === w.id ? 'selected' : ''}
+                    onClick={() => onClickRow(w)}
+                  >
+                    <td>{w.severity}</td>
+                    <td>{w.name}</td>
+                    <td>{w.desc}</td>
+                    <td>{w.version}</td>
+                  </tr>
+                )
+              }
+              </tbody>
+            </table>
+          </div>
+        </CardPanel>
         <TwoButtonsBlockCustom name1="Cancel" action1={onClickClose} name2="OK" action2={onClickOK}/>
       </Modal>
     )
