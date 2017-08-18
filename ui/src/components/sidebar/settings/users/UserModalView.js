d0274272897561e311188fdc017f1dcc7edb4d05
nicodev-git <schaefernicker@gmail.com>
2017-08-18 16:11:49 +0800

    Fixed modals

--- ui/src/components/sidebar/settings/users/UserModalView.js
+++ ui/src/components/sidebar/settings/users/UserModalView.js
@@ -8,30 +8,31 @@ export default class UserModalView extends React.Component {
   render () {
     const {onHide, onSubmit, defaultmaps, roles, selectedRoles, onChangeRole} = this.props
     return (
-      <Modal title="User" onRequestClose={onHide}>
+      <Modal title="User" onRequestClose={onHide} contentStyle={{width: 685}}>
         <form onSubmit={onSubmit}>
-          <CardPanel className="margin-md-bottom">
-            <div className="form-column">
-              <Field name="username" component={FormInput} label="Name"/>
-              <Field name="fullname" component={FormInput} label="Full Name"/>
-              <Field name="password" type="password" component={FormInput} label="Password"/>
-              <Field name="email" component={FormInput} label="Email"/>
-              <Field name="phone" component={FormInput} label="Phone"/>
-              <Field name="defaultMapId" component={FormSelect} label="Default Map" options={defaultmaps}/>
-              <SelectField multiple hintText="Role" onChange={onChangeRole} value={selectedRoles}>
-                {roles.map(option =>
-                  <MenuItem
-                    key={option.value}
-                    insetChildren
-                    checked={selectedRoles.includes(option.value)}
-                    value={option.value}
-                    primaryText={option.label}
-                  />
-                )}
-              </SelectField>
+          <CardPanel title="User Settings" className="margin-md-bottom">
+            <Field name="username" component={FormInput} label="Name" className="mr-dialog"/>
+            <Field name="fullname" component={FormInput} label="Full Name"/>
 
-              <Field name="enabled" component={FormCheckbox} label="Enabled" />
-            </div>
+            <Field name="password" type="password" component={FormInput} label="Password" className="mr-dialog"/>
+            <Field name="email" component={FormInput} label="Email"/>
+
+            <Field name="phone" component={FormInput} label="Phone" className="mr-dialog"/>
+            <Field name="defaultMapId" component={FormSelect} label="Default Map" options={defaultmaps}/>
+
+            <SelectField multiple hintText="Role" onChange={onChangeRole} value={selectedRoles}>
+              {roles.map(option =>
+                <MenuItem
+                  key={option.value}
+                  insetChildren
+                  checked={selectedRoles.includes(option.value)}
+                  value={option.value}
+                  primaryText={option.label}
+                />
+              )}
+            </SelectField>
+
+            <Field name="enabled" component={FormCheckbox} label="Enabled" />
           </CardPanel>
           <SubmitBlock name="Save" onClick={onHide}/>
         </form>
