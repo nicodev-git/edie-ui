9d67ae35a972e8348082ed1240f071269421deb2
nicodev-git <schaefernicker@gmail.com>
2017-08-19 16:03:31 +0800

    Fixed monitor pick

--- ui/src/components/common/wizard/input/MonitorPickModal.js
+++ ui/src/components/common/wizard/input/MonitorPickModal.js
@@ -3,10 +3,6 @@ import React from 'react'
 import MonitorPickModalView from './MonitorPickModalView'
 
 export default class MonitorPickModal extends React.Component {
-  onHide () {
-
-  }
-
   render () {
     return (
       <MonitorPickModalView
--- ui/src/components/common/wizard/input/MonitorPickModalView.js
+++ ui/src/components/common/wizard/input/MonitorPickModalView.js
@@ -1,13 +1,29 @@
 import React from 'react'
-import { Modal, CardPanel } from 'components/modal/parts'
+
+import { Modal } from 'components/modal/parts'
+import { extImageBaseUrl } from 'shared/Global'
 
 export default class MonitorPickModalView extends React.Component {
+  renderTpl (tpl) {
+    return (
+      <li key={tpl.id} className="web-applet-card">
+        <div className="applet-card-body " style={{background: '#2468ff'}}>
+          <div className="padding-md">
+            <img src={`${extImageBaseUrl}${tpl.image}`} alt="" height="48"/><br/>
+            {tpl.name}
+          </div>
+        </div>
+      </li>
+    )
+  }
+
   render () {
     const {onHide} = this.props
     return (
-      <Modal title="Monitors" onRequestClose={onHide}>
-        <CardPanel className="margin-md-bottom">
-        </CardPanel>
+      <Modal title="Monitors" onRequestClose={onHide} contentStyle={{width: 1000, maxWidth: 'initial'}}>
+        <ul className="web-applet-cards">
+          {this.props.templates.map(this.renderTpl.bind(this))}
+        </ul>
       </Modal>
     )
   }
