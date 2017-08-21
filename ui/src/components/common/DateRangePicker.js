8bd13ff52450073b935383d44ceb47517ed2ab00
nicodev-git <schaefernicker@gmail.com>
2017-08-21 22:29:24 +0800

    Fixed date range

--- ui/src/components/common/DateRangePicker.js
+++ ui/src/components/common/DateRangePicker.js
@@ -17,68 +17,92 @@ const locale = {
   firstDay: moment.localeData().firstDayOfWeek()
 }
 
+export const getRanges = () => {
+  return {
+    'Ever': [
+      moment('2000-01-01'),
+      moment().endOf('year')
+    ],
+    [moment().add('-1', 'years').format('YYYY')]: [
+      moment().add('-1', 'years').startOf('year'),
+      moment().add('-1', 'years').endOf('year')
+    ],
+    [moment().startOf('years').format('YYYY')]: [
+      moment().startOf('year'),
+      moment().endOf('year')
+    ],
+    [moment().add('-1', 'months').format('MMMM')]: [
+      moment().add(-1, 'months').startOf('month'),
+      moment().add(-1, 'months').endOf('month')
+    ],
+    [moment().startOf('month').format('MMMM')]: [
+      moment().startOf('month'),
+      moment().endOf('month')
+    ],
+    'Last 30 Days': [
+      moment().add(-30, 'days').startOf('day'),
+      moment().endOf('day')
+    ],
+    'Last 7 Days': [
+      moment().add(-6, 'days').startOf('day'),
+      moment().endOf('day')
+    ],
+    'Last 3 Months' : [
+      moment().add(-3, 'months').startOf('day'),
+      moment().endOf('day')
+    ],
+    'Last 6 Months' : [
+      moment().add(-6, 'months').startOf('day'),
+      moment().endOf('day')
+    ],
+    'Last 1 Year' : [
+      moment().add(-12, 'months').startOf('day'),
+      moment().endOf('day')
+    ],
+    'Since Yesterday': [
+      moment().add(-1, 'days').startOf('day'),
+      moment().endOf('day')
+    ],
+    'Yesterday': [
+      moment().add(-1, 'days').startOf('day'),
+      moment().add(-1, 'days').endOf('day')
+    ],
+    'Today': [
+      moment().startOf('day'),
+      moment().endOf('day')
+    ],
+    'Last Hour': [
+      moment().add(-1, 'hours').startOf('minute'),
+      moment().endOf('minute')
+    ]
+  }
+}
+
+export const getRangeLabel = (rangeConfig, startDate, endDate) => {
+  const momentStartDate = isString(startDate) ? moment(startDate, dateFormat) : (isNumber(startDate) ? moment(startDate) : startDate)
+  const momentEndDate = isString(endDate) ? moment(endDate, dateFormat) : (isNumber(endDate) ? moment(endDate) : endDate)
+
+  const startDateStr = momentStartDate.format(dateFormat)
+  const endDateStr = momentEndDate.format(dateFormat)
+
+  let label = ''
+
+  keys(rangeConfig).forEach(key => {
+    if (rangeConfig[key][0].format(dateFormat) === startDateStr &&
+      rangeConfig[key][1].format(dateFormat) === endDateStr) {
+      label = key
+    }
+  })
+
+  if (!label) label = `${startDateStr} - ${endDateStr}`
+  return {label, momentStartDate, momentEndDate}
+}
+
 export default class DateRangePicker extends React.Component {
   constructor (props) {
     super(props)
 
-    let rangeConfig = {
-      'Ever': [
-        moment('2000-01-01'),
-        moment().endOf('year')
-      ],
-      [moment().add('-1', 'years').format('YYYY')]: [
-        moment().add('-1', 'years').startOf('year'),
-        moment().add('-1', 'years').endOf('year')
-      ],
-      [moment().startOf('years').format('YYYY')]: [
-        moment().startOf('year'),
-        moment().endOf('year')
-      ],
-      [moment().add('-1', 'months').format('MMMM')]: [
-        moment().add(-1, 'months').startOf('month'),
-        moment().add(-1, 'months').endOf('month')
-      ],
-      [moment().startOf('month').format('MMMM')]: [
-        moment().startOf('month'),
-        moment().endOf('month')
-      ],
-      'Last 30 Days': [
-        moment().add(-30, 'days').startOf('day'),
-        moment().endOf('day')
-      ],
-      'Last 7 Days': [
-        moment().add(-6, 'days').startOf('day'),
-        moment().endOf('day')
-      ],
-      'Last 3 Months' : [
-        moment().add(-3, 'months').startOf('day'),
-        moment().endOf('day')
-      ],
-      'Last 6 Months' : [
-        moment().add(-6, 'months').startOf('day'),
-        moment().endOf('day')
-      ],
-      'Last 1 Year' : [
-        moment().add(-12, 'months').startOf('day'),
-        moment().endOf('day')
-      ],
-      'Since Yesterday': [
-        moment().add(-1, 'days').startOf('day'),
-        moment().endOf('day')
-      ],
-      'Yesterday': [
-        moment().add(-1, 'days').startOf('day'),
-        moment().add(-1, 'days').endOf('day')
-      ],
-      'Today': [
-        moment().startOf('day'),
-        moment().endOf('day')
-      ],
-      'Last Hour': [
-        moment().add(-1, 'hours').startOf('minute'),
-        moment().endOf('minute')
-      ]
-    }
+    const rangeConfig = getRanges()
     this.state = {
       rangeConfig
     }
@@ -92,22 +116,9 @@ export default class DateRangePicker extends React.Component {
     const {rangeConfig} = this.state
     let { className, startDate, endDate, children, renderer, style } = this.props
 
-    const momentStartDate = isString(startDate) ? moment(startDate, dateFormat) : (isNumber(startDate) ? moment(startDate) : startDate)
-    const momentEndDate = isString(endDate) ? moment(endDate, dateFormat) : (isNumber(endDate) ? moment(endDate) : endDate)
+    const {label, momentStartDate, momentEndDate} = getRangeLabel(rangeConfig, startDate, endDate)
 
-    const startDateStr = momentStartDate.format(dateFormat)
-    const endDateStr = momentEndDate.format(dateFormat)
 
-    let label = ''
-
-    keys(rangeConfig).forEach(key => {
-      if (rangeConfig[key][0].format(dateFormat) === startDateStr &&
-        rangeConfig[key][1].format(dateFormat) === endDateStr) {
-        label = key
-      }
-    })
-
-    if (!label) label = `${startDateStr} - ${endDateStr}`
     return (
       <ReactDateRangePicker
         timePicker
