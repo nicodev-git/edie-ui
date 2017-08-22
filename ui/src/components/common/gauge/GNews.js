8d0f9f7831265e1588ddc4b3080286f1a3a7805d
nicodev-git <schaefernicker@gmail.com>
2017-08-22 23:35:16 +0800

    Fixed news widget

--- ui/src/components/common/gauge/GNews.js
+++ ui/src/components/common/gauge/GNews.js
@@ -15,14 +15,10 @@ export default class GNews extends React.Component {
   constructor (props) {
     super (props)
     this.state = {
-      value: parseInt(Math.random() * 100, 10),
-      draw: 1,
-      hover: false
+      articles: []
     }
     this.renderBackView = this.renderBackView.bind(this)
     this.renderFrontView = this.renderFrontView.bind(this)
-    this.onMouseEnter = this.onMouseEnter.bind(this)
-    this.onMouseOut = this.onMouseOut.bind(this)
   }
 
   onSubmit (options, values) {
@@ -45,104 +41,14 @@ export default class GNews extends React.Component {
     this.props.removeDeviceGauge(this.props.gauge, this.props.device)
   }
 
-  getSearchData () {
-    const {searchList, gauge} = this.props
-    const {resource, duration, durationUnit, workflowId, savedSearchId} = gauge
-
-    const dateFrom = moment().add(-duration, `${durationUnit}s`).startOf(durationUnit).format(dateFormat)
-    const dateTo = moment().endOf(durationUnit).format(dateFormat)
-
-    if (resource === 'incident')  {
-      const searchParams = {
-        draw: this.state.draw,
-        query: '',
-        workflow: workflowId || '',
-        collections: 'incident,event',
-        dateFrom,
-        dateTo,
-        severity: severities.map(p => p.value).join(','),
-        tag: '',
-        monitorTypes: ''
-      }
-      return {
-        searchParams
-      }
-    }
-
-    const index = findIndex(searchList, {id: savedSearchId})
-    if (index < 0) {
-      console.log('Saved search not found.')
-      return {
-        searchParams: {
-          draw: this.state.draw,
-          dateFrom,
-          dateTo,
-          query: '',
-          workflow: '',
-          collections: 'incident',
-          severity: 'HIGH,MEDIUM,LOW,AUDIT',
-          tag: '',
-          monitorTypes: ''
-        }
-      }
-    }
-    const savedSearch = searchList[index]
-    const searchParams = JSON.parse(savedSearch.data)
-
-    return {
-      searchParams: {
-        ...searchParams,
-        draw: this.state.draw,
-        dateFrom,
-        dateTo
-      },
-      viewFilter: savedSearch.viewFilter,
-      viewCols: savedSearch.viewCols,
-      viewMode: gauge.tableViewMode
-    }
-  }
-
-  onClickRefresh () {
-    this.setState({
-      draw: this.state.draw + 1
-    })
-  }
-
-  onMouseEnter () {
-    this.setState({hover: true})
-  }
-
-  onMouseOut () {
-    this.setState({hover: false})
-  }
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  renderRefresh (data) {
-    const {hover} = this.state
-    if (data.viewFilter !== viewFilters.log.name || !hover) return null
 
-    return (
-      <div style={{zIndex: 3}}>
-        <div style={{position: 'absolute', right: 10, top: 4}}>
-          <IconButton onTouchTap={this.onClickRefresh.bind(this)}>
-            <RefreshIcon size={32}/>
-          </IconButton>
-        </div>
-      </div>
-    )
-  }
   renderFrontView () {
     const data = this.getSearchData()
 
     return (
-      <div className="flex-vertical flex-1 padding-sm" onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseOut}>
-        {this.renderRefresh(data)}
-        <NormalTable
-          {...this.props}
-          params={data.searchParams}
-          viewFilter={data.viewFilter || ''}
-          viewCols={data.viewCols || []}
-          viewMode={data.viewMode || 'json'}
-        />
+      <div className="flex-vertical flex-1">
+
       </div>
     )
   }
