744e4154991715d241a15a8b86d2a6a4d63a6111
nicodev-git <schaefernicker@gmail.com>
2017-08-28 15:25:10 +0800

    Fixed picker

--- ui/src/components/common/gauge/GaugePickerView.js
+++ ui/src/components/common/gauge/GaugePickerView.js
@@ -1,13 +1,28 @@
 import React from 'react'
+
 import { Modal } from 'components/modal/parts'
+import AppletCard from 'components/common/AppletCard'
+import { extImageBaseUrl, appletColors as colors } from 'shared/Global'
 
 export default class GaugePickerView extends React.Component {
+  renderTpl (tpl, i) {
+    return (
+      <AppletCard
+        key={tpl.id}
+        color={colors[i % colors.length]}
+        name={''}
+        desc={tpl.name}
+        img={`${extImageBaseUrl}${tpl.image}`}
+      />
+    )
+  }
+
   render () {
     const {onHide} = this.props
     return (
       <Modal title="Gauge" onRequestClose={onHide} contentStyle={{width: 1058, maxWidth: 'initial'}}>
         <ul className="web-applet-cards" style={{marginTop: 40}}>
-
+          {this.props.gauge.map(this.renderTpl.bind(this))}
         </ul>
       </Modal>
     )
