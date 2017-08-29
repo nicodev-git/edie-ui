4255f2b94fc4160f6b2b6b8ba72359ff1916bb34
nicodev-git <schaefernicker@gmail.com>
2017-08-29 17:56:22 +0800

    Fixed log

--- ui/src/components/dashboard/log/DetailLogModalView.js
+++ ui/src/components/dashboard/log/DetailLogModalView.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 import { Modal } from 'components/modal/parts'
 
 export default class DetailLogModalView extends React.Component {
+  onRefRow (ref) {
+    setTimeout(() => {
+      const node = ReactDOM.findDOMNode(ref)
+      node.scrollIntoView({behavior: 'smooth'})
+    }, 100)
+  }
   renderTable () {
     const list = this.props.items
     const {rowId} = this.props
@@ -10,7 +17,7 @@ export default class DetailLogModalView extends React.Component {
     return (
       <div className="bg-white">
         {list.map((row, index) =>
-          <div key={row.id} className={row.id === rowId ? 'highlight' : ''}>
+          <div key={row.id} className={row.id === rowId ? 'highlight' : ''} ref={row.id === rowId ? this.onRefRow.bind(this) : null}>
             <div dangerouslySetInnerHTML={{__html: row.entity && row.entity.dataobj ? row.entity.dataobj.line : ' '}}/>
           </div>
         )}
