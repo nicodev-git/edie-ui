cddb0dddee969f59bdf6467ee4fabefaf8efdc47
nicodev-git <schaefernicker@gmail.com>
2017-08-31 21:43:33 +0800

    Fixed gauge picker

--- ui/src/components/common/gauge/GaugePickerView.js
+++ ui/src/components/common/gauge/GaugePickerView.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Drawer, IconButton} from 'material-ui'
 
 import DashboardIcon from 'material-ui/svg-icons/action/dashboard'
+import DeviceIcon from 'material-ui/svg-icons/device/devices'
 import PlaylistIcon from 'material-ui/svg-icons/av/playlist-add-check'
 
 import { Modal } from 'components/modal/parts'
@@ -28,12 +29,18 @@ export default class GaugePickerView extends React.Component {
   render () {
     const {onHide, gauges} = this.props
     return (
-      <Modal title="Gauge" onRequestClose={onHide} contentStyle={{width: 1158, maxWidth: 'initial'}}>
-        <div style={{marginTop: 40}} className="flex-horizontal">
-          <div>
+      <Modal title="Gauge" onRequestClose={onHide} contentStyle={{width: 1058 + sidebarWidth, maxWidth: 'initial'}}>
+        <div style={{marginTop: 40, minHeight: 500}} className="flex-horizontal">
+          <div style={{width: sidebarWidth}}>
             <Drawer open width={sidebarWidth} containerStyle={{...sidebarStyle, left: 'initial', bottom: 0, top: 56, height: 'initial'}}>
               <div>
                 <IconButton><DashboardIcon color="#ffffff"/></IconButton>
+              </div>
+              <div>
+                <IconButton><PlaylistIcon color="#ffffff"/></IconButton>
+              </div>
+              <div>
+                <IconButton><DeviceIcon color="#ffffff"/></IconButton>
               </div>
             </Drawer>
           </div>
