73a145af3c2ae37d1b5a60403e13853c9e4fd5ed
nicodev-git <schaefernicker@gmail.com>
2017-09-04 17:22:39 +0800

    style change

--- ui/src/style/common/muiForm.js
+++ ui/src/style/common/muiForm.js
@@ -1,38 +1,38 @@
-import {errorColor, textColor, primeColor, backgroundColor, headerColor} from './muiColor'
-import {fontSize, smallFontSize} from './muiFont'
+import { errorColor, textColor, primeColor, backgroundColor, headerColor } from './muiColor'
+import { fontSize, smallFontSize } from './muiFont'
 
 export const errorStyle = {
-  color: errorColor
+    color: errorColor
 }
 
 export const inputStyle = {
-  color: textColor,
-  fontSize: fontSize
+    color: textColor,
+    fontSize: fontSize
 }
 
 export const smallInputStyle = {
-  color: textColor,
-  fontSize: smallFontSize
+    color: textColor,
+    fontSize: smallFontSize
 }
 
 export const underlineStyle = {
-  borderColor: primeColor
+    borderColor: primeColor
 }
 
 export const selectedItemStyle = {
-  color: primeColor
+    color: primeColor
 }
 
 export const headerStyle = {
-  backgroundColor: backgroundColor,
-  color: headerColor
+    backgroundColor: backgroundColor,
+    color: headerColor
 }
 
 export const subHeaderStyle = {
-  color: primeColor,
-  fontSize: fontSize,
-  // wordBreak: 'break-all',
-  paddingTop: 48
+    color: primeColor,
+    fontSize: fontSize,
+    // wordBreak: 'break-all',
+    paddingTop: 48
 }
 
 export const underlineFocusStyle = {
@@ -40,14 +40,14 @@ export const underlineFocusStyle = {
 }
 
 export const dialogBodyStyle = {
-  background: '#efefef',
-  padding: '8px 48px 48px',
-  overflowY: 'auto'
+    background: '#efefef',
+    padding: '0',
+    overflowY: 'auto'
 }
 export const dialogTitleStyle = {
-  background: '#324454',
-  color: 'white',
-  fontSize: 14,
-  padding: '12px 24px',
-  margin: 0
+    background: '#324454',
+    color: 'white',
+    fontSize: 14,
+    padding: '12px 24px',
+    margin: 0
 }
