00321d173ced078a5ff35ecd0be56dc0ed631e44
nicodev-git <schaefernicker@gmail.com>
2017-09-05 04:11:10 +0800

    fixed code style

--- ui/src/components/common/gauge/gaugeSubItem/Device.js
+++ ui/src/components/common/gauge/gaugeSubItem/Device.js
@@ -10,26 +10,25 @@ export default class Device extends React.Component {
         return ( <div className="padding-md-left padding-md-right">
         <div className="row">
           <div className="col-md-12">
-          <div style={{height: height || 450, overflow: 'auto', border: '1px solid gray'}}>
-          <table className={`table table-hover ${tableClass}`}>
-            <tbody>
-            <tr>
-              <td style={{textAlign:'center'}}><b>Devices</b></td>
-            </tr>
-            {(devices || []).map((p, i) =>
-              <tr key={p.id}>
-                <td
-                  className={selectedDevice && selectedDevice.id === p.id ? 'selected' : ''}
-                  >{p.name}</td>
-               
-              </tr>
-            )}
-            </tbody>
-          </table>
-        </div>
+            <div style={{height: height || 450, overflow: 'auto', border: '1px solid gray'}}>
+              <table className={`table table-hover ${tableClass}`}>
+                <tbody>
+                  <tr>
+                    <td style={{textAlign:'center'}}><b>Devices</b></td>
+                  </tr>
+                    {(devices || []).map((p, i) =>
+                      <tr key={p.id}>
+                        <td
+                          className={selectedDevice && selectedDevice.id === p.id ? 'selected' : ''}
+                          >{p.name}</td>               
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
         );
     }
 }
