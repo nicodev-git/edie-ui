ea5bf96f05fe3b6b8c17f32ad23763897c20a34e
nicodev-git <schaefernicker@gmail.com>
2017-09-05 01:04:08 +0800

    change device

--- ui/src/components/common/gauge/gaugeSubItem/Device.js
+++ ui/src/components/common/gauge/gaugeSubItem/Device.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Device extends React.Component {   
     render() {
         return (
-            <div>aa</div>
+            <div>device</div>
         );
     }
 }
