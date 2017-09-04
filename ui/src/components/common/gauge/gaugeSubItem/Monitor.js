7afe8dd777cbba998ff04f1559a5588764230f51
nicodev-git <schaefernicker@gmail.com>
2017-09-05 02:40:09 +0800

    change monitor component

--- ui/src/components/common/gauge/gaugeSubItem/Monitor.js
+++ ui/src/components/common/gauge/gaugeSubItem/Monitor.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import {TextField, SelectField, MenuItem, RaisedButton, Checkbox} from 'material-ui'
 
-export default class Monitor extends React.Component {    
+export default class Monitor extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            selectedItem: null,
+        };
+        this.onChange = this.onChange.bind(this);
+    }
+    onChange(event, key, payload) {
+        this.setState({selectedItem: payload});
+    }   
     render() {
         const {devices} = this.props        
+        const { selectedItem } = this.state;
         return (
          <div className="tabs-custom flex-vertical flex-1">
          <div className="padding-lg-left">
           <SelectField
-            floatingLabelText="Devices" value={'123'} 
-            className="valign-top">
+            floatingLabelText="Devices" 
+            value={selectedItem} 
+            className="valign-top"
+            onChange={this.onChange}
+          >
             {(devices || []).map(p =>
               <MenuItem key={p.id} value={p.id} primaryText={p.name}/>
             )}
