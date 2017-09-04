03c10ebd41d34931330288390df26eb2717b2c50
nicodev-git <schaefernicker@gmail.com>
2017-09-05 00:18:13 +0800

    created dashboard component

---
+++ ui/src/components/common/gauge/gaugeSubItem/Dashboard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import AppletCard from '../../AppletCard';
+import { Modal } from '../../../../components/modal/parts';
+
+import { extImageBaseUrl, appletColors as colors } from '../../../../shared/Global';
+
+export default class Dashboard extends React.Component {
+    renderTpl (tpl, i) {
+        const {onClickMenuItem} = this.props
+        return (
+          <AppletCard
+            key={tpl.id}
+            color={colors[i % colors.length]}
+            name={''}
+            desc={tpl.name}
+            img={`${extImageBaseUrl}${tpl.image}`}
+            onClick={() => onClickMenuItem(tpl)}
+          />
+        )
+      }
+    render() {
+        return (
+            <ul className="web-applet-cards">
+                {this.props.gauges.map(this.renderTpl.bind(this))}
+            </ul>
+        );
+    }
+}
