0a547d15bbcf8a1b57395ec93639781db5e06ee4
nicodev-git <schaefernicker@gmail.com>
2017-09-05 15:19:01 +0800

    Fixed style

--- ui/src/components/common/gauge/GaugePickerView.js
+++ ui/src/components/common/gauge/GaugePickerView.js
@@ -7,67 +7,69 @@ import PlaylistIcon from 'material-ui/svg-icons/av/playlist-add-check'
 import Dashboard from './gaugeSubItem/Dashboard'
 import Device from './gaugeSubItem/Device'
 import Monitor from './gaugeSubItem/Monitor'
-import { Modal } from 'components/modal/parts'
+import {Modal} from 'components/modal/parts'
 
-import { iconButtonStyle,sidebarWidth, sidebarStyle } from 'style/common/materialStyles'
+import {iconButtonStyle, sidebarWidth, sidebarStyle} from 'style/common/materialStyles'
 
 export default class GaugePickerView extends React.Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
     this.state = {
       selectedSubItem: 'dashboard',
-    };
+    }
   }
-  onClickSubItem(subItem) {
+
+  onClickSubItem (subItem) {
     this.setState({
       selectedSubItem: subItem,
-    });
+    })
   }
-  
+
 
   render () {
-    const {onHide, gauges} = this.props;
-    const { selectedSubItem } = this.state;
-    let subPage;
+    const {onHide, gauges} = this.props
+    const {selectedSubItem} = this.state
+    let subPage
     switch (selectedSubItem) {
-      case 'dashboard': 
+      case 'dashboard':
         subPage = (
           <Dashboard
             gauges={gauges}
             onClickMenuItem={this.props.onClickMenuItem}
           />
-        );
-        break;
+        )
+        break
       case 'monitor':
-      subPage = (
-        <Monitor
-          gauges={gauges}
-          devices={this.props.devices}
-        />
-      );
-        break;
+        subPage = (
+          <Monitor
+            gauges={gauges}
+            devices={this.props.devices}
+          />
+        )
+        break
       case 'device':
-      subPage = (
-        <Device
-          gauges={gauges}          
-          devices={this.props.devices}
-        />
-      );
-        break;
-     default:
+        subPage = (
+          <Device
+            gauges={gauges}
+            devices={this.props.devices}
+          />
+        )
+        break
+      default:
         subPage = (
           <Dashboard
             gauges={gauges}
             onClickMenuItem={this.props.onClickMenuItem}
           />
-        );
-        break;
+        )
+        break
     }
     return (
       <Modal title="Gauge" onRequestClose={onHide} contentStyle={{width: 1058 + sidebarWidth, maxWidth: 'initial'}}>
         <div style={{marginTop: 40, minHeight: 500}} className="flex-horizontal">
           <div style={{width: sidebarWidth}}>
-            <Drawer open width={sidebarWidth} containerStyle={{...sidebarStyle, left: 'initial', bottom: 0, top: 56, height: 'initial'}}>
+            <Drawer open width={sidebarWidth}
+                    containerStyle={{...sidebarStyle, bottom: 0, top: 56, height: 'initial'}}>
               <div>
                 <IconButton
                   tooltip="Dashboard"
