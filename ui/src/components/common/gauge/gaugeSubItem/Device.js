4b3b0c1fc6c44314901ff252956d9622579f31f6
nicodev-git <schaefernicker@gmail.com>
2017-09-05 21:36:17 +0800

    Fixed device add

--- ui/src/components/common/gauge/gaugeSubItem/Device.js
+++ ui/src/components/common/gauge/gaugeSubItem/Device.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import {IconButton} from 'material-ui'
+import AddCircleIcon from 'material-ui/svg-icons/content/add-circle'
+
 import AppletCard from 'components/common/AppletCard'
 import { extImageBaseUrl, appletColors as colors } from 'shared/Global'
 
 export default class Device extends React.Component {
+  onClickAddDevice () {
+
+  }
+
   renderTpl (tpl, i) {
     const {onClickMenuItem} = this.props
     return (
@@ -16,11 +23,23 @@ export default class Device extends React.Component {
       />
     )
   }
+  renderAddMenu () {
+    return (
+      <div className="text-right" style={{position: 'absolute', top: -45, right: 0}}>
+        <IconButton onTouchTap={this.onClickAddDevice.bind(this)}>
+          <AddCircleIcon />
+        </IconButton>
+      </div>
+    )
+  }
   render() {
     return (
-      <ul className="web-applet-cards">
-        {this.props.devices.map(this.renderTpl.bind(this))}
-      </ul>
+      <div>
+        {this.renderAddMenu()}
+        <ul className="web-applet-cards">
+          {this.props.devices.map(this.renderTpl.bind(this))}
+        </ul>
+      </div>
     )
   }
 }
