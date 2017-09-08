565f83677f5d8d9da7569644cb91644c7bbafc0d
nicodev-git <schaefernicker@gmail.com>
2017-09-08 14:13:29 +0800

    Fixed server detail page

--- ui/src/components/dashboard/serverdetail/ServerDetail.js
+++ ui/src/components/dashboard/serverdetail/ServerDetail.js
@@ -3,6 +3,16 @@ import {findIndex} from 'lodash'
 import { Switch } from 'react-router'
 import { Route } from 'react-router-dom'
 
+import MainControl from 'containers/dashboard/serverdetail/MainControlContainer'
+import EventLogs from 'containers/dashboard/serverdetail/EventLogsContainer'
+import Apps from 'containers/dashboard/serverdetail/AppsContainer'
+import Processes from 'containers/dashboard/serverdetail/ProcessContainer'
+import Services from 'containers/dashboard/serverdetail/ServiceContainer'
+import Users from 'containers/dashboard/serverdetail/UsersContainer'
+import Firewall from 'containers/dashboard/serverdetail/FirewallContainer'
+import Network from 'containers/dashboard/serverdetail/NetworkContainer'
+import Command from 'containers/dashboard/serverdetail/CommandContainer'
+
 import RefreshOverlay from 'components/common/RefreshOverlay'
 
 export default class ServerDetail extends React.Component {
@@ -24,19 +34,19 @@ export default class ServerDetail extends React.Component {
   render () {
     const device = this.getDevice()
     if (!device) return <RefreshOverlay/>
-    // return (
-    //   <Switch>
-    //     <Route path="/serverdetail/:id" exact component={DeviceMonitorsContainer}/>
-    //     <Route path="/serverdetail/:id/eventlog" component={DeviceEventLogs}/>
-    //     <Route path="/serverdetail/:id/app" component={DeviceApps}/>
-    //     <Route path="/serverdetail/:id/process" component={DeviceProcesses}/>
-    //     <Route path="/serverdetail/:id/service" component={DeviceServices}/>
-    //     <Route path="/serverdetail/:id/user" component={DeviceUsers}/>
-    //     <Route path="/serverdetail/:id/firewall" component={DeviceFirewall}/>
-    //     <Route path="/serverdetail/:id/network" component={DeviceNetwork}/>
-    //     <Route path="/serverdetail/:id/command" component={DeviceCommand}/>
-    //   </Switch>
-    // )
+    return (
+      <Switch>
+        <Route path="/serverdetail/:id" exact component={MainControl}/>
+        <Route path="/serverdetail/:id/eventlog" component={EventLogs}/>
+        <Route path="/serverdetail/:id/app" component={Apps}/>
+        <Route path="/serverdetail/:id/process" component={Processes}/>
+        <Route path="/serverdetail/:id/service" component={Services}/>
+        <Route path="/serverdetail/:id/user" component={Users}/>
+        <Route path="/serverdetail/:id/firewall" component={Firewall}/>
+        <Route path="/serverdetail/:id/network" component={Network}/>
+        <Route path="/serverdetail/:id/command" component={Command}/>
+      </Switch>
+    )
     return null
   }
 }
