baf480a0b5998212bf2a1d0382ba250e3cbeabb7
nicodev-git <schaefernicker@gmail.com>
2017-09-08 13:06:04 +0800

    Added tabs

--- ui/src/components/dashboard/serverdetail/ServerDetail.js
+++ ui/src/components/dashboard/serverdetail/ServerDetail.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import {findIndex} from 'lodash'
+
+import RefreshOverlay from 'components/common/RefreshOverlay'
 
 export default class ServerDetail extends React.Component {
+  getDeviceId () {
+    return this.props.match.params.id
+  }
+
+  getDevice () {
+    const {devices} = this.props
+    const index = findIndex(devices, {id: this.getDeviceId()})
+    if (index < 0) return null
+    return devices[index]
+  }
+
+  componentWillMount () {
+    this.props.fetchDevice(this.getDeviceId())
+  }
+
   render () {
+    const device = this.getDevice()
+    if (!device) return <RefreshOverlay/>
     return (
       <div>
         Detail page
---
+++ ui/src/components/dashboard/serverdetail/ServerDetailTab.js
@@ -0,0 +1,37 @@
+export default (deviceId, templateName) => {
+  const items = [{
+    title: 'Monitors',
+    path: `/serverdetail/${deviceId}`
+  }, {
+    title: 'Event Logs',
+    path: `/serverdetail/${deviceId}/eventlog`,
+    exclude: ['Linux Server']
+  }, {
+    title: 'Applications',
+    path: `/serverdetail/${deviceId}/app`,
+    exclude: ['Linux Server']
+  }, {
+    title: 'Processes',
+    path: `/serverdetail/${deviceId}/process`
+  }, {
+    title: 'Services',
+    path: `/serverdetail/${deviceId}/service`
+  }, {
+    title: 'Users',
+    path: `/serverdetail/${deviceId}/user`
+  }, {
+    title: 'Firewall',
+    path: `/serverdetail/${deviceId}/firewall`
+  }, {
+    title: 'Network',
+    path: `/serverdetail/${deviceId}/network`
+  }, {
+    title: 'Command',
+    path: `/serverdetail/${deviceId}/command`
+  }]
+
+  if (templateName) {
+    return items.filter(p => !p.exclude || p.exclude.indexOf(templateName) < 0)
+  }
+  return items
+}
