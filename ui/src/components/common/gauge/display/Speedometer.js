c781e7a9a3b73598b10e41b2fd4dc4cb9eed1852
nicodev-git <schaefernicker@gmail.com>
2017-09-11 15:18:33 +0800

    Fixed speedometer

--- ui/src/components/common/gauge/display/Speedometer.js
+++ ui/src/components/common/gauge/display/Speedometer.js
@@ -2,14 +2,21 @@ import React from 'react'
 import Dimen from 'react-dimensions'
 import D3Speedometer from 'react-d3-speedometer'
 
+const colorGreen = '#65CE67'
+const colorRed = '#EF5A5A'
+
 class Speedometer extends React.Component {
   render () {
     const {value, containerWidth, containerHeight} = this.props
     return (
       <D3Speedometer
-        minValue={0} maxValue={100} segments={1} value={value}
+        textColor="transparent"
+        minValue={0} maxValue={100} segments={2} value={value}
         width={containerWidth}
         height={containerHeight}
+        startColor={value > 50 ? colorRed : colorGreen}
+        endColor={value > 50 ? colorRed : colorGreen}
+        needleColor="#2B436E"
       />
     )
   }
