863b4b0c2ba2798c5ae1335e030ecec6714c7258
nicodev-git <schaefernicker@gmail.com>
2017-09-12 23:44:21 +0800

    Fixed agent picker

--- ui/src/components/common/wizard/input/AgentPicker.js
+++ ui/src/components/common/wizard/input/AgentPicker.js
@@ -6,8 +6,11 @@ import {RadioButtonGroup} from 'redux-form-material-ui'
 import {FormSelect} from 'components/modal/parts'
 
 export default class AgentPicker extends React.Component {
+  onClickInstall () {
+    this.props.installAgent(this.props.values)
+  }
   render () {
-    const {values, collectors} = this.props
+    const {values, collectors, installAgents} = this.props
     const {agent} = values
 
     const collectorOptions = collectors.map(p => ({
@@ -16,11 +19,20 @@ export default class AgentPicker extends React.Component {
 
     let agentLabel = 'Agent'
     if (!agent) {
+      let installAgent = installAgents.filter(a => a.id === values.id)
+      installAgent = installAgent.length ? installAgent[0] : null
+      const installing = installAgent && installAgent.status === 'installing'
+
       agentLabel = (
         <div>
           <div className="inline-block" style={{width: 100}}>Agent</div>
-          <div className="inline-block" style={{textDecoration: 'underline', color: 'rgba(0, 0, 0, 0.87)'}}>Install Agent</div>
-          <CircularProgress className="valign-top margin-md-left" size={24}/>
+          <div
+            className="inline-block"
+            style={{textDecoration: 'underline', color: 'rgba(0, 0, 0, 0.87)'}}
+            onClick={this.onClickInstall.bind(this)}>
+            {installing ? 'Installing...' : 'Install Agent'}
+          </div>
+          {installing ? <CircularProgress className="valign-top margin-md-left" size={24}/> : null}
         </div>
       )
     }
@@ -33,7 +45,7 @@ export default class AgentPicker extends React.Component {
       <div style={{minHeight: 140}}>
         <Field name="agentType" component={RadioButtonGroup} className="margin-md-top">
           <RadioButton value="" label="None" className="pull-left"/>
-          <RadioButton value="agent" label={agentLabel} className="pull-left" disabled={!agent} style={{marginTop: 14}}/>
+          <RadioButton value="agent" label={agentLabel} className="pull-left" disabled={!agent} style={{marginTop: 14, cursor: 'pointer'}}/>
           <RadioButton value="collector" label={collectorLabel} className="pull-left" style={{width: 120, marginTop: 14}}/>
         </Field>
 
