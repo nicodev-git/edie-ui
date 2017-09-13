67ab884f11e0e8b54011568ef8de4ebb5ce91990
nicodev-git <schaefernicker@gmail.com>
2017-09-13 16:50:06 +0800

    Fixed device add

--- ui/src/components/dashboard/main/SysDashboardView.js
+++ ui/src/components/dashboard/main/SysDashboardView.js
@@ -4,8 +4,21 @@ import AddCircleIcon from 'material-ui/svg-icons/content/add-circle'
 
 import AppletCard from 'components/common/AppletCard'
 import { extImageBaseUrl, appletColors as colors } from 'shared/Global'
+import { wizardConfig, getDeviceType } from 'components/common/wizard/WizardConfig'
+import DeviceWizardContainer from 'containers/shared/wizard/DeviceWizardContainer'
+
+import { showAlert } from 'components/common/Alert'
 
 export default class SysDashboardView extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      deviceWizardConfig: {},
+      deviceWizardVisible: false,
+    }
+  }
+
   getServers () {
     const {devices, allDevices} = this.props
     return (devices || allDevices).filter(p => (p.tags || []).includes('Server'))
@@ -22,7 +35,50 @@ export default class SysDashboardView extends React.Component {
 
   onClickAddItem (tpl) {
     console.log(tpl)
+
+    const options = {
+      title: tpl.name,
+      type: getDeviceType(tpl.name),
+      imgName: tpl.image,
+      imageUrl: `/externalpictures?name=${tpl.image}`,
+      x: 0,
+      y: 0,
+      width: 50,
+      height: 50,
+
+      tpl,
+      monitors: tpl.monitors,
+      templateName: tpl.name,
+      workflowids: tpl.workflowids || []
+    }
+
+    this.showAddWizard(options, (id, name, data) => {
+
+    }, () => {
+
+    })
   }
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  showAddWizard (options, callback, closeCallback) {
+    if (wizardConfig[options.type] === null) {
+      showAlert(`Unrecognized Type: ${options.type}`) // eslint-disable-line no-undef
+      return
+    }
+
+    this.setState({
+      deviceWizardConfig: {
+        options, callback, closeCallback
+      },
+      deviceWizardVisible: true
+    })
+  }
+
+  onFinishAddWizard (callback, res, params, url) {
+    console.log(params)
+    this.props.addDevice(params, url)
+  }
+
   /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   renderServer (server, i) {
@@ -54,7 +110,42 @@ export default class SysDashboardView extends React.Component {
       </div>
     )
   }
+  renderDeviceWizard () {
+    if (!this.state.deviceWizardVisible) return null
 
+    const {options, callback, closeCallback} = this.state.deviceWizardConfig
+
+    let extra = {
+      mapid: null,
+      x: options.x,
+      y: options.y,
+      width: options.width,
+      height: options.height,
+      image: options.imgName,
+      templateName: options.templateName,
+      workflowids: options.workflowids,
+      tags: options.tpl.tags || []
+    }
+
+    const config = {
+      mapid: null
+    }
+
+    return (
+      <DeviceWizardContainer
+        deviceType={options.type}
+        onClose={() => {
+          this.setState({deviceWizardVisible: false})
+          closeCallback && closeCallback()
+        }}
+        title={options.title}
+        monitors={options.monitors}
+        extraParams={extra}
+        configParams={config}
+        onFinish={this.onFinishAddWizard.bind(this, callback)}
+      />
+    )
+  }
   render () {
     return (
       <div>
@@ -62,6 +153,7 @@ export default class SysDashboardView extends React.Component {
         <ul className="web-applet-cards">
           {this.getServers().map(this.renderServer.bind(this))}
         </ul>
+        {this.renderDeviceWizard()}
       </div>
     )
   }
