3b14ac3613da60f7148e092d5e9b2bd75120c6bb
nicodev-git <schaefernicker@gmail.com>
2017-09-19 12:36:03 +0800

    Fixed style

--- ui/src/components/common/wizard/input/CredPicker.js
+++ ui/src/components/common/wizard/input/CredPicker.js
@@ -1,39 +1,11 @@
 import React from 'react'
-// import { Field } from 'redux-form'
 import { IconButton } from 'material-ui'
 import AddCircleIcon from 'material-ui/svg-icons/content/add-circle'
 import CloseIcon from 'material-ui/svg-icons/navigation/close'
-// import {FormSelect, FormInput} from 'components/modal/parts'
 
 import { CardPanel } from 'components/modal/parts'
 
 export default class CredPicker extends React.Component {
-  // render () {
-  //   const {credentials, credentialTypes, onChangeCredential} = this.props
-  //   const options = credentials.map(p => ({label: p.name, value: p.id}))
-  //   const typeOptions = credentialTypes.map(p => ({label: p.name, value: p.name}))
-  //   return (
-  //     <div className="flex-horizontal">
-  //       <div>
-  //         <RadioButtonGroup name="credentialSelect" defaultSelected="existing" onChange={(e, value) => onChangeCredential(value)}>
-  //           <RadioButton value="" label="None"/>
-  //           <RadioButton value="existing" label="Existing" style={{marginTop: 30}}/>
-  //           <RadioButton value="new" label="New" style={{marginTop: 30}} />
-  //         </RadioButtonGroup>
-  //       </div>
-  //       <div className="flex-1" style={{paddingLeft: 16, paddingTop: 36}}>
-  //         <div>
-  //           <Field name="credentialId" component={FormSelect} className="valign-top mr-dialog" options={options} floatingLabel="Credential" style={{marginTop: -20}}/>
-  //         </div>
-  //         <div style={{marginTop: -20}}>
-  //           <Field name="credtype" component={FormSelect} className="valign-top" floatingLabel="Type" style={{width: 150, marginRight: 12}} options={typeOptions}/>
-  //           <Field name="creduser" component={FormInput} className="valign-top mr-dialog" floatingLabel="User" style={{width: 150}}/>
-  //           <Field name="credpassword" component={FormInput} type="password" className="valign-top" floatingLabel="Password" style={{width: 150}}/>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   )
-  // }
   onClickAdd () {
     this.props.showDeviceCredsPicker(true)
   }
@@ -60,7 +32,7 @@ export default class CredPicker extends React.Component {
     const credentials = this.props.deviceCredentials
     return (
       <CardPanel title="Credentials" tools={this.renderButtons()}>
-        <div style={{maxHeight: 300, overflow: 'auto'}}>
+        <div style={{minHeight: 200, maxHeight: 300, overflow: 'auto'}}>
           <table className="table table-hover">
             <thead>
             <tr>
