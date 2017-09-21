9b809b28b3aa9b8c95ccdd4b609eeb28a2057f30
nicodev-git <schaefernicker@gmail.com>
2017-09-21 11:25:22 +0800

    Fixed tree view

--- ui/src/components/sidebar/search/SearchMonitorModalView.js
+++ ui/src/components/sidebar/search/SearchMonitorModalView.js
@@ -1,28 +1,55 @@
 import React from 'react'
 
+import SortableTree from 'react-sortable-tree'
+
 import {TwoButtonsBlockCustom, Modal, CardPanel} from 'components/modal/parts'
 
 export default class SearchMonitorModalView extends React.Component {
+  renderTree () {
+    const {allDevices, onChangeTreeData} = this.props
+    const monitorTreeData = allDevices.filter(p => p.monitors && p.monitors.length).map(device => ({
+      title: device.name,
+      children: device.monitors.map(monitor => ({
+        title: monitor.name
+      }))
+    }))
+    return (
+      <div style={{ height: 400 }}>
+        <SortableTree
+          treeData={monitorTreeData}
+          onChange={onChangeTreeData}
+          canDrag={false}
+        />
+      </div>
+    )
+  }
+
+  renderTable () {
+    // return (
+    //   <div style={{maxHeight: 400, overflow: 'auto'}}>
+    //     <table className="table table-hover">
+    //       <tbody>
+    //       {allDevices.map(device => (device.monitors || []).map(monitor =>
+    //         <tr
+    //           key={monitor.uid}
+    //           onClick={() => onClickRow(monitor)}
+    //           className={selected && selected.filter(p => p.uid === monitor.uid).length ? 'selected' : ''}
+    //         >
+    //           <td>{`${device.name} - ${monitor.name}`}</td>
+    //         </tr>
+    //       ))}
+    //       </tbody>
+    //     </table>
+    //   </div>
+    // )
+  }
+
   render () {
     const {selected, allDevices, onClickOK, onClickClose, onClickRow, onClickShowAny} = this.props
     return (
       <Modal title="Monitors" onRequestClose={onClickClose}>
         <CardPanel title="Monitors">
-          <div style={{maxHeight: 400, overflow: 'auto'}}>
-            <table className="table table-hover">
-              <tbody>
-              {allDevices.map(device => (device.monitors || []).map(monitor =>
-                <tr
-                  key={monitor.uid}
-                  onClick={() => onClickRow(monitor)}
-                  className={selected && selected.filter(p => p.uid === monitor.uid).length ? 'selected' : ''}
-                >
-                  <td>{`${device.name} - ${monitor.name}`}</td>
-                </tr>
-              ))}
-              </tbody>
-            </table>
-          </div>
+          {this.renderTree()}
         </CardPanel>
         <TwoButtonsBlockCustom name1="Show Any" name2="OK" action1={onClickShowAny} action2={onClickOK}/>
       </Modal>
