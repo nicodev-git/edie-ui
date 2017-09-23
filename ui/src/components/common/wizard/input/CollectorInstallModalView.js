ad1a2eae6f7e09477da1b82468d7da0258c9255e
nicodev-git <schaefernicker@gmail.com>
2017-09-23 15:54:06 +0800

    Fixed download url

--- ui/src/components/common/wizard/input/CollectorInstallModalView.js
+++ ui/src/components/common/wizard/input/CollectorInstallModalView.js
@@ -4,6 +4,7 @@ import { Field, Form } from 'redux-form'
 import RefreshIcon from 'material-ui/svg-icons/navigation/refresh'
 
 import { FormInput, SubmitBlock, Modal, CardPanel } from 'components/modal/parts'
+import {ROOT_URL} from 'actions/config'
 
 export default class CollectorInstallModalView extends React.Component {
   renderForm () {
@@ -19,18 +20,26 @@ export default class CollectorInstallModalView extends React.Component {
   }
   renderDownload () {
     return (
-      <CardPanel>
-        <a href="/device/downloadCollector" target="_blank">Click here to download collector.</a><br/>
-        Please install this collector on your windows pc.<br/>
-        Please make sure collector port(534 by default) is open to incident manager server.<br/>
-        You can click refresh after installed and click Test to check.<br/>
+      <div>
+        <CardPanel title="Collector Install">
+          Please install this collector on your windows pc.<br/>
+          Please make sure collector port(534 by default) is open to incident manager server.<br/>
+          You can click refresh after installed and click Test to check.<br/>
 
-        <div>
-          <RaisedButton label="Test" className="valign-middle"/>
-          <SelectField floatingLabelText="Collector" style={{verticalAlign: 'middle'}}/>
-          <IconButton className="valign-middle"><RefreshIcon/></IconButton>
-        </div>
-      </CardPanel>
+          <div className="margin-lg-top">
+            <div className="valign-middle inline-block" style={{marginTop: -20}}>
+              <SelectField floatingLabelText="Collector"/>
+            </div>
+            <IconButton className="valign-middle"><RefreshIcon/></IconButton>
+            <RaisedButton label="Test" className="valign-middle margin-md-left"/>
+          </div>
+        </CardPanel>
+        <a
+          href={`${ROOT_URL}/downloadCollector?filename=`}
+          target="_blank"
+          style={{textDecoration: 'underline', fontSize: 13}}>
+          Click here to download collector.</a><br/>
+      </div>
     )
   }
   render () {
