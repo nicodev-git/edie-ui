a2a59d03262cf1369a94c2744b14b1df6f023aee
nicodev-git <schaefernicker@gmail.com>
2017-10-02 14:51:06 +0800

    Added show null option

--- ui/src/components/sidebar/search/EntityDetailModal.js
+++ ui/src/components/sidebar/search/EntityDetailModal.js
@@ -3,6 +3,18 @@ import React from 'react'
 import EntityDetailModalView from './EntityDetailModalView'
 
 export default class EntityDetailModal extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isShowNull: false
+    }
+  }
+  onCheckShowNull (e, checked) {
+    this.setState({
+      isShowNull: checked
+    })
+  }
+
   onHide () {
     this.props.showEntityDetailModal(false)
   }
@@ -11,6 +23,8 @@ export default class EntityDetailModal extends React.Component {
       <EntityDetailModalView
         {...this.props}
         onHide={this.onHide.bind(this)}
+        isShowNull={this.state.isShowNull}
+        onCheckShowNull={this.onCheckShowNull.bind(this)}
       />
     )
   }
--- ui/src/components/sidebar/search/EntityDetailModalView.js
+++ ui/src/components/sidebar/search/EntityDetailModalView.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import {Checkbox} from 'material-ui'
+
 import { Modal, CardPanel } from 'components/modal/parts'
 import ReactJson from 'react-json-view'
 
 export default class EntityDetailModalView extends React.Component {
   render () {
-    const {onHide, detailEntity} = this.props
+    const {onHide, detailEntity, isShowNull, onCheckShowNull} = this.props
     return (
       <Modal title="Detail" onRequestClose={onHide} contentStyle={{width: 1035, maxWidth: 'initial'}}>
         <CardPanel title="Detail">
+          <Checkbox label="Show Null" checked={isShowNull} onCheck={onCheckShowNull}/>
           <div style={{width: '100%', overflow: 'auto'}}>
             <ReactJson
               name={false}
