3cc496b1a8eb6d86dd8075cbe7645af3eee4c081
nicodev-git <schaefernicker@gmail.com>
2017-10-30 15:51:48 +0800

    Fixed style

--- ui/src/components/sidebar/search/EntityDetailModalView.js
+++ ui/src/components/sidebar/search/EntityDetailModalView.js
@@ -14,6 +14,26 @@ import { Modal, CardPanel } from 'components/modal/parts'
               valueRenderer={valueRenderer}
             />
  */
+
+const theme = {
+  "base00": "#ffffff",
+  "base01": "#e0e0e0",
+  "base02": "#c5c8c6",
+  "base03": "#b4b7b4",
+  "base04": "#969896",
+  "base05": "#373b41",
+  "base06": "#282a2e",
+  "base07": "#1d1f21",
+  "base08": "#CC342B",
+  "base09": "#F96A38",
+  "base0A": "#FBA922",
+  "base0B": "#198844",
+  "base0C": "#3971ED",
+  "base0D": "#3971ED",
+  "base0E": "#A36AC7",
+  "base0F": "#3971ED"
+}
+
 export default class EntityDetailModalView extends React.Component {
   render () {
     const {onHide, detailEntity, isShowNull, onCheckShowNull, valueRenderer} = this.props
@@ -24,6 +44,8 @@ export default class EntityDetailModalView extends React.Component {
           <div style={{width: '100%', overflow: 'auto'}}>
             <JSONTree
               data={detailEntity}
+              theme={theme}
+              invertTheme={false}
               shouldExpandNode={() => true}
               valueRenderer={valueRenderer}
             />
