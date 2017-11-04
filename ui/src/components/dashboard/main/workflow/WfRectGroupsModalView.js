6bf2accbbdef71399fd5107f6d1e5ed52d2d6abf
nicodev-git <schaefernicker@gmail.com>
2017-11-04 18:21:58 +0800

    Fixed modal

--- ui/src/components/dashboard/main/workflow/WfRectGroupsModalView.js
+++ ui/src/components/dashboard/main/workflow/WfRectGroupsModalView.js
@@ -5,13 +5,15 @@ import {Modal, CardPanel} from 'components/modal/parts'
 
 export default class WfRectGroupsModalView extends React.Component {
   render () {
-    const {onHide, wfRectGroups, name, onChangeName} = this.props
+    const {onHide, wfRectGroups, name, onChangeName, onClickAdd} = this.props
     return (
       <Modal title="Groups" onRequestClose={onHide}>
-        <CardPanel title="Groups">
+        <CardPanel title="Add Group">
           <TextField name="name" hintText="Name" value={name} onChange={onChangeName}/>
-          <RaisedButton label="Add" className="margin-lg-left"/>
+          <RaisedButton label="Add" className="margin-lg-left" onClick={onClickAdd}/>
+        </CardPanel>
 
+        <CardPanel title="Edit Grups">
           <div style={{maxHeight: 300, overflow: 'auto'}}>
             <table className="table">
               <tbody>
