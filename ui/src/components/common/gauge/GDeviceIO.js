fad3c2dc6103b9e8dd1d8f266a3d2bf559a7371f
nicodev-git <schaefernicker@gmail.com>
2017-11-08 12:32:23 +0800

    Added io widget

---
+++ ui/src/components/common/gauge/GDeviceIO.js
@@ -0,0 +1,192 @@
+import React from 'react'
+
+export default class GDeviceIO extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      loading: true,
+      memory: null,
+      cpu: null,
+      disk: null,
+      up: false
+    }
+    this.renderBackView = this.renderBackView.bind(this)
+    this.renderFrontView = this.renderFrontView.bind(this)
+  }
+
+  componentDidMount () {
+    this.startUpdate()
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (JSON.stringify(prevProps.gauge) !== JSON.stringify(this.props.gauge)) {
+      this.stopUpdate()
+      setTimeout(() => {
+        this.startUpdate()
+      }, 10)
+    }
+  }
+
+  componentWillUnmount () {
+    this.stopUpdate()
+  }
+
+  startUpdate () {
+    this.setState({
+      loading: true,
+      memory: null,
+      cpu: null,
+      disk: null,
+      up: false
+    })
+    this.monitorSocket = new MonitorSocket({
+      listener: this.onMonitorMessage.bind(this)
+    })
+    this.monitorSocket.connect(this.onSocketOpen.bind(this))
+
+    checkAgentUp(this.getDeviceId(), up => {
+      this.setState({up, loading: false})
+    })
+  }
+
+  stopUpdate () {
+    this.monitorSocket && this.monitorSocket.close()
+  }
+  ///////////////////////////////////////////////////////////////////
+
+  onSocketOpen () {
+    this.monitorSocket.send({
+      action: 'enable-realtime',
+      monitors: 'basic',
+      deviceId: this.getDeviceId()
+    })
+  }
+
+  onMonitorMessage (msg) {
+    if (msg.action === 'update' && msg.deviceId === this.getDeviceId()) {
+      const {cpu, memory, disk} = msg.data
+      const state = {}
+      if (cpu) state.cpu = cpu
+      if (memory) state.memory = memory
+      if (disk) state.disk = sumDisks(disk)
+      state.loading = false
+
+      this.setState(state)
+    }
+  }
+
+  ////////////////////////////////////////////////////
+  getDeviceId () {
+    return this.props.gauge.deviceId
+  }
+
+  getDevice () {
+    const {devices} = this.props
+    const index = findIndex(devices, {id: this.getDeviceId()})
+    if (index < 0) return null
+    return devices[index]
+  }
+
+  onSubmit (options, values) {
+    console.log(values)
+
+    if (!values.name) {
+      showAlert('Please type name.')
+      return
+    }
+    const gauge = {
+      ...this.props.gauge,
+      ...values
+    }
+
+    this.props.updateDeviceGauge(gauge, this.props.device)
+    options.onClickFlip()
+  }
+  getTitle () {
+    const {gauge} = this.props
+    const devices = this.props.allDevices || this.props.devices
+    const index = findIndex(devices, {id: gauge.deviceId})
+    if (index < 0) return gauge.name
+    return `[${devices[index].name}] ${gauge.name}`
+  }
+
+  /////////////////////////////////////////////////////
+
+  renderItem (item, i) {
+    return (
+      <div key={i} className="flex-1" style={{height: '100%', position: 'relative', overflow: 'hidden'}}>
+        <Speedometer {...item}/>
+      </div>
+    )
+  }
+
+  renderFrontView () {
+    const device = this.getDevice()
+
+
+    if (!device) return <div />
+
+    const up = this.state.up
+
+    if (up) {
+      const {cpu, memory, disk} = this.state
+      const cpuValue = cpu ? (cpu.length ? cpu[0].Usage : cpu.Usage) : 0
+      const memValue = memory ?  Math.ceil(memory.UsedSize * 100 / memory.TotalSize) : 0
+      const diskValue = disk ? Math.ceil(disk.FreeSpace * 100 / disk.TotalSpace) : 0
+
+      const items = [{
+        title1: `${cpuValue}%`,
+        title2: cpu ? `${cpu.length ? cpu[0].Model : cpu.Model}` : '',
+        title3: 'CPU Utilization',
+        value: cpuValue
+      }, {
+        title1: `${memValue}%`,
+        title2: memory ? `${memory.UsedSize}M / ${memory.TotalSize}M` : '',
+        title3: 'Memory Utilization',
+        value: memValue
+      }, {
+        title1: `${diskValue}%`,
+        title2: disk ? `${disk.FreeSpace}G / ${disk.TotalSpace}G` : '',
+        title3: 'Disk Utilization',
+        value: diskValue
+      }]
+
+      return (
+        <div className="flex-1 flex-horizontal" style={{marginTop: 8}}>
+          {items.map(this.renderItem.bind(this))}
+        </div>
+      )
+    } else {
+      return (
+        <NoDataPanel />
+      )
+    }
+
+  }
+
+  renderBackView (options) {
+    return (
+      <div>
+        <GEditView
+          {...this.props}
+          onSubmit={this.onSubmit.bind(this, options)}
+        />
+      </div>
+    )
+  }
+
+  render () {
+    return (
+      <FlipView
+        {...this.props}
+
+        title={this.getTitle()}
+        bodyStyle={{padding: "0px 20px 20px"}}
+
+        loading={this.state.loading}
+        renderFrontView={this.renderFrontView}
+        renderBackView={this.renderBackView}
+      />
+    )
+  }
+}
