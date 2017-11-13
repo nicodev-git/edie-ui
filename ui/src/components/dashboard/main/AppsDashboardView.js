d442883a36b19ded4ed2f4fd421136146414bc21
nicodev-git <schaefernicker@gmail.com>
2017-11-13 20:38:25 +0800

    Added filter

--- ui/src/components/dashboard/main/AppsDashboardView.js
+++ ui/src/components/dashboard/main/AppsDashboardView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {IconButton} from 'material-ui'
+import {IconButton, TextField} from 'material-ui'
 import EditIcon from 'material-ui/svg-icons/content/create'
 
 import EditConfigModal from './apps/EditConfigModal'
@@ -7,12 +7,22 @@ import AppletCard from 'components/common/AppletCard'
 import { appletColors as colors } from 'shared/Global'
 
 export default class AppsDashboardView extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      filter: ''
+    }
+  }
   componentWillMount () {
     this.props.fetchAllApps(false)
   }
 
   getServers () {
-    return this.props.allApps || []
+    const {filter} = this.state
+    const list = this.props.allApps || []
+    const s = filter.toLowerCase()
+    if (!s) return list
+    return list.filter(p => p.Name.toLowerCase().indexOf(s) >= 0)
   }
 
   onClickPref () {
@@ -28,6 +38,12 @@ export default class AppsDashboardView extends React.Component {
 
   onClosePrefModal () {
     this.props.showAppsPrefModal(false)
+  }
+
+  onChangeFilter (e, value) {
+    this.setState({
+      filter: value
+    })
   }
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -48,6 +64,9 @@ export default class AppsDashboardView extends React.Component {
   renderAddMenu () {
     return (
       <div className="text-right" style={{position: 'absolute', top: -45, right: 0}}>
+        <TextField name="filter" value={this.state.filter} onChange={this.onChangeFilter.bind(this)}
+                   hintText="Search"
+                   className="valign-top"/>
         <IconButton onTouchTap={this.onClickPref.bind(this)}><EditIcon /></IconButton>
       </div>
     )
