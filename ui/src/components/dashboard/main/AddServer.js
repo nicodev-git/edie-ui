89377bc1b8d814b21bcee43719327695962655a8
nicodev-git <schaefernicker@gmail.com>
2017-11-17 17:54:38 +0800

    Added server page

--- ui/src/components/dashboard/main/AddServer.js
+++ ui/src/components/dashboard/main/AddServer.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import { wizardConfig, getDeviceType } from 'components/common/wizard/WizardConfig'
+import DeviceWizardContainer from 'containers/shared/wizard/DeviceWizardContainer'
 
 export default class AddServer extends React.Component {
+  componentWillMount () {
+    this.props.fetchDeviceTemplates()
+    this.props.fetchMonitorTemplates()
+  }
+
+
+  closeCallback () {
+
+  }
+  addDevice () {
+
+  }
+  deleteMapDevice () {
+
+  }
+
+  onFinishAddWizard () {
+
+  }
+
   render () {
+    const {options} = this.state.deviceWizardConfig
+
+    let extra = {
+      x: 0,
+      y: 0,
+      width: 50,
+      height: 50,
+      image: options.imgName,
+      templateName: options.templateName,
+      workflowids: options.workflowids,
+      tags: options.tpl.tags || []
+    }
+
     return (
-      <div></div>
+      <DeviceWizardContainer
+        deviceType={options.type}
+        onClose={this.closeCallback.bind(this)}
+        title={options.title}
+        monitors={options.monitors}
+        extraParams={extra}
+        configParams={{}}
+        onFinish={this.onFinishAddWizard.bind(this)}
+        addDevice={this.addDevice.bind(this)}
+        removeDevice={this.deleteMapDevice.bind(this)}
+      />
     )
   }
 }
