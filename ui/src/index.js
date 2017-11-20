850221f091d37bccbb297b18d080fcb8bf83c934
nicodev-git <schaefernicker@gmail.com>
2017-11-20 20:37:18 +0800

    Bumped version

--- ui/src/index.js
+++ ui/src/index.js
@@ -13,7 +13,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 0.31.21')
+console.log('Version: 0.32.0')
 
 injectTapEventPlugin()
 ReactDOM.render(
