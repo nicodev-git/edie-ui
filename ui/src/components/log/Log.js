dafce36eb3f986bf56a6a8fb33538a1da0d31b2c
nicodev-git <schaefernicker@gmail.com>
2017-11-22 20:59:04 +0800

    Added log page

--- ui/src/components/log/Log.js
+++ ui/src/components/log/Log.js
@@ -1,11 +1,27 @@
 import React from 'react'
 
+import TabPage from 'components/common/TabPage'
+import TabPageBody from 'components/common/TabPageBody'
+
 export default class Log extends React.Component {
   render () {
     return (
-      <div>
-        Log
-      </div>
+      <TabPage>
+        <div className="padding-md">
+          <span className="tab-title">Log</span>
+        </div>
+
+        <TabPageBody tabs={[]} history={this.props.history} location={this.props.location}>
+          <div className="flex-horizontal" style={{height: '100%'}}>
+            <div>
+              <div className="header-blue">Log</div>
+            </div>
+            <div className="flex-1">
+              <div className="header-red">Content</div>
+            </div>
+          </div>
+        </TabPageBody>
+      </TabPage>
     )
   }
 }
