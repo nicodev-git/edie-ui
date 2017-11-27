cb1497e83cb67a0fb07aee6518b5c698fb091a85
nicodev-git <schaefernicker@gmail.com>
2017-11-27 16:51:37 +0800

    Fixed diagram view

--- ui/src/components/dashboard/map/device/main/workflows/DiagramModalView.js
+++ ui/src/components/dashboard/map/device/main/workflows/DiagramModalView.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 
-import { TwoButtonsBlockCustom, Modal } from 'components/modal/parts'
+import { /*TwoButtonsBlockCustom, */Modal } from 'components/modal/parts'
 
 export default class DiagramModalView extends Component {
   render () {
-    const {header, onHide, onSave, dragLayer, toolbar, sidebar,
+    const {header, onHide,/* onSave,*/ dragLayer, toolbar, sidebar,
       panel, objectModal, noModal} = this.props
     const content = (
-      <div className={`diagram ${noModal ? 'flex-vertical' : ''}`} style={noModal ? {height: '100%'} : null}>
+      <div className={`diagram ${noModal ? 'flex-vertical' : ''}`}
+           style={noModal ? {height: '100%'} : null}>
         {dragLayer}
         {toolbar}
         <div className="flex-1 flex-horizontal">
