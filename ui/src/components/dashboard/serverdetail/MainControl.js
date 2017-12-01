0e6b0f55402bf30c6a6c217d3265b962efbe83b4
nicodev-git <schaefernicker@gmail.com>
2017-12-01 15:05:51 +0800

    Fixed main widgets

--- ui/src/components/dashboard/serverdetail/MainControl.js
+++ ui/src/components/dashboard/serverdetail/MainControl.js
@@ -72,7 +72,8 @@ export default class MainControl extends React.Component {
       ['Incidents', {x: 0, y: h * 2, w: w * 2}],
       ['Workflows', {x: 0, y: h * 3, w: w * 2}],
       ['NetStat', {x: 0, y: h * 4, w: w * 2}],
-      ['DiskParts', {x: 0, y: h * 5, w}]
+      ['DiskParts', {x: 0, y: h * 5, w}],
+      ['Network', {x: w, y: h * 5, w}]
     ]
 
 
