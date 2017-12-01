904a581f86696968176c381907ff2731e0ec17b6
nicodev-git <schaefernicker@gmail.com>
2017-12-01 20:56:13 +0800

    Fixed edit page

--- ui/src/components/sidebar/settings/users/EditUser.js
+++ ui/src/components/sidebar/settings/users/EditUser.js
@@ -3,12 +3,14 @@ import {findIndex, assign} from 'lodash'
 import { reduxForm } from 'redux-form'
 import { connect } from 'react-redux'
 
-import {SelectField, MenuItem} from 'material-ui'
+import {SelectField, MenuItem, Checkbox} from 'material-ui'
 import { Field } from 'redux-form'
-import { FormInput, FormSelect, FormCheckbox, SubmitBlock, Modal, CardPanel } from 'components/modal/parts'
+import { FormInput, FormSelect, FormCheckbox, SubmitBlock, CardPanel } from 'components/modal/parts'
 import { roleOptions } from 'shared/Global'
 import {mainMenu} from 'components/sidebar/Config'
 import { validate } from 'components/modal/validation/NameValidation'
+import TabPage from 'components/common/TabPage'
+import TabPageBody from 'components/common/TabPageBody'
 
 class EditUser extends React.Component {
   componentWillMount () {
@@ -19,7 +21,7 @@ class EditUser extends React.Component {
   }
   componentDidUpdate (prevProps) {
     const {users, editUser, match} = this.props
-    if (prevProps.users !== users && !editUser) {
+    if (users && users.length && prevProps.users !== users && !editUser) {
       const index = findIndex(users, {username: match.params.user})
       if (index < 0) {
         this.props.history.push(`/settings/users`)
@@ -41,7 +43,9 @@ class EditUser extends React.Component {
       this.props.addSettingUser(user)
     }
   }
-
+  onCheckRole (value) {
+    //
+  }
   onChangeRole (e, index, values) {
     this.props.selectUserRoles(values)
   }
@@ -57,49 +61,81 @@ class EditUser extends React.Component {
     const permissions = selectedPermissions
     if (!editUser) return <div>Loading...</div>
     return (
-      <div>
-        <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-          <CardPanel title="User Settings" className="margin-md-bottom">
-            <Field name="username" component={FormInput} label="Name" className="mr-dialog"/>
-            <Field name="fullname" component={FormInput} label="Full Name"/>
+      <TabPage>
+        <div style={{margin: '16px 20px 0'}}>
+          <span className="tab-title">Edit User</span>
+        </div>
 
-            <Field name="password" type="password" component={FormInput} label="Password" className="mr-dialog"/>
-            <Field name="email" component={FormInput} label="Email"/>
+        <TabPageBody tabs={[]} transparent>
+          <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+            <CardPanel title="User Settings" className="margin-md-bottom">
+              <Field name="username" component={FormInput} label="Name" className="mr-dialog"/>
+              <Field name="fullname" component={FormInput} label="Full Name" className="mr-dialog"/>
 
-            <Field name="phone" component={FormInput} label="Phone" className="valign-top mr-dialog"/>
-            <Field name="defaultMapId" component={FormSelect} label="Default Map" options={defaultmaps} className="valign-top"/>
+              <Field name="password" type="password" component={FormInput} label="Password" className="mr-dialog"/>
+              <Field name="email" component={FormInput} label="Email" className="mr-dialog"/>
 
-            <SelectField multiple hintText="Role" onChange={this.onChangeRole.bind(this)} value={selectedRoles}
-                         className="mr-dialog">
-              {roles.map(option =>
-                <MenuItem
-                  key={option.value}
-                  insetChildren
-                  checked={selectedRoles.includes(option.value)}
-                  value={option.value}
-                  primaryText={option.label}
-                />
-              )}
-            </SelectField>
+              <Field name="phone" component={FormInput} label="Phone" className="valign-top mr-dialog"/>
+              <Field name="defaultMapId" component={FormSelect} label="Default Map" options={defaultmaps} className="valign-top mr-dialog"/>
 
 
-            <SelectField multiple hintText="Permission" onChange={this.onChangePermission.bind(this)} value={permissions}>
-              {mainMenu.map(p =>
-                <MenuItem
-                  key={p.id}
-                  insetChildren
-                  checked={permissions.includes(p.roleMenuId)}
-                  value={p.roleMenuId}
-                  primaryText={p.title}
-                />
-              )}
-            </SelectField>
+              <div className="hidden">
+              <SelectField multiple hintText="Role" onChange={this.onChangeRole.bind(this)} value={selectedRoles}
+                           className="mr-dialog">
+                {roles.map(option =>
+                  <MenuItem
+                    key={option.value}
+                    insetChildren
+                    checked={selectedRoles.includes(option.value)}
+                    value={option.value}
+                    primaryText={option.label}
+                  />
+                )}
+              </SelectField>
 
-            <Field name="enabled" component={FormCheckbox} label="Enabled" />
-          </CardPanel>
-          <SubmitBlock name="Save"/>
-        </form>
-      </div>
+
+              <SelectField multiple hintText="Permission" onChange={this.onChangePermission.bind(this)} value={permissions}>
+                {mainMenu.map(p =>
+                  <MenuItem
+                    key={p.id}
+                    insetChildren
+                    checked={permissions.includes(p.roleMenuId)}
+                    value={p.roleMenuId}
+                    primaryText={p.title}
+                  />
+                )}
+              </SelectField>
+              </div>
+
+              <Field name="enabled" component={FormCheckbox} label="Enabled" />
+            </CardPanel>
+
+            <div className="row">
+              <div className="col-md-6">
+                <CardPanel title="Roles">
+                  <table className="table table-hover">
+                    <tbody>
+                    {roles.map(option =>
+                      <tr key={option.value}>
+                        <td>
+                          <Checkbox label={option.label} checked={selectedRoles.includes(option.value)}
+                                    onCheck={this.onCheckRole.bind(this, option.value)}/>
+                        </td>
+                      </tr>
+                    )}
+                    </tbody>
+                  </table>
+                </CardPanel>
+              </div>
+              <div className="col-md-6">
+                <CardPanel title="Permissions">
+                </CardPanel>
+              </div>
+            </div>
+            <SubmitBlock name="Save"/>
+          </form>
+        </TabPageBody>
+      </TabPage>
     )
   }
 }
