d50b379bf225d1e00ec2d0a2b6fbfe444f316e2f
nicodev-git <schaefernicker@gmail.com>
2017-12-02 18:37:44 +0800

    Fixed map edit

--- ui/src/index.js
+++ ui/src/index.js
@@ -13,7 +13,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 0.33.11')
+console.log('Version: 0.33.12')
 
 injectTapEventPlugin()
 ReactDOM.render(
