b581f30e919bc278135fe7c2dfddefdfaf0899a2
nicodev-git <schaefernicker@gmail.com>
2018-02-23 12:58:48 +0800

    Fixed metric panel

--- ui/src/components/common/Metric.js
+++ ui/src/components/common/Metric.js
@@ -4,7 +4,8 @@ import Button from 'material-ui/Button'
 const style = {
   width: '100%',
   color: '#7c7c7e',
-  padding: '0 2px'
+  padding: '0 2px',
+  height: 28
 }
 
 // const labelStyle = {
@@ -16,10 +17,10 @@ const Metric = ({icon, value, title, onClick}) => (
           onTouchTap={onClick}
           style={style}
   >
-    <div style={style} className="margin-sm-top">
+    <div style={style}>
       <span className="incident-button-value">{value}</span>
-      <div className="inline-block" style={{marginTop: -15}}>{title}</div>
-      <div className="inline-block">{icon}</div>
+      <div className="inline-block valign-middle">{title}</div>
+      <div className="inline-block valign-middle margin-xs-top">{icon}</div>
     </div>
   </Button>
 )
