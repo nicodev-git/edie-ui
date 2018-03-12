dcdaf3f047baa4a4fd8b02b2037a1ec1f6b86c88
nicodev-git <schaefernicker@gmail.com>
2018-03-12 22:18:49 +0800

    Fixed gauges

--- ui/src/index.js
+++ ui/src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 0.37.19')
+console.log('Version: 0.37.20')
 
 ReactDOM.render(
     <Provider store={store}>
