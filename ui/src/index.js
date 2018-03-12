f9e536726ef636286bd70de415ce478be89b23f9
nicodev-git <schaefernicker@gmail.com>
2018-03-12 18:23:23 +0800

    Fixed bar chart

--- ui/src/index.js
+++ ui/src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 0.37.13')
+console.log('Version: 0.37.14')
 
 ReactDOM.render(
     <Provider store={store}>
