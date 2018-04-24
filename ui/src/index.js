114e77c48976bb575e811dc285677424b18bde58
nicodev-git <schaefernicker@gmail.com>
2018-04-24 12:20:55 +0800

    Fixed agent picker

--- ui/src/index.js
+++ ui/src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 0.38.1')
+console.log('Version: 0.38.2')
 
 ReactDOM.render(
     <Provider store={store}>
