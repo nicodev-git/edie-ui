443d8b55fd1d30e18cf595ef9e428f4ad5656a12
nicodev-git <schaefernicker@gmail.com>
2018-06-26 21:44:31 +0800

    Fixed tabs

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 2018.1.39')
+console.log('Version: 2018.1.40')
 
 ReactDOM.render(
     <Provider store={store}>
