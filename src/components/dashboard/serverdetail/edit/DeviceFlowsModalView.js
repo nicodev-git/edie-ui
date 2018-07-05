76817c57a1d67036a800040c9b4e02ea8c8b7c77
nicodev-git <schaefernicker@gmail.com>
2018-07-05 15:54:57 +0800

    Fixed device flow modal

--- src/components/dashboard/serverdetail/edit/DeviceFlowsModalView.js
+++ src/components/dashboard/serverdetail/edit/DeviceFlowsModalView.js
@@ -1,15 +1,28 @@
 import React from 'react'
-import {Form} from 'redux-form'
-import { Modal } from 'components/modal/parts'
-import CardPanel from "../../../modal/parts/CardPanel";
+import { Modal, CardPanel } from 'components/modal/parts'
 
 export default class DeviceFlowsModalView extends React.Component {
   render () {
-    const {onHide, workflows} = this.props
+    const {onHide, deviceWorkflows} = this.props
     return (
       <Modal title="Flows" onRequestClose={onHide}>
         <CardPanel title="Flows">
-
+          <table className="table table-hover">
+            <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+            </tr>
+            </thead>
+            <tbody>
+            {deviceWorkflows.map(p =>
+              <tr key={p.uuid}>
+                <td>{p.name}</td>
+                <td>{p.description}</td>
+              </tr>
+            )}
+            </tbody>
+          </table>
         </CardPanel>
       </Modal>
     )
