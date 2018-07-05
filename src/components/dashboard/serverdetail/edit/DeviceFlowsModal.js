2383d309f47d1a4caee8b986d372caa4f580da8a
nicodev-git <schaefernicker@gmail.com>
2018-07-05 18:11:14 +0800

    Fixed flow modal

--- src/components/dashboard/serverdetail/edit/DeviceFlowsModal.js
+++ src/components/dashboard/serverdetail/edit/DeviceFlowsModal.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {findIndex} from 'lodash'
 
 import DeviceFlowsModalView from './DeviceFlowsModalView'
 import FlowPickerModal from './FlowPickerModal'
@@ -12,16 +13,49 @@ export default class DeviceFlowsModal extends React.Component {
   }
 
   componentWillMount () {
-    const {device} = this.props
-    this.props.fetchDeviceWorkflows({
-      uuid: device.workflowids || []
-    })
+    // const {device} = this.props
+    // this.props.fetchDeviceWorkflows({
+    //   uuid: device.workflowids || []
+    // })
+    this.props.fetchWorkflows()
   }
 
-  onClickAdd () {
+  onClickShowPicker () {
     this.setState({
       flowPickerOpen: true
     })
+  }
+
+  onClickAddWf (wf) {
+    const {device} = this.props
+    let {workflowids} = device
+
+    this.setState({
+      flowPickerOpen: false
+    })
+
+    if (!workflowids) workflowids = []
+    if (workflowids.indexOf(wf.uuid) >= 0) return
+    workflowids = [...workflowids, wf.uuid]
+
+    this.props.updateMapDevice({
+      ...device,
+      workflowids
+    })
+  }
+
+  getDeviceWorkflows () {
+    const {workflows, device} = this.props
+    const {workflowids} = device
+    if (!workflowids) return []
+
+    const found = []
+    workflowids.forEach(uuid => {
+      const index = findIndex(workflows, {uuid})
+      if (index >= 0) found.push(workflows[index])
+    })
+
+    return found
   }
 
   renderFlowPicker () {
@@ -30,6 +64,7 @@ export default class DeviceFlowsModal extends React.Component {
       <FlowPickerModal
         fetchWorkflows={this.props.fetchWorkflows}
         workflows={this.props.workflows}
+        onClickOK={this.onClickAddWf.bind(this)}
         onClickClose={() => this.setState({flowPickerOpen: false})}
       />
     )
@@ -39,7 +74,8 @@ export default class DeviceFlowsModal extends React.Component {
     return (
       <DeviceFlowsModalView
         {...this.props}
-        onClickAdd={this.onClickAdd.bind(this)}
+        deviceWorkflows={this.getDeviceWorkflows()}
+        onClickAdd={this.onClickShowPicker.bind(this)}
       >
         {this.renderFlowPicker()}
       </DeviceFlowsModalView>
