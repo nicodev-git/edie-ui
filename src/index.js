73e000b4c1c856cefe744d085fe7ef3622de72e9
nicodev-git <schaefernicker@gmail.com>
2018-07-16 16:23:39 +0800

    Fixed device info

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 2018.1.65')
+console.log('Version: 2018.1.66')
 
 ReactDOM.render(
     <Provider store={store}>
