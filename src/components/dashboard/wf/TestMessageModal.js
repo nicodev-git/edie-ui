d63132576024a43d09bf2eedb6da73df854fbc1e
nicodev-git <schaefernicker@gmail.com>
2018-07-25 13:45:54 +0800

    Fixed message modal

--- src/components/dashboard/wf/TestMessageModal.js
+++ src/components/dashboard/wf/TestMessageModal.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import {reduxForm, getFormValues} from 'redux-form'
+import {connect} from 'react-redux'
 
 import TestMessageModalView from './TestMessageModalView'
 
 export default class TestMessageModal extends React.Component {
+  onSubmit (values) {
+    const {onSubmit} = this.props
+
+
+    onSubmit(values)
+  }
+
   render() {
     return (
       <TestMessageModalView
-
+        onSubmit={handleSubmit(this.onSubmit.bind(this))}
+        onClickClose={onClickClose}
       />
     )
   }
 }
+
+export default connect(
+  (state, props) => ({
+    initialValues: {},
+    allValues: getFormValues('wfTestMsgForm')(state)
+  })
+)(reduxForm({form: 'wfTestMsgForm'})(TestCaseModal))
