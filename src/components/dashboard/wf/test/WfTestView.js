8e0c3b8b98b24f183df26516c9ed466db4ecc1d4
nicodev-git <schaefernicker@gmail.com>
2018-07-28 19:08:17 +0800

    Added tool bar

--- src/components/dashboard/wf/test/WfTestView.js
+++ src/components/dashboard/wf/test/WfTestView.js
@@ -14,7 +14,7 @@ export default class WfTestView extends React.Component {
   renderMessages () {
     const {caseModal} = this.props
     return (
-      <div className="flex-1" style={{minHeight: 300}}>
+      <div className="flex-1 bg-white">
         {caseModal}
       </div>
     )
@@ -22,7 +22,7 @@ export default class WfTestView extends React.Component {
 
   renderIncidents () {
     return (
-      <div className="flex-1">
+      <div className="flex-1 bg-white margin-md-top">
 
       </div>
     )
@@ -36,7 +36,7 @@ export default class WfTestView extends React.Component {
     } = this.props
 
     return (
-      <div className="margin-md-right" style={{width: 350}}>
+      <div className="flex-1 margin-md-right bg-white">
         <div>
           <table className="table table-hover">
             <thead>
@@ -80,23 +80,27 @@ export default class WfTestView extends React.Component {
       <TabPage>
         <TabPageHeader title="Simulation">
           <div className="text-center margin-md-top">
-            <div className="pull-left text-left">
-              <FormControl className="valign-middle" style={{minWidth: 200}}>
-                <InputLabel>Test Group</InputLabel>
-                <Select displayEmpty value={selectedGroupId || ''} onChange={selectGroupId}>
-                  {testGroups.map(p => <MenuItem key={p.id} value={p.id}>{p.name}</MenuItem>)}
-                </Select>
-              </FormControl>
-            </div>
-            <div className="pull-right">
-              <AddIcon onClick={onClickAddGroup} className="link valign-middle margin-sm-top"/>
-              <EditIcon onClick={onClickEditGroup} className="link valign-middle margin-sm-top"/>
+            <div style={{background: '#dadada', paddingLeft: 10}}>
+              <div style={{height: 60}}>
+                <div className="pull-left text-left">
+                  <FormControl className="valign-middle" style={{minWidth: 200}}>
+                    <InputLabel>Test Group</InputLabel>
+                    <Select displayEmpty value={selectedGroupId || ''} onChange={selectGroupId}>
+                      {testGroups.map(p => <MenuItem key={p.id} value={p.id}>{p.name}</MenuItem>)}
+                    </Select>
+                  </FormControl>
+                </div>
+                <div className="pull-right margin-xs-top">
+                  <AddIcon onClick={onClickAddGroup} className="link valign-middle margin-sm-top"/>
+                  <EditIcon onClick={onClickEditGroup} className="link valign-middle margin-sm-top"/>
+                </div>
+              </div>
             </div>
           </div>
         </TabPageHeader>
 
         <TabPageBody history={this.props.history} location={this.props.location} transparent>
-          <div className="flex-vertical padding-sm">
+          <div className="flex-1 flex-vertical padding-sm" style={{height: '100%'}}>
             <div className="flex-1 flex-horizontal">
               {this.renderTestCases()}
               {this.renderMessages()}
