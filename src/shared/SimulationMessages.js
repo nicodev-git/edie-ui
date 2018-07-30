6d568013380194965ded2156cd30db9ad4207c79
nicodev-git <schaefernicker@gmail.com>
2018-07-30 17:46:32 +0800

    Fixed simulation

--- src/shared/SimulationMessages.js
+++ src/shared/SimulationMessages.js
@@ -1,7 +1,23 @@
 export const messageTypes = [{
   name: 'CPU',
-  img: 'cpu.png'
+  img: 'cpu.png',
+  data: [{
+    key: 'usage',
+    label: 'Usage'
+  }, {
+    key: 'time',
+    label: 'Time'
+  }],
+  json: ''
 }, {
   name: 'Root Login',
-  img: 'credential.png'
+  img: 'credential.png',
+  data: [{
+    key: 'ip',
+    label: 'IP'
+  }, {
+    key: 'time',
+    label: 'Time'
+  }],
+  json: ''
 }]
