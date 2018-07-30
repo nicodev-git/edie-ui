17fe57d75a4cddc1ba5287eb00737c32f1234f16
nicodev-git <schaefernicker@gmail.com>
2018-07-30 20:22:59 +0800

    Fixed edit modal

--- src/components/sidebar/wf/simulation/TestMessageEditModal.js
+++ src/components/sidebar/wf/simulation/TestMessageEditModal.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import {find} from 'lodash'
 
 import TestMessageEditModalView from './TestMessageEditModalView'
-
 import {messageTypes} from 'shared/SimulationMessages'
 
 export default class TestMessageEditModal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  getMessageType () {
+    const {typeName} = this.props
+    const msgType = find(messageTypes, {name: typeName})
+    return msgType
+  }
+
   render() {
     const {onClose} = this.props
     return (
