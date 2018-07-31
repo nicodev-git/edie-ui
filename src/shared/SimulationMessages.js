e0a22a877dba2c7ccf6359b9311c60b80bb71498
nicodev-git <schaefernicker@gmail.com>
2018-07-31 17:55:24 +0800

    Added login failure message

--- src/shared/SimulationMessages.js
+++ src/shared/SimulationMessages.js
@@ -36,4 +36,27 @@ export const messageTypes = [{
     default: 'root'
   }],
   json: '{"incident":{"monitorName":"Logfile","monitortype":"logfile","description":"","startTimestamp":${timestamp},"data":{"file":"/var/log/secure","line":"${time} cloud4 sshd[15564]: Accepted password for root from ${ip} port 54376 ssh2"},"params":{"parse":"{\\"line\\":\\"AnyLine\\"}"}}}'
+}, {
+  name: 'Login Failure',
+  img: 'credential.png',
+  data: [{
+    key: 'ip',
+    label: 'IP',
+    default: '192.168.254.200'
+  }, {
+    key: 'time',
+    label: 'Time',
+    default: 'Jul 20 17:51:15'
+  }, {
+    key: 'timestamp',
+    label: 'Timestamp',
+    default: 1532080275000,
+    type: 'datetime',
+    format: ''
+  }, {
+    key: 'user',
+    label: 'User',
+    default: 'root'
+  }],
+  json: '{"incident":{"monitorName":"Logfile","monitortype":"logfile","description":"","startTimestamp":${timestamp},"data":{"file":"/var/log/secure","line":"${time} cloud4 sshd[21332]: Failed password for root from ${ip} port 2086 ssh2"},"params":{"parse":"{\\"line\\":\\"AnyLine\\"}"}}}'
 }]
