02e9321e30947fdfdf2cc4fb00bdb89987694470
nicodev-git <schaefernicker@gmail.com>
2018-08-01 21:01:08 +0800

    Fixed grok cells

--- src/components/sidebar/settings/braincell/Braincells.js
+++ src/components/sidebar/settings/braincell/Braincells.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button, Select, MenuItem} from '@material-ui/core'
+import {Button, Select, MenuItem, Chip} from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/Delete'
 import EditIcon from '@material-ui/icons/Create'
 import ReactTooltip from 'react-tooltip'
@@ -116,7 +116,7 @@ export default class Braincells extends React.Component {
 
     renderKey (p) {
         if (p.type === 'Grok') return ((p.params2 || {}).tags || []).join(', ')
-        return p.key
+        return <Chip>{p.key}</Chip>
     }
 
     renderContent () {
