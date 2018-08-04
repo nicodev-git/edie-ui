ed577c9eb6460e6ed88d2e98f674870ab896e22f
nicodev-git <schaefernicker@gmail.com>
2018-08-04 11:26:34 +0800

    Fixed product detail

--- src/components/sidebar/settings/product/VendorProductModalView.js
+++ src/components/sidebar/settings/product/VendorProductModalView.js
@@ -11,25 +11,28 @@ class SimulationModalView extends React.Component {
     return (
       <div className="flex-1 padding-md">
         <form onSubmit={onSubmit}>
-          <AppBar position="static">
-            <Toolbar variant="dense">
-              <Typography variant="title" color="inherit">
-                Basic
-              </Typography>
-            </Toolbar>
-          </AppBar>
 
-          <div className="row">
-            <div className="col-md-2 col-lg-2">
-              <Field name="name" component={FormInput} floatingLabel="Name" className="margin-md-right" fullWidth/>
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <h3>Vendor Product Info</h3>
             </div>
-            <div className="col-md-2 col-lg-2">
-              <Field name="version" component={FormInput} floatingLabel="Version" className="margin-md-right" fullWidth/>
-            </div>
-            <div className="col-md-8 col-lg-6">
-              <Field name="description" component={FormInput} floatingLabel="Description" className="margin-md-right" fullWidth/>
+            <div className="panel-body">
+              <div className="row">
+                <div className="col-md-2 col-lg-2">
+                  <Field name="name" component={FormInput} floatingLabel="Name" className="margin-md-right" fullWidth/>
+                </div>
+                <div className="col-md-2 col-lg-2">
+                  <Field name="version" component={FormInput} floatingLabel="Version" className="margin-md-right" fullWidth/>
+                </div>
+                <div className="col-md-8 col-lg-6">
+                  <Field name="description" component={FormInput} floatingLabel="Description" className="margin-md-right" fullWidth/>
+                </div>
+              </div>
             </div>
           </div>
+
+
+
 
           <CardPanel title="Tags">
 
