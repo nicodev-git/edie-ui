20510c9d50e6880a04265bf6e5f058fcafd2550d
nicodev-git <schaefernicker@gmail.com>
2018-08-08 22:04:32 +0800

    Fixed add

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 2018.1.20')
+console.log('Version: 2018.1.21')
 
 ReactDOM.render(
     <Provider store={store}>
