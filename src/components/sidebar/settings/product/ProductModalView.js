b14ba6299ba078b0725e793dfa3355b41c981664
nicodev-git <schaefernicker@gmail.com>
2018-08-10 23:01:51 +0800

    Fixed height

--- src/components/sidebar/settings/product/ProductModalView.js
+++ src/components/sidebar/settings/product/ProductModalView.js
@@ -3,6 +3,10 @@ import {Field} from 'redux-form'
 
 import {Modal, CardPanel} from 'components/modal/parts'
 
+const divStyle = {
+  minHeight: 500
+}
+
 export default class ProductModalView extends React.Component {
   render () {
     const {onClickClose} = this.props
@@ -10,12 +14,16 @@ export default class ProductModalView extends React.Component {
       <Modal title="Product" onRequestClose={onClickClose}>
         <div className="flex-horizontal">
           <CardPanel title="Vendor" className="flex-1">
+            <div style={divStyle}>
 
+            </div>
           </CardPanel>
           <div style={{width: 12}}>
           </div>
           <CardPanel title="List" className="flex-1">
+            <div style={divStyle}>
 
+            </div>
           </CardPanel>
         </div>
       </Modal>
