dd4e7df651cdd1ac7eeb8e50cc89e8b1fd8dd75c
nicodev-git <schaefernicker@gmail.com>
2018-08-11 21:21:12 +0800

    Fixed actions

--- src/components/sidebar/settings/product/ProductTypeModal.js
+++ src/components/sidebar/settings/product/ProductTypeModal.js
@@ -15,22 +15,38 @@ class ProductTypeModal extends React.Component {
 
   handleFormSubmit (values) {
     const {editType} = this.props
+    const {actions} = this.state
     this.props.onSave({
       ...editType,
-      ...values
+      ...values,
+      actions
     })
   }
 
   onClickAddAction () {
-
+    const action = window.prompt('Please type action')
+    if (!action) return
+    const {actions} = this.state
+    this.setState({
+      actions: [...actions, action]
+    })
   }
 
-  onClickEditAction () {
-
+  onClickEditAction (index) {
+    const {actions} = this.state
+    const action = window.prompt('Please type action', actions[index])
+    if (!action) return
+    this.setState({
+      actions: actions.map((p, i) => i === index ? action : p)
+    })
   }
 
-  onClickDeleteAction () {
-
+  onClickDeleteAction (index) {
+    const {actions} = this.state
+    if (!window.confirm('Click OK to delete')) return
+    this.setState({
+      actions: actions.filter((p, i) => i !== index)
+    })
   }
 
   render () {
--- src/components/sidebar/settings/product/ProductTypeModalView.js
+++ src/components/sidebar/settings/product/ProductTypeModalView.js
@@ -10,36 +10,38 @@ import {Modal, CardPanel, SubmitBlock, FormInput} from 'components/modal/parts'
 export default class ProductTypeModalView extends React.Component {
   render () {
     const {onClose, onSubmit,
-      actions, onClickEditAction, onClickDeleteAction} = this.props
+      actions, onClickAddAction, onClickEditAction, onClickDeleteAction} = this.props
     return (
       <Modal title="Product Type" onRequestClose={onClose}>
         <form onSubmit={onSubmit}>
           <CardPanel title="Product Type">
             <Field name="name" component={FormInput} floatingLabel="Name" className="margin-md-right" fullWidth/>
           </CardPanel>
-          <CardPanel title="Actions">
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Action</TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-              </TableHead>
-
-              <TableBody>
-                {actions.map((p, i) =>
-                  <TableRow key={i}>
-                    <TableCell  component="th" scope="row">
-                      {p}
-                    </TableCell>
-                    <TableCell className="nowrap">
-                      <EditIcon className="link margin-sm-right" onClick={() => onClickEditAction(i)}/>
-                      <DeleteIcon className="link" onClick={() => onClickDeleteAction(i)}/>
-                    </TableCell>
+          <CardPanel title="Actions" tools={<AddIcon className="link" onClick={onClickAddAction}/>}>
+            <div style={{maxHeight: 500, overflow: 'auto'}}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Action</TableCell>
+                    <TableCell></TableCell>
                   </TableRow>
-                )}
-              </TableBody>
-            </Table>
+                </TableHead>
+
+                <TableBody>
+                  {actions.map((p, i) =>
+                    <TableRow key={i}>
+                      <TableCell  component="th" scope="row">
+                        {p}
+                      </TableCell>
+                      <TableCell className="nowrap">
+                        <EditIcon className="link margin-sm-right" onClick={() => onClickEditAction(i)}/>
+                        <DeleteIcon className="link" onClick={() => onClickDeleteAction(i)}/>
+                      </TableCell>
+                    </TableRow>
+                  )}
+                </TableBody>
+              </Table>
+            </div>
           </CardPanel>
           <SubmitBlock name="Save"/>
         </form>
