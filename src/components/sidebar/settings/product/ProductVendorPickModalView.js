2e5fc6d06eeafc41c7fc051f2d6f99e917d71ca3
nicodev-git <schaefernicker@gmail.com>
2018-08-13 12:12:37 +0800

    Fixed vendor pick

--- src/components/sidebar/settings/product/ProductVendorPickModalView.js
+++ src/components/sidebar/settings/product/ProductVendorPickModalView.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import {Field} from 'redux-form'
-import {Button} from '@material-ui/core'
-import {Modal, CardPanel, SubmitBlock, FormSelect, FormInput} from 'components/modal/parts'
+import {FormControlLabel, Radio} from '@material-ui/core'
+import {Modal, CardPanel, SubmitBlock, FormSelect, FormInput, FormRadioGroup} from 'components/modal/parts'
 
 export default class ProductVendorPickModalView extends React.Component {
   render () {
-    const {onClose, onSubmit, onChooseExisting, productVendors} = this.props
+    const {onClose, onSubmit, productVendors} = this.props
     return (
       <Modal title="Product Vendor" onRequestClose={onClose}>
         <form onSubmit={onSubmit}>
-          <CardPanel title="New">
-            <Field name="name" component={FormInput} floatingLabel="Name" className="margin-md-right" fullWidth/>
-            <SubmitBlock name="Save"/>
-          </CardPanel>
+          <CardPanel>
+            <Field name="addType" component={FormRadioGroup} className="margin-md-top">
+              <FormControlLabel control={<Radio />} value="new" label="New"/>
+              <FormControlLabel control={<Radio />} value="existing" label="Existing"/>
+            </Field>
 
-          <CardPanel title="Existing">
+            <Field name="name" component={FormInput} floatingLabel="Name" className="margin-md-right" fullWidth/>
+
             <Field name="existingId" component={FormSelect} floatingLabel="Existing" className="margin-md-right"
                    options={productVendors.map(p => ({label: p.name, value: p.id}))}
                    style={{minWidth: 120}}/>
-            <Button variant="raised" onClick={onChooseExisting}>Add</Button>
+
+            <SubmitBlock name="Save"/>
           </CardPanel>
         </form>
       </Modal>
