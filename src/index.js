ec7fdf0d5870fd5941af3f7694624724110844f6
nicodev-git <schaefernicker@gmail.com>
2018-08-16 14:48:52 +0800

    Fixed shapes

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 2018.1.28')
+console.log('Version: 2018.1.29')
 console.log('React Version: ' + React.version)
 
 ReactDOM.render(
