c2f4d3bc26b9171ae46068a685474d99dbfb82ed
nicodev-git <schaefernicker@gmail.com>
2018-08-18 23:05:42 +0800

    Fixed form

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 2018.1.48')
+console.log('Version: 2018.1.49')
 console.log('React Version: ' + React.version)
 
 ReactDOM.render(
