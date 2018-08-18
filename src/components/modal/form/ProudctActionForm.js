473262f73089570db5c0e6690051a14e0d7e8e59
nicodev-git <schaefernicker@gmail.com>
2018-08-18 19:01:20 +0800

    Fixed type id

---
+++ src/components/modal/form/ProudctActionForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Field} from 'redux-form'
+
+import {
+  FormInput,
+  CardPanel
+} from 'components/modal/parts'
+
+export default class ProductActionForm extends React.Component {
+  render () {
+    return (
+      <div>
+        <CardPanel title="Action">
+          <Field name="field" component={FormInput} floatingLabel="Action Id"
+                 className="valign-top margin-md-right"/>
+          <Field name="varField" component={FormInput} floatingLabel="Type Id"
+                 className="valign-top margin-md-right"/>
+        </CardPanel>
+
+      </div>
+    )
+  }
+}
