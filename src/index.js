3e72e9199377c54133b2f209e3d12dfda0a0d1f5
nicodev-git <schaefernicker@gmail.com>
2018-08-20 17:40:31 +0800

    Fixed config

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 2018.1.55')
+console.log('Version: 2018.1.56')
 console.log('React Version: ' + React.version)
 
 ReactDOM.render(
