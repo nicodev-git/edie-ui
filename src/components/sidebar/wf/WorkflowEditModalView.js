b38440f2454fe83f4d3d54474b6b8d18b98c576f
nicodev-git <schaefernicker@gmail.com>
2018-08-25 14:05:18 +0800

    Fixed filter types

--- src/components/sidebar/wf/WorkflowEditModalView.js
+++ src/components/sidebar/wf/WorkflowEditModalView.js
@@ -503,20 +503,6 @@ class WorkflowEditModalView extends React.Component {
     }
   }
 
-  cumulativeOffset(element) {
-    var top = 0, left = 0;
-    do {
-      top += element.offsetTop  || 0;
-      left += element.offsetLeft || 0;
-      element = element.offsetParent
-    } while(element)
-
-    return {
-      top: top,
-      left: left
-    }
-  }
-
   renderEditPopover () {
     const {shapeModal, shapeAnchorEl, onCloseShapeModal} = this.props
     if (!shapeModal) return null
