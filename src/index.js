f32be5f0efbb1ed51eb1c79eb98358f6d64661d2
nicodev-git <schaefernicker@gmail.com>
2018-08-27 23:14:09 +0800

    Fixed modal

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 2018.1.90')
+console.log('Version: 2018.1.91')
 console.log('React Version: ' + React.version)
 
 ReactDOM.render(
