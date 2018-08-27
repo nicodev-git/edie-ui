008e7fd2bcc80c9f0f73750f299278c6ecdad179
nicodev-git <schaefernicker@gmail.com>
2018-08-27 16:39:18 +0800

    Fixed wf

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 2018.1.86')
+console.log('Version: 2018.1.87')
 console.log('React Version: ' + React.version)
 
 ReactDOM.render(
