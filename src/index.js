ad7a0c940f2e61809d56a3166fa42dbd289c6ed6
nicodev-git <schaefernicker@gmail.com>
2018-08-30 21:57:46 +0800

    Fixed label

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './style'
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
-console.log('Version: 2018.1.107')
+console.log('Version: 2018.1.108')
 console.log('React Version: ' + React.version)
 
 ReactDOM.render(
