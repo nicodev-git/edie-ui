f01f4c286eebc798cf89e4e5c5121b02c96536fb
nicodev-git <schaefernicker@gmail.com>
2018-09-05 18:01:03 +0800

    Fixed output objects page

--- src/components/sidebar/wf/outputObjects/OutputObjects.js
+++ src/components/sidebar/wf/outputObjects/OutputObjects.js
@@ -3,12 +3,28 @@ import React from 'react'
 import TabPage from 'components/common/TabPage'
 import TabPageBody from 'components/common/TabPageBody'
 import TabPageHeader from 'components/common/TabPageHeader'
+import {Button} from "@material-ui/core";
 
 export default class OutputObjects extends React.Component {
+  onClickAdd () {
+
+  }
+
   render() {
     return (
-      <div>
-      </div>
+      <TabPage>
+        <TabPageHeader title="Output Objects">
+          <div className="text-center margin-md-top">
+            <div className="pull-right">
+              <Button variant="raised" onClick={this.onClickAdd.bind(this)}>Add</Button>&nbsp;
+            </div>
+          </div>
+        </TabPageHeader>
+
+        <TabPageBody history={this.props.history} location={this.props.location}>
+
+        </TabPageBody>
+      </TabPage>
     )
   }
 }
