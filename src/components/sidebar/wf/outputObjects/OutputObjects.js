af5bbe431d86d4db136a692351ef35218bf6eba3
nicodev-git <schaefernicker@gmail.com>
2018-09-05 17:27:35 +0800

    Fixed outputs

---
+++ src/components/sidebar/wf/outputObjects/OutputObjects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import TabPage from 'components/common/TabPage'
+import TabPageBody from 'components/common/TabPageBody'
+import TabPageHeader from 'components/common/TabPageHeader'
+
+export default class OutputObjects extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
