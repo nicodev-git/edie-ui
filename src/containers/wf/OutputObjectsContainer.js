1547143e5015e59849a8116fc818fd8888da0509
nicodev-git <schaefernicker@gmail.com>
2018-09-10 14:00:00 +0800

    Fixed path

--- src/containers/wf/OutputObjectsContainer.js
+++ src/containers/wf/OutputObjectsContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 
-import OutputObjects from 'components/sidebar/wf/outputObjects/OutputObjects'
+import OutputObjects from 'components/sidebar/wf/playbookObjects/OutputObjects'
 
 import {
   fetchOutputObjects,
