ade77655df5389cae67ee5185f30b3a318cd4fa5
nicodev-git <schaefernicker@gmail.com>
2018-09-13 12:37:56 +0800

    Fixed map item

--- src/components/dashboard/map/mapItem/MapItemModal.js
+++ src/components/dashboard/map/mapItem/MapItemModal.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import {reduxForm} from 'redux-form'
+import connect from 'react-redux'
 
 import MapItemModalView from './MapItemModalView'
+
 export default class MapItemModal extends React.Component {
+  handleFormSubmit (values) {
+    this.props.onSave(values)
+  }
+
   render() {
+    const {handleSubmit, onClose} = this.props
     return (
-      <MapItemModalView/>
+      <MapItemModalView
+        onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}
+        onClose={onClose}
+      />
     )
   }
 }
+
+export default connect(
+  (state, props) => ({
+    initialValues: props.editMapItem
+  })
+)(reduxForm({form: 'mapItemForm'})(MapItemModal))
