33155fe219211e9c61129415eac6cd0fbbaceedb
nicodev-git <schaefernicker@gmail.com>
2018-09-13 11:48:37 +0800

    Fixed map item add

--- src/components/dashboard/map/mapItem/MapItemModalView.js
+++ src/components/dashboard/map/mapItem/MapItemModalView.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Field } from 'redux-form'
+
+import {
+  Modal,
+  CardPanel,
+  FormSelect,
+  FormInput
+} from 'components/modal/parts'
+import {Button} from "@material-ui/core";
+
+const mapItemTypes = [
+
+]
 
 export default class MapItemModalView extends React.Component {
   render() {
+    const {onSubmit, onClose} = this.props
+
     return (
-      <div></div>
+      <Modal title="Map Item" onRequestClose={onClose}>
+        <form onSubmit={onSubmit}>
+          <CardPanel title="Detail">
+            <FormSelect name="type" component={FormSelect} options={mapItemTypes}/>
+
+          </CardPanel>
+
+          <div className="padding-md">
+            <Button variant="raised" type="submit" className="margin-md-top">Save</Button>
+          </div>
+        </form>
+      </Modal>
     )
   }
 }
