1b94b4fb6d87c73cf51f1e1b2267b23e70ca54f7
nicodev-git <schaefernicker@gmail.com>
2018-09-25 10:04:34 +0300

    fix

--- src/actions/config.js
+++ src/actions/config.js
@@ -1,2 +1,2 @@
-export const ROOT_URL = 'https://eddieui-server.avatar.securegion.com'
-export const SRA_URL = 'https://aaa.avatar.securegion.com'
+export const ROOT_URL = 'http://eddieui-server.avatar.securegion.com'
+export const SRA_URL = 'http://aaa.avatar.securegion.com'
