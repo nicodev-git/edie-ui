497ae51b24dacb169f45f30032440803f7e50669
nicodev-git <schaefernicker@gmail.com>
2018-09-25 10:59:20 +0300

    fix dns

--- src/actions/config.js
+++ src/actions/config.js
@@ -1,2 +1,2 @@
-export const ROOT_URL = 'https://eddieui-server.avatar.securegion.com'
-export const SRA_URL = 'https://aaa.avatar.securegion.com'
+export const ROOT_URL = 'https://uiserver-avatar.securegion.com'
+export const SRA_URL = 'https://aaa-avatar.securegion.com'
